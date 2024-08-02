2f53f99f8a1e2d03681078fdc98d1ead34a9be2c
dertwist <45437750+dertwist@users.noreply.github.com>
2024-08-02 14:07:26 +0200

    BatchCreator GUI done

--- BatchCreator/BatchCreator_file_parser.py
+++ BatchCreator/BatchCreator_file_parser.py
@@ -1,11 +1,12 @@
-import configparser, os
+import configparser, os, json
 from preferences import config_dir
 
 config_file_path = os.path.join(config_dir, 'batch_creator.cfg')
 os.makedirs(config_dir, exist_ok=True)
-config = configparser.ConfigParser()
+
 
 def bc_set_config_value(section, key, value):
+    config = configparser.ConfigParser()
     if not config.has_section(section):
         config.add_section(section)
     config.set(section, key, value)
@@ -13,11 +14,13 @@ def bc_set_config_value(section, key, value):
         config.write(configfile)
 
 def bc_get_config_value(section, key):
+    config = configparser.ConfigParser()
     if config.has_section(section) and config.has_option(section, key):
         return config.get(section, key)
     return None
 
 def default_settings():
+    config = configparser.ConfigParser()
     if os.path.exists(config_file_path):
         config.read(config_file_path)
     else:
@@ -27,30 +30,28 @@ def default_settings():
 default_settings()
 
 def batch_creator_file_parser_parse(config_file):
-    # Create a ConfigParser object
     config = configparser.ConfigParser()
+    # Create a ConfigParser object
 
     # Read the configuration file
     config.read(config_file)
 
     # Extract values from the configuration
     version = config.get('APP', 'version', fallback=None)
-    content = config.get('CONTENT', 'file', fallback=None)
+    content = json.loads(config.get('CONTENT','file',fallback=None))
     exceptions = config.get('EXCEPTIONS', 'ignore_list', fallback=None)
 
     return version, content, exceptions
 
 def batch_creator_file_parser_output(version, content, exceptions, file_path):
-
     config = configparser.ConfigParser()
-
     # Add sections and key-value pairs
     config['APP'] = {
         'name': 'BatchCreator',
         'version': version
     }
     config['CONTENT'] = {
-        'file': content
+        'file': json.dumps(content)
     }
     config['EXCEPTIONS'] = {
         'ignore_list': exceptions
@@ -72,7 +73,7 @@ def batch_creator_file_parser_initialize(version,file_path):
         'version': version
     }
     config['CONTENT'] = {
-        'file': ""
+        'file': json.dumps('')
     }
     config['EXCEPTIONS'] = {
         'ignore_list': 'name.extension,name.extension,relative_path'
