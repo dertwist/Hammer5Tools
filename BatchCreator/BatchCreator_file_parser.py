6e5ba8231dcdbdc5600decb153e65c9648c736b3
dertwist <45437750+dertwist@users.noreply.github.com>
2024-08-02 11:39:11 +0200

    Save file function

--- BatchCreator/BatchCreator_file_parser.py
+++ BatchCreator/BatchCreator_file_parser.py
@@ -10,11 +10,33 @@ def batch_creator_file_parser_parse(config_file):
     # Extract values from the configuration
     version = config.get('APP', 'version', fallback=None)
     content = config.get('CONTENT', 'file', fallback=None)
-    exceptions = config.get('EXCEPTIONS', 'example', fallback=None)
+    exceptions = config.get('EXCEPTIONS', 'ignore_list', fallback=None)
 
     return version, content, exceptions
 
-def batch_creator_file_parser_initialize(version):
+def batch_creator_file_parser_output(version, content, exceptions, file_path):
+
+    config = configparser.ConfigParser()
+
+    # Add sections and key-value pairs
+    config['APP'] = {
+        'name': 'BatchCreator',
+        'version': version
+    }
+    config['CONTENT'] = {
+        'file': content
+    }
+    config['EXCEPTIONS'] = {
+        'ignore_list': exceptions
+    }
+    try:
+        with open(file_path, 'w') as configfile:
+            config.write(configfile)
+        print(f"File created at: {file_path}")
+    except Exception as e:
+        print(f"Failed to create file: {e}")
+
+def batch_creator_file_parser_initialize(version,file_path):
     # Create a ConfigParser object
     config = configparser.ConfigParser()
 
@@ -27,6 +49,12 @@ def batch_creator_file_parser_initialize(version):
         'file': ""
     }
     config['EXCEPTIONS'] = {
-        'example': 'name.extension'
+        'ignore_list': 'name.extension,name.extension,relative_path'
     }
-    return config
+
+    try:
+        with open(file_path, 'w') as configfile:
+            config.write(configfile)
+        print(f"File created at: {file_path}")
+    except Exception as e:
+        print(f"Failed to create file: {e}")
