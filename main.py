86b0f894f96bb66c16ea0f6f01cc460f983527f1
dertwist <45437750+dertwist@users.noreply.github.com>
2024-08-03 02:16:22 +0200

    Remember lat opened path fix

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ LOCK_FILE = os.path.join(tempfile.gettempdir(), 'hammer5tools.lock')
 SOCKET_HOST = 'localhost'
 SOCKET_PORT = 65432
 
-app_version = '1.2.0'
+app_version = '1.2.1'
 batchcreator_version = '1.0.0'
 
 
