b57ec696c77aac11e60542cc8a9b975a671a71a6
dertwist <45437750+dertwist@users.noreply.github.com>
2024-08-03 10:23:41 +0200

    Memory leak fixed, update check update

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ LOCK_FILE = os.path.join(tempfile.gettempdir(), 'hammer5tools.lock')
 SOCKET_HOST = 'localhost'
 SOCKET_PORT = 65432
 
-app_version = '1.3.0'
-batchcreator_version = '1.0.1'
+app_version = '1.3.1'
+batchcreator_version = '1.0.2'
 class Widget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -38,7 +38,7 @@ class Widget(QWidget):
         self.preferences_dialog = None
         self.Create_addon_Dialog = None
         self.Delete_addon_Dialog = None
-        check_updates("https://github.com/dertwist/Hammer5Tools", app_version)
+        check_updates("https://github.com/dertwist/Hammer5Tools", app_version, True)
 
         try:
             if get_config_bool('APP', 'first_launch'):
@@ -75,17 +75,18 @@ class Widget(QWidget):
             print("Wrong Cs2 Path")
     def refresh_addon_combobox(self):
         self.ui.ComboBoxSelectAddon.currentTextChanged.disconnect(self.selected_addon_name)
+
         addon = get_addon_name()
         self.ui.ComboBoxSelectAddon.clear()
         self.populate_addon_combobox()
         print(addon)
         self.ui.ComboBoxSelectAddon.setCurrentText(addon)
+
         self.ui.ComboBoxSelectAddon.currentTextChanged.connect(self.selected_addon_name)
 
     def setup_buttons(self):
         self.ui.Launch_Addon_Button.clicked.connect(launch_addon)
         self.ui.FixNoSteamLogon_Button.clicked.connect(self.SteamNoLogonFix)
-        # self.ui.ComboBoxSelectAddon.currentIndexChanged.connect(self.selected_addon_name)
         self.ui.ComboBoxSelectAddon.currentTextChanged.connect(self.selected_addon_name)
         # if index 0, loads tabs
         if self.ui.ComboBoxSelectAddon.currentText() == get_addon_name():
@@ -125,14 +126,14 @@ class Widget(QWidget):
         # Clean up BatchCreator_MainWindow
         try:
             if hasattr(self, 'BatchCreator_MainWindow') and self.BatchCreator_MainWindow:
-                self.BatchCreator_MainWindow.deleteLater()
-                print('1')
+                self.BatchCreator_MainWindow.close()
         except Exception as e:
             print('Error while cleaning up BatchCreator_MainWindow:', e)
 
         # Create a new instance of BatchCreatorMainWindow
         try:
             self.BatchCreator_MainWindow = BatchCreatorMainWindow(batchcreator_version)
+
             self.ui.BatchCreator_tab.layout().addWidget(self.BatchCreator_MainWindow)
         except Exception as e:
             print('Error while setting up BatchCreator_MainWindow:', e)
@@ -145,7 +146,7 @@ class Widget(QWidget):
 
     def open_preferences_dialog(self):
         if self.preferences_dialog is None:
-            self.preferences_dialog = PreferencesDialog(self)
+            self.preferences_dialog = PreferencesDialog(app_version, self)
             self.preferences_dialog.show()
             self.preferences_dialog.finished.connect(self.preferences_dialog_closed)
 
