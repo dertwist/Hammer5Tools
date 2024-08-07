f6a933653a64a72a1e054bab0ac97dbd5be1b2b8
dertwist <45437750+dertwist@users.noreply.github.com>
2024-08-08 01:03:41 +0200

    Batch Creator output and input folder dialogs

--- BatchCreator/BatchCreator_process.py
+++ BatchCreator/BatchCreator_process.py
@@ -45,21 +45,36 @@ def batchcreator_process_all(current_path_file, process, preview):
     ignore_extensions = [f"{item}" for item in process['ignore_extensions'].split(',')]
     files_r = search_files(folder_path, algorithm, ignore_extensions)
     if preview:
-        files_list_out = []
-        for root, dirs, files in os.walk(folder_path):
-            for file in files:
-                if not any(file.endswith(ext) for ext in ignore_extensions):
-                    files_list_out.append(file)
-        return files_r, files_list_out, extension
+        if bool((process['load_from_the_folder'])):
+            files_list_out = []
+            for root, dirs, files in os.walk(folder_path):
+                for file in files:
+                    if not any(file.endswith(ext) for ext in ignore_extensions):
+                        files_list_out.append(file)
+            return files_r, files_list_out, extension, folder_path
+        return None, None, None, None
     else:
-        content = (batch_creator_file_parser_parse(current_path_file))[1]
-        for item in files_r:
-            content_out = content.replace("#$FOLDER_PATH$#", folder_path_relative)
-            content_out = content_out.replace("#$ASSET_NAME$#", item)
-            filename = os.path.join(folder_path, item) + f".{extension}"
-            with open(filename, 'w') as file:
-                # Write some content to the file
-                file.write(content_out)
+        if bool((process['output_to_the_folder'])):
+            content = (batch_creator_file_parser_parse(current_path_file))[1]
+            for item in files_r:
+                content_out = content.replace("#$FOLDER_PATH$#", folder_path_relative)
+                content_out = content_out.replace("#$ASSET_NAME$#", item)
+                filename = os.path.join(folder_path, item) + f".{extension}"
+                with open(filename, 'w') as file:
+                    # Write some content to the file
+                    file.write(content_out)
 
-            print(f'File {filename} created successfully.')
+                print(f'File {filename} created successfully.')
+        else:
+            content = (batch_creator_file_parser_parse(current_path_file))[1]
+            for item in files_r:
+                content_out = content.replace("#$FOLDER_PATH$#", folder_path_relative)
+                content_out = content_out.replace("#$ASSET_NAME$#", item)
+                filename = os.path.join(folder_path, item) + f".{extension}"
+                with open(filename, 'w') as file:
+                    # Write some content to the file
+                    file.write(content_out)
+
+                print(f'File {filename} created successfully.')
+
 
