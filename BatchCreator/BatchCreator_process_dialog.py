5b794fc7b3cd959408af4b473364beb9c89dba77
dertwist <45437750+dertwist@users.noreply.github.com>
2024-08-08 02:19:39 +0200

    Batch Creator ignore functional

--- BatchCreator/BatchCreator_process_dialog.py
+++ BatchCreator/BatchCreator_process_dialog.py
@@ -1,5 +1,5 @@
 from BatchCreator.ui_BatchCreator_process_dialog import Ui_BatchCreator_process_Dialog
-from PySide6.QtWidgets import QDialog,QLabel, QFileDialog
+from PySide6.QtWidgets import QDialog,QLabel, QFileDialog, QPushButton, QHBoxLayout, QWidget, QListWidgetItem
 from distutils.util import strtobool
 from BatchCreator.BatchCreator_process import batchcreator_process_all
 import os
@@ -31,7 +31,7 @@ class BatchCreator_process_Dialog(QDialog):
             self.ui.ignore_extensions_lineEdit.textChanged.connect(self.on_ignore_extensions_changed)
 
             self.ui.ignore_files_lineEdit.setText(process['ignore_list'])
-            self.ui.ignore_files_lineEdit.textChanged.connect(self.on_ignore_extensions_changed)
+            self.ui.ignore_files_lineEdit.textChanged.connect(self.on_ignore_files_changed)
 
             self.ui.select_files_to_process_button.clicked.connect(self.on_pressed_select_files_to_process_button)
             self.ui.choose_output_button.clicked.connect(self.on_pressed_choose_output_button)
@@ -72,12 +72,50 @@ class BatchCreator_process_Dialog(QDialog):
         batchcreator_process_all(self.current_file_path, preview=False, process=self.process)
     def process_preview(self):
         files = batchcreator_process_all(self.current_file_path, preview=True, process=self.process)
-        print(files)
         self.ui.Input_files_preview_scrollarea.clear()
         self.ui.output_files_preview_scrollarea.clear()
-        for item in files[1]:
-            label = QLabel(item)
-            self.ui.Input_files_preview_scrollarea.addItem(label.text())
+        ignore_list = self.process['ignore_list']
+        ignore_list = [item.strip() for item in ignore_list.split(',')]
+        print(type(ignore_list))
+        print(ignore_list)
+        for index, item in enumerate(files[1]):
+            if item in ignore_list:
+                print(item)
+                pass
+            else:
+                label = QLabel(item)
+                # Update the QPushButton stylesheet to make it smaller
+                remove_button = QPushButton('Ignore')
+                remove_button.setStyleSheet(
+                    "QPushButton { font-size: 10px; width: 16px; padding: 2px; margin: 0px;}")
+
+                def ignore_item(item):
+                    # Get the current scroll position
+                    scroll_position = self.ui.Input_files_preview_scrollarea.verticalScrollBar().value()
+
+                    # Remove the item from the UI
+                    self.process['ignore_list'] += f",{item}"
+                    self.process_preview()
+                    item_widget.deleteLater()
+
+                    # Reset the scroll position after deletion
+                    self.ui.Input_files_preview_scrollarea.verticalScrollBar().setValue(scroll_position)
+
+                # Use lambda to capture the current value of item
+                remove_button.clicked.connect(lambda _, item=item: ignore_item(item))
+
+                item_widget = QWidget()
+                h_layout = QHBoxLayout()
+                h_layout.addWidget(label)
+                h_layout.addWidget(remove_button)
+                item_widget.setLayout(h_layout)
+
+                list_item = QListWidgetItem()
+                list_item.setSizeHint(item_widget.sizeHint())  # Set the size hint for the item
+
+                self.ui.Input_files_preview_scrollarea.addItem(list_item)
+                self.ui.Input_files_preview_scrollarea.setItemWidget(list_item, item_widget)
+        self.ui.ignore_files_lineEdit.setText(self.process['ignore_list'])
         for item in files[0]:
             label = QLabel(item + f".{files[2]}")
             self.ui.output_files_preview_scrollarea.addItem(label.text())
