2368f25efbf61a62e225f7ce5592b55b3c6d40d4
dertwist <45437750+dertwist@users.noreply.github.com>
2024-08-10 11:06:17 +0200

    Soundevent editor properties list

--- soudevent_editor/properties/soundevent_editor_properties_list.py
+++ soudevent_editor/properties/soundevent_editor_properties_list.py
@@ -5,8 +5,65 @@ soundevent_editor_properties = [
     {'randomize_position_max_radius': '3000'},
     {'retrigger_interval_min': '10'},
     {'retrigger_interval_max': '30'},
-    {'base': ''},
-    {'pitch': '1'}
+    {'position': '[0,0,0]'},
+    {'enable_child_events': 'true'},
+    {'badsp_presetse': 'reverb_9_mediumChamber'},
+    {'soundevent_01': ''},
+    {'randomize_position_hemisphere': 'false'},
+    {'distance_volume_mapping_curve': """		[
+			[
+				0.0, 1.0, 0.0, 0.0,
+				2.0, 3.0,
+			],
+			[
+				500.0, 1.0, 0.0, 0.0,
+				2.0, 3.0,
+			],
+		]"""},
+    {'fadetime_volume_mapping_curve': """		[
+			[
+				0.0, 1.0, 0.0, 0.0,
+				2.0, 3.0,
+			],
+			[
+				500.0, 1.0, 0.0, 0.0,
+				2.0, 3.0,
+			],
+		]"""},
+    {'distance_unfiltered_stereo_mapping_curve': """		[
+			[
+				0.0, 1.0, 0.0, 0.0,
+				2.0, 3.0,
+			],
+			[
+				500.0, 1.0, 0.0, 0.0,
+				2.0, 3.0,
+			],
+		]"""},
+    {'time_volume_mapping_curve': """		[
+			[
+				0.0, 1.0, 0.0, 0.0,
+				2.0, 3.0,
+			],
+			[
+				500.0, 1.0, 0.0, 0.0,
+				2.0, 3.0,
+			],
+		]"""},
+    {'type': 'csgo_mega'},
+    {'mixgroup': 'Amb_Common'},
+    {'occlusion_intensity': '0.0'},
+    {'distance_effect_mix': '0.0'},
+    {'restrict_source_reverb': 'true'},
+    {'use_distance_unfiltered_stereo_mapping_curve': 'true'},
+    {'use_time_volume_mapping_curve': 'true'},
+    {'use_time_volume_mapping_curve': 'false'},
+    {'override_dsp_preset': 'true'},
+    {'set_child_position': 'true'},
+    {'reverb_wet': '1.0'},
+    {'pitch': '1'},
+    {'position_relative_to_player': 'false'},
+    {'use_world_position': 'false'}
 
 
 ]
