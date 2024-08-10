643c862623995ced536aeed170f858397fe67c28
dertwist <45437750+dertwist@users.noreply.github.com>
2024-08-10 17:41:34 +0200

    Popup Ui

--- popup_menu/popup_menu_main.py
+++ popup_menu/popup_menu_main.py
@@ -5,9 +5,10 @@ from PySide6.QtWidgets import QPushButton
 from PySide6.QtCore import QEvent, Qt, QSize
 from PySide6.QtCore import Signal
 from popup_menu.ui_popup_menu import Ui_PoPupMenu
+from PySide6.QtWidgets import QSpacerItem, QSizePolicy
 class PopupMenu(QDialog):
-    label_clicked = Signal(str)  # Define a signal that takes a string as an argument
-    add_property_signal = Signal(str,str)
+    label_clicked = Signal(str)
+    add_property_signal = Signal(str, str)
 
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -16,25 +17,30 @@ class PopupMenu(QDialog):
         self.setWindowFlags(self.windowFlags() | Qt.FramelessWindowHint)
         self.setGeometry(200, 200, 400, 300)
 
+
+        self.ui.lineEdit.textChanged.connect(self.search_text_changed)
+
         layout = QVBoxLayout(self.ui.scrollArea)
         scroll_content = QWidget()
         scroll_layout = QVBoxLayout(scroll_content)
-        scroll_layout.setContentsMargins(0,0,2,0)
+        scroll_layout.setContentsMargins(0, 0, 2, 0)
+        scroll_layout.addSpacerItem( QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding))
 
         for i in range(16):
             label = QLabel(f"Element {i + 1}")
             label.mousePressEvent = lambda event, text=label.text(): self.label_clicked.emit(text)
 
-
             element_layout = QHBoxLayout()
-            element_layout.setContentsMargins(0,0,0,0)
+            element_layout.setContentsMargins(0, 0, 0, 0)
             element_layout.addWidget(label)
-
+            scroll_layout.insertLayout(scroll_layout.count() - 1, element_layout)
             label.setStyleSheet("""
             QLabel {
+                 font: 580 10pt "Segoe UI";
                 border-bottom: 0.5px solid black;  
                 border-radius: 0px; border-color: 
                 rgba(40, 40, 40, 255);
+                padding-top:8px;
             }
             QLabel:hover {
                 background-color: #414956;
@@ -48,8 +54,8 @@ class PopupMenu(QDialog):
                 border: 2px solid black;
                 border-radius: 2px;
                 border-color: rgba(80, 80, 80, 255);
-                height:12px;
-                width:12px;
+                height: 12px;
+                width: 12px;
                 color: #E3E3E3;
                 background-color: #1C1C1C;
             }
@@ -59,11 +65,10 @@ class PopupMenu(QDialog):
             }
             QToolButton:pressed {
                 background-color: #1C1C1C;
-                margin:  0px;
+                margin: 0px;
                 margin-left: 0px;
                 margin-right: 0px;
                 font: 700 10pt "Sego";
-            
             }
             """)
 
@@ -71,7 +76,7 @@ class PopupMenu(QDialog):
             bookmark_icon_add = QIcon("://icons/bookmark_add_24dp_9D9D9D_FILL0_wght400_GRAD0_opsz24.svg")
             tool_button.setIcon(bookmark_icon_add)
             tool_button.setIconSize(QSize(24, 24))
-            element_layout.addWidget(tool_button)
+            # element_layout.addWidget(tool_button)
 
             scroll_layout.addLayout(element_layout)
 
@@ -80,14 +85,34 @@ class PopupMenu(QDialog):
     def event(self, event):
         if event.type() == QEvent.WindowDeactivate:
             self.close()
-            return True  # Return True after handling the event
-
-        return super().event(event)  # Return the result of the base class event method if the condition is not met
+            return True
+        return super().event(event)
 
     def showEvent(self, event):
         cursor_pos = QCursor.pos()
         self.move(cursor_pos)
         super().showEvent(event)
+
+    def search_text_changed(self):
+        search_text = self.ui.lineEdit.text().lower()  # Get the text from the QLineEdit and convert to lowercase for case-insensitive search
+
+        scroll_content = self.ui.scrollArea.widget()  # Get the scroll area widget contents
+        for i in range(scroll_content.layout().count()):
+            element_layout_item = scroll_content.layout().itemAt(i)
+
+            if element_layout_item is not None:
+                element_layout = element_layout_item.layout()  # Get the layout of each element
+
+                if element_layout is not None:
+                    label = element_layout.itemAt(0).widget()  # Get the label widget from the layout
+                    # tool_button = element_layout.itemAt(1).widget()  # Get the tool button widget from the layout
+
+                    if search_text in label.text().lower():  # Check if the search text is present in the label text
+                        element_layout.itemAt(0).widget().show()  # Show the label if it matches the search text
+                        # tool_button.show()  # Show the tool button if it matches the search text
+                    else:
+                        element_layout.itemAt(0).widget().hide()  # Hide the label if it doesn't match the search text
+                        # tool_button.hide()  # Hide the tool button if it doesn't match the search text
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
