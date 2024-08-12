a171a9c8a5f4f9ef49c24896d6e5f6f8feeec5a4
dertwist <45437750+dertwist@users.noreply.github.com>
2024-08-12 08:38:54 +0200

    cleanup

--- BatchCreator/BatchCreator_custom_highlighter.py
+++ BatchCreator/BatchCreator_custom_highlighter.py
@@ -6,27 +6,21 @@ class CustomHighlighter(QSyntaxHighlighter):
         super().__init__(parent)
         self.highlighting_rules = []
 
-        # Define the format for the custom text
         custom_format = QTextCharFormat()
         custom_format.setForeground(QColor("#C78662"))
 
-        # Define the format for the new keywords
         keyword_format = QTextCharFormat()
-        keyword_format.setForeground(QColor("#FF5733"))  # Example color, change as needed
+        keyword_format.setForeground(QColor("#FF5733"))
 
-        # Define the format for text within quotes
         quoted_text_format = QTextCharFormat()
         quoted_text_format.setForeground(QColor("#6bc7c4"))
 
-        # Define the format for FOLDER_PATH pattern
         folder_path_format = QTextCharFormat()
-        folder_path_format.setForeground(QColor("#C78662"))  # Example color, change as needed
+        folder_path_format.setForeground(QColor("#C78662"))
 
-        # Define the format for ASSET_NAME pattern
         asset_name_format = QTextCharFormat()
-        asset_name_format.setForeground(QColor("#C78662"))  # Example color, change as needed
+        asset_name_format.setForeground(QColor("#C78662"))
 
-        # Define the new keyword patterns
         keyword_patterns = [
             r"\bTextureAmbientOcclusion\b",
             r"\bTextureColor1\b",
@@ -35,22 +29,18 @@ class CustomHighlighter(QSyntaxHighlighter):
             r"\bTextureNormal\b"
         ]
 
-        # Add the keyword patterns and their format to the highlighting rules
         for pattern in keyword_patterns:
             regex = QRegularExpression(pattern)
             self.highlighting_rules.append((regex, keyword_format))
 
-        # Add the pattern for text within quotes
         quoted_text_pattern = QRegularExpression(r'"\s*[^"]+\s*"')
         self.highlighting_rules.append((quoted_text_pattern, quoted_text_format))
 
-        # Define the patterns to be highlighted
         patterns = [
             (r"#\$FOLDER_PATH\$#", folder_path_format),
             (r"#\$ASSET_NAME\$#", asset_name_format)
         ]
 
-        # Add the existing patterns and their format to the highlighting rules
         for pattern, format in patterns:
             regex = QRegularExpression(pattern)
             self.highlighting_rules.append((regex, format))
--- loading_editor/svg_drag_and_drop.py
+++ loading_editor/svg_drag_and_drop.py
@@ -1,17 +1,15 @@
 import os
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QPixmap
-from PySide6.QtWidgets import QLabel, QVBoxLayout, QWidget, QMainWindow, QApplication
-from PySide6.QtSvg import QSvgRenderer
-from PySide6.QtGui import QImage, QPainter
+from PySide6.QtWidgets import QLabel, QVBoxLayout, QWidget, QMainWindow
 
 
-class ImageLabel(QLabel):  # Inherit from QLabel instead of QSvgWidget
+class ImageLabel(QLabel):
     def __init__(self):
         super().__init__()
         self.setAlignment(Qt.AlignCenter)
         self.setText('Drag and drop an icon in svg format')
-        self.setStyleSheet("margin: 0px; border: 0px;")  # Remove margin and border
+        self.setStyleSheet("margin: 0px; border: 0px;")
 
     def updatePixmap(self, image):
         super().setPixmap(image)
@@ -28,8 +26,8 @@ class Svg_Drag_and_Drop(QMainWindow):
         self.setCentralWidget(centralWidget)
         self.photoViewer = ImageLabel()
         mainLayout.addWidget(self.photoViewer)
-        self.setStyleSheet("margin: 0px; border: 0px;")  # Remove margin and border for the main window
-        self.file_path = None  # Initialize the file_path variable
+        self.setStyleSheet("margin: 0px; border: 0px;")
+        self.file_path = None
 
     def dragEnterEvent(self, event):
         if event.mimeData().hasUrls():
@@ -50,7 +48,7 @@ class Svg_Drag_and_Drop(QMainWindow):
                 if file_path.lower().endswith('.svg'):
                     self.photoViewer.updatePixmap(QPixmap(file_path))
                     self.photoViewer.setText(f'{file_name}')
-                    self.file_path = file_path  # Store the file path
+                    self.file_path = file_path
                 else:
                     self.photoViewer.setText('Only SVG files are accepted.')
             event.acceptProposedAction()
@@ -61,9 +59,3 @@ class Svg_Drag_and_Drop(QMainWindow):
         if not self.file_path or not self.file_path.lower().endswith('.svg'):
             raise ValueError("The file is not an SVG file.")
         return self.file_path
-
-# Example usage:
-# app = QApplication([])
-# window = Svg_Drag_and_Drop()
-# window.show()
-# app.exec()
