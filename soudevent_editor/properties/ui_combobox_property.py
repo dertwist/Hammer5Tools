b8ee387aebd8cea6cdedb33b6f573748350b90e7
dertwist <45437750+dertwist@users.noreply.github.com>
2024-08-16 17:30:10 +0200

    parsing soundevents from the game

---
+++ soudevent_editor/properties/ui_combobox_property.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'combobox_property.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QComboBox, QFrame, QHBoxLayout,
+    QLabel, QSizePolicy, QWidget)
+
+class Ui_PropertyWidet(object):
+    def setupUi(self, PropertyWidet):
+        if not PropertyWidet.objectName():
+            PropertyWidet.setObjectName(u"PropertyWidet")
+        PropertyWidet.resize(683, 48)
+        PropertyWidet.setMinimumSize(QSize(0, 48))
+        PropertyWidet.setMaximumSize(QSize(16777215, 48))
+        PropertyWidet.setStyleSheet(u"")
+        self.horizontalLayout = QHBoxLayout(PropertyWidet)
+        self.horizontalLayout.setSpacing(0)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.frame = QFrame(PropertyWidet)
+        self.frame.setObjectName(u"frame")
+        self.frame.setStyleSheet(u".QFrame {\n"
+"    font: 580 10pt \"Segoe UI\";\n"
+"    border: 2px solid black;\n"
+"    border-radius: 0px;\n"
+"    border-left: 0px;\n"
+"    border-right: 0px;\n"
+"	border-top: 0px;\n"
+"    border-color: rgba(50, 50, 50, 255);\n"
+"    padding: 8px;\n"
+"    padding-left: 6px;\n"
+"    padding-right: 6px;\n"
+"    color: #E3E3E3;\n"
+"    background-color: #1C1C1C;\n"
+"}\n"
+"\n"
+".QFrame::hover {\n"
+"}\n"
+".QFrame::selected {\n"
+"    background-color: #414956;\n"
+"}")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.frame.setLineWidth(0)
+        self.horizontalLayout_2 = QHBoxLayout(self.frame)
+        self.horizontalLayout_2.setSpacing(0)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.label = QLabel(self.frame)
+        self.label.setObjectName(u"label")
+        self.label.setTextFormat(Qt.AutoText)
+        self.label.setWordWrap(False)
+
+        self.horizontalLayout_2.addWidget(self.label)
+
+        self.comboBox = QComboBox(self.frame)
+        self.comboBox.setObjectName(u"comboBox")
+        self.comboBox.setEditable(True)
+
+        self.horizontalLayout_2.addWidget(self.comboBox)
+
+
+        self.horizontalLayout.addWidget(self.frame)
+
+
+        self.retranslateUi(PropertyWidet)
+
+        QMetaObject.connectSlotsByName(PropertyWidet)
+    # setupUi
+
+    def retranslateUi(self, PropertyWidet):
+        PropertyWidet.setWindowTitle(QCoreApplication.translate("PropertyWidet", u"Form", None))
+        self.label.setText(QCoreApplication.translate("PropertyWidet", u"TextLabel", None))
+    # retranslateUi
+
