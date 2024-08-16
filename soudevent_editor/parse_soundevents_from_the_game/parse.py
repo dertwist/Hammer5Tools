15b348f0048995c9bb79a71b9c8268b63fe7302a
dertwist <45437750+dertwist@users.noreply.github.com>
2024-08-16 17:37:45 +0200

    parsing soundevents from the game

--- soudevent_editor/parse_soundevents_from_the_game/parse.py
+++ soudevent_editor/parse_soundevents_from_the_game/parse.py
@@ -1,25 +1,18 @@
 import keyvalues3 as kv3
 import os
-
-# Path to the soundevents folder
 soundevents_folder = 'soundevents'
 soundevents = []
 for root, _, files in os.walk(soundevents_folder):
     for file_name in files:
-        if file_name.endswith('.vsndevts'):  # Check if the file is a vsndevts file
+        if file_name.endswith('.vsndevts'):
             file_path = os.path.join(root, file_name)
-
             try:
-                # Apply the code snippet for each file
                 data = kv3.read(file_path)
-
                 for key in data.keys():
                     print(key)
-                    soundevents.append(key)  # Append the key to the soundevents list
-
+                    soundevents.append(key)
             except UnicodeDecodeError as e:
                 print(f"Error decoding file: {file_path}. {e}")
 
-with open('sounds.txt', 'w') as file:
-    for event in soundevents:
-        file.write(event + '\n')
+with open('items.py', 'w') as file:
+    file.write("soundevents = " + str(soundevents))
