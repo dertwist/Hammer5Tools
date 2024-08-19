fc06746cd54c7567d025879c54a3eed1d53f3317
dertwist <45437750+dertwist@users.noreply.github.com>
2024-08-19 09:37:04 +0200

    tree

--- testing/kv3_parcer/kv3_parser.py
+++ testing/kv3_parcer/kv3_parser.py
@@ -4,8 +4,9 @@ import keyvalues3 as kv3
 
 from PySide6.QtWidgets import QApplication, QMainWindow, QTreeView, QVBoxLayout, QWidget, QHeaderView
 from PySide6.QtGui import QStandardItemModel, QStandardItem
+from PySide6.QtWidgets import QPushButton
 data = {'m_Variables': [{'_class': 'CSmartPropVariable_Float', 'm_VariableName': 'length', 'm_nElementID': 61}, {'_class': 'CSmartPropVariable_Int', 'm_VariableName': 'height', 'm_nElementID': 62}], 'generic_data_type': 'CSmartPropRoot', '_editor': {'m_nElementID':'1'}}
-
+from PySide6.QtCore import Qt
 data_read = kv3.read('sample.vsmart')
 data = data_read
 print(type(data), data)
@@ -64,56 +65,33 @@ import json
 
 
 def add_items(parent, data):
-    # for key, value in data.items():
-    #     print(key, value)
-    #     if isinstance(value, dict):
-    #         # Create a parent item for the dictionary key
-    #         parent_item = QStandardItem(str(key))
-    #         parent.appendRow(parent_item)
-    #         # Recursively call add_items for the nested dictionary
-    #         add_items(parent_item, value)
-    #     elif isinstance(value, list):
-    #         # Create a parent item for the list key
-    #         parent_item = QStandardItem(str(key))
-    #         parent.appendRow(parent_item)
-    #         # Iterate through the list and add items for each element
-    #         for index, item in enumerate(value):
-    #             item_key = QStandardItem(f"[{index}]")
-    #             parent_item.appendRow(item_key)
-    #             # Recursively call add_items for the list element
-    #             add_items(item_key, item)
-    #     else:
-    #         # Create items for non-dict and non-list values
-    #         key_item = QStandardItem(str(key))
-    #         value_item = QStandardItem(str(value))
-    #         parent.appendRow([key_item, value_item])
-
-
 
     for key, value in data.items():
         key_item = QStandardItem(key)
         if isinstance(value, dict):
-            value_item = QStandardItem("")
-            parent.appendRow([key_item, value_item])
-            add_items(key_item, value)
+                print(1, value)
+                value_item = QStandardItem("")
+                parent.appendRow([key_item, value_item])
+                if value == '_editor':
+                    pass
+                else:
+                    add_items(key_item, value)
         elif isinstance(value, list):
             list_parent = QStandardItem(key)
             parent.appendRow(list_parent)
             try:
                 for item in value:
-                    if key == 'm_Children':
-                        if isinstance(item, dict):
-                            child_item = QStandardItem(item['_class'])
-                            list_parent.appendRow(child_item)
-                            print(item)
-                            if 'm_Children' in item:
-                                add_items(child_item, item)  # Recursively call add_items for the list element
-                    else:
-                        pass
-                        # if isinstance(item, dict):
-                        #     child_item = QStandardItem(item['_class'])  # Create an empty item for the list element
-                        #     list_parent.appendRow(child_item)
-                        #     add_items(child_item, item)  # Recursively call add_items for the list element
+                    child_item = QStandardItem(item['_class'])
+                    # if item.get('_class') == 'CSmartPropElement_Group':
+                    # print('CSmartPropElement_Model', item)
+                    # keys_except_m_children = [key for key in data.keys() if key != 'm_Children']
+                    # child_value = QStandardItem(', '.join(keys_except_m_children))
+                    # list_parent.appendRow([child_item, child_value])
+                    # else:
+                    list_parent.appendRow(child_item)
+                    print(item)
+                    # if 'm_Children' in item:
+                    add_items(child_item, item)
             except:
                 pass
 
@@ -135,6 +113,40 @@ tree_view = QTreeView()
 model = QStandardItemModel()
 model.setHorizontalHeaderLabels(["Key", "Value"])
 root_item = model.invisibleRootItem()
+
+
+global data_out_1
+
+def export_tree_data(parent_item, output_dict):
+    for row in range(parent_item.rowCount()):
+        item = parent_item.child(row, 0)
+        key = item.text()  # Get the text of the item as the key
+        value_item = parent_item.child(row, 1)
+        if value_item:
+            value = value_item.text()  # Get the text of the value item
+            output_dict[key] = value  # Store key-value pair in the dictionary
+            if item.hasChildren():
+                child_output_dict = {}  # Create a new dictionary for the nested key-value pairs
+                output_dict[key] = child_output_dict
+                export_tree_data(item, child_output_dict)
+
+def export_data():
+    def print_items(parent_item, indent=""):
+        for row in range(parent_item.rowCount()):
+            key_item = parent_item.index(row, 0).data()
+            value_item = parent_item.index(row, 1).data()
+            if value_item is not None:
+                print(indent + key_item, value_item)
+                if value_item.hasChildren():
+                    print_items(value_item, indent + "  ")
+            else:
+                print(indent + key_item, "None")
+
+    print_items(model)
+# Create a QPushButton for exporting data
+export_button = QPushButton("Export Data")
+export_button.clicked.connect(export_data)
+layout.addWidget(export_button)
 
 # Populate the model with data
 add_items(root_item, data)
