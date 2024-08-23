f54c18f0d3117060ce0c86227f29892732181b20
dertwist <45437750+dertwist@users.noreply.github.com>
2024-08-23 23:52:26 +0200

    Open output file

--- testing/kv3_svnd.py
+++
@@ -1,3 +0,0 @@
-import keyvalues3 as kv3
-data = kv3.read("E:\SteamLibrary\steamapps\common\Counter-Strike Global Offensive\content\csgo_addons\de_selathom\soundevents\soundevents_addon.vsndevts")
-print(data['new.soundevent'])
