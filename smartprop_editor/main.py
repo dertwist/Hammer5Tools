448ec88bf65d00eb1a4eaca2a85ece16178a6761
dertwist <45437750+dertwist@users.noreply.github.com>
2024-09-03 22:14:45 +0200

    New ui for smartprops

--- smartprop_editor/main.py
+++ smartprop_editor/main.py
@@ -24,48 +24,13 @@ import keyvalues3 as kv3
 from PySide6.QtCore import QPoint, QSize
 
 # Save positions and sizes of dock widgets
-def save_dock_widget_positions_sizes(dock_widgets):
-    positions_sizes = {}
-    for dock_widget in dock_widgets:
-        widget_name = dock_widget.objectName()
-        position = [(dock_widget.pos()).x(), (dock_widget.pos()).y()]
-        print('position', position)
-        size = [dock_widget.size().width(), dock_widget.size().height()]
-        print('size', size)
-        positions_sizes[widget_name] = {
-            'position': str(position),
-            'size': str(size)
-        }
-    # Save positions and sizes to configuration
-    set_config_value('SMARTPROPS_DOCK_WIDGETS', 'positions_sizes', str(positions_sizes))  # Convert positions_sizes to string
 
-# Restore positions and sizes of dock widgets
-# Restore positions and sizes of dock widgets
-def restore_dock_widget_positions_sizes(dock_widgets):
-    positions_sizes = get_config_value('SMARTPROPS_DOCK_WIDGETS', 'positions_sizes')
-    print(type(positions_sizes), positions_sizes)
-    import ast
-
-    if positions_sizes:
-        positions_sizes = ast.literal_eval(positions_sizes)
-        print(type(positions_sizes), positions_sizes)
-        print((positions_sizes.items()))
-        for item in positions_sizes.items():
-            print(item)
-        for dock_widget in dock_widgets:
-
-            widget_name = dock_widget.objectName()
-            position = positions_sizes[widget_name]['position']
-            position = ast.literal_eval(position)
-            size = positions_sizes[widget_name]['size']
-            size = ast.literal_eval(size)
-            print(type(position), position)
-            print(widget_name, position, size)
-            dock_widget.move(position[0],position[1])
-            dock_widget.resize(size[0],size[1])
+from PySide6.QtCore import QByteArray
 
 
 # In your SmartPropEditorMainWindow class
+from PySide6.QtCore import QByteArray
+
 class SmartPropEditorMainWindow(QMainWindow):
     def __init__(self, version="1", parent=None):
         super().__init__(parent)
@@ -73,15 +38,8 @@ class SmartPropEditorMainWindow(QMainWindow):
         self.ui.setupUi(self)
         self.ui.version_label.setText(version)
 
-        # Call functions to restore positions and sizes
-        dock_widgets = [self.ui.dockWidget_2, self.ui.dockWidget_3]  # Add all dock widgets here
-        # self.ui.dockWidget_2.titleBarWidget()
-        restore_dock_widget_positions_sizes(dock_widgets)
-
     def closeEvent(self, event):
-        dock_widgets = [self.ui.dockWidget_2, self.ui.dockWidget_3]  # Add all dock widgets here
-        save_dock_widget_positions_sizes(dock_widgets)
-        # event.accept()
+        pass
 
 # Main block remains the same
 if __name__ == "__main__":
