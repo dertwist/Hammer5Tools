582da69289094dbcc67c1699b4f8f6e49c7b181b
dertwist <45437750+dertwist@users.noreply.github.com>
2024-09-07 12:58:03 +0200

    Documentation update

--- documentation/ui_documentation_dialog.py
+++ documentation/ui_documentation_dialog.py
@@ -24,14 +24,14 @@ class Ui_documentation_dialog(object):
     def setupUi(self, documentation_dialog):
         if not documentation_dialog.objectName():
             documentation_dialog.setObjectName(u"documentation_dialog")
-        documentation_dialog.resize(645, 540)
+        documentation_dialog.resize(645, 505)
         sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(documentation_dialog.sizePolicy().hasHeightForWidth())
         documentation_dialog.setSizePolicy(sizePolicy)
-        documentation_dialog.setMinimumSize(QSize(645, 540))
-        documentation_dialog.setMaximumSize(QSize(645, 540))
+        documentation_dialog.setMinimumSize(QSize(645, 505))
+        documentation_dialog.setMaximumSize(QSize(645, 505))
         documentation_dialog.setStyleSheet(u"background-color: #1C1C1C;")
         documentation_dialog.setSizeGripEnabled(False)
         documentation_dialog.setModal(False)
@@ -96,131 +96,9 @@ class Ui_documentation_dialog(object):
 
         self.verticalLayout.addWidget(self.frame_4)
 
-        self.frame_2 = QFrame(documentation_dialog)
-        self.frame_2.setObjectName(u"frame_2")
-        self.frame_2.setFrameShape(QFrame.StyledPanel)
-        self.frame_2.setFrameShadow(QFrame.Raised)
-        self.horizontalLayout_2 = QHBoxLayout(self.frame_2)
-        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.horizontalLayout_2.setContentsMargins(9, 0, 9, 0)
-        self.online_help_loading_ediotor_button = QPushButton(self.frame_2)
-        self.online_help_loading_ediotor_button.setObjectName(u"online_help_loading_ediotor_button")
-        self.online_help_loading_ediotor_button.setStyleSheet(u"\n"
-"    /* QPushButton default and hover styles */\n"
-"    QPushButton {\n"
-"\n"
-"        font: 600 10pt \"Segoe UI\";\n"
-"	\n"
-"\n"
-"        border: 2px solid black;\n"
-"        border-radius: 4px;\n"
-"        border-color: rgba(80, 80, 80, 255);\n"
-"        height:22px;\n"
-"        padding-top: 2px;\n"
-"        padding-bottom:2px;\n"
-"        padding-left: 4px;\n"
-"        padding-right: 4px;\n"
-"        color: #E3E3E3;\n"
-"        background-color: #1C1C1C;\n"
-"    }\n"
-"    QPushButton:hover {\n"
-"        background-color: #414956;\n"
-"        color: white;\n"
-"    }\n"
-"    QPushButton:pressed {\n"
-"        background-color: red;\n"
-"        background-color: #1C1C1C;\n"
-"        margin: 1 px;\n"
-"        margin-left: 2px;\n"
-"        margin-right: 2px;\n"
-"\n"
-"    }")
-        icon = QIcon()
-        icon.addFile(u":/icons/help_24dp_9D9D9D_FILL0_wght400_GRAD0_opsz24.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
-        self.online_help_loading_ediotor_button.setIcon(icon)
-        self.online_help_loading_ediotor_button.setIconSize(QSize(20, 20))
-
-        self.horizontalLayout_2.addWidget(self.online_help_loading_ediotor_button)
-
-        self.online_help_soundevent_editor_button = QPushButton(self.frame_2)
-        self.online_help_soundevent_editor_button.setObjectName(u"online_help_soundevent_editor_button")
-        self.online_help_soundevent_editor_button.setStyleSheet(u"\n"
-"    /* QPushButton default and hover styles */\n"
-"    QPushButton {\n"
-"\n"
-"        font: 600 10pt \"Segoe UI\";\n"
-"	\n"
-"\n"
-"        border: 2px solid black;\n"
-"        border-radius: 4px;\n"
-"        border-color: rgba(80, 80, 80, 255);\n"
-"        height:22px;\n"
-"        padding-top: 2px;\n"
-"        padding-bottom:2px;\n"
-"        padding-left: 4px;\n"
-"        padding-right: 4px;\n"
-"        color: #E3E3E3;\n"
-"        background-color: #1C1C1C;\n"
-"    }\n"
-"    QPushButton:hover {\n"
-"        background-color: #414956;\n"
-"        color: white;\n"
-"    }\n"
-"    QPushButton:pressed {\n"
-"        background-color: red;\n"
-"        background-color: #1C1C1C;\n"
-"        margin: 1 px;\n"
-"        margin-left: 2px;\n"
-"        margin-right: 2px;\n"
-"\n"
-"    }")
-        self.online_help_soundevent_editor_button.setIcon(icon)
-        self.online_help_soundevent_editor_button.setIconSize(QSize(20, 20))
-
-        self.horizontalLayout_2.addWidget(self.online_help_soundevent_editor_button)
-
-        self.online_help_batchcreator_button = QPushButton(self.frame_2)
-        self.online_help_batchcreator_button.setObjectName(u"online_help_batchcreator_button")
-        self.online_help_batchcreator_button.setStyleSheet(u"\n"
-"    /* QPushButton default and hover styles */\n"
-"    QPushButton {\n"
-"\n"
-"        font: 600 10pt \"Segoe UI\";\n"
-"	\n"
-"\n"
-"        border: 2px solid black;\n"
-"        border-radius: 4px;\n"
-"        border-color: rgba(80, 80, 80, 255);\n"
-"        height:22px;\n"
-"        padding-top: 2px;\n"
-"        padding-bottom:2px;\n"
-"        padding-left: 4px;\n"
-"        padding-right: 4px;\n"
-"        color: #E3E3E3;\n"
-"        background-color: #1C1C1C;\n"
-"    }\n"
-"    QPushButton:hover {\n"
-"        background-color: #414956;\n"
-"        color: white;\n"
-"    }\n"
-"    QPushButton:pressed {\n"
-"        background-color: red;\n"
-"        background-color: #1C1C1C;\n"
-"        margin: 1 px;\n"
-"        margin-left: 2px;\n"
-"        margin-right: 2px;\n"
-"\n"
-"    }")
-        self.online_help_batchcreator_button.setIcon(icon)
-        self.online_help_batchcreator_button.setIconSize(QSize(20, 20))
-
-        self.horizontalLayout_2.addWidget(self.online_help_batchcreator_button)
-
-
-        self.verticalLayout.addWidget(self.frame_2)
-
         self.frame_3 = QFrame(documentation_dialog)
         self.frame_3.setObjectName(u"frame_3")
+        self.frame_3.setMaximumSize(QSize(16777215, 44))
         self.frame_3.setFrameShape(QFrame.StyledPanel)
         self.frame_3.setFrameShadow(QFrame.Raised)
         self.horizontalLayout_3 = QHBoxLayout(self.frame_3)
@@ -259,16 +137,55 @@ class Ui_documentation_dialog(object):
 "        margin-right: 2px;\n"
 "\n"
 "    }")
-        icon1 = QIcon()
-        icon1.addFile(u":/icons/all_match_24dp_9D9D9D_FILL0_wght400_GRAD0_opsz24.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
-        self.report_a_bug_button.setIcon(icon1)
+        icon = QIcon()
+        icon.addFile(u":/icons/all_match_24dp_9D9D9D_FILL0_wght400_GRAD0_opsz24.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.report_a_bug_button.setIcon(icon)
         self.report_a_bug_button.setIconSize(QSize(20, 20))
 
         self.horizontalLayout_3.addWidget(self.report_a_bug_button)
 
-        self.github_page_button = QPushButton(self.frame_3)
-        self.github_page_button.setObjectName(u"github_page_button")
-        self.github_page_button.setStyleSheet(u"\n"
+        self.open_documentation_button = QPushButton(self.frame_3)
+        self.open_documentation_button.setObjectName(u"open_documentation_button")
+        self.open_documentation_button.setStyleSheet(u"\n"
+"    /* QPushButton default and hover styles */\n"
+"    QPushButton {\n"
+"\n"
+"        font: 600 10pt \"Segoe UI\";\n"
+"	\n"
+"\n"
+"        border: 2px solid black;\n"
+"        border-radius: 4px;\n"
+"        border-color: rgba(80, 80, 80, 255);\n"
+"        height:22px;\n"
+"        padding-top: 2px;\n"
+"        padding-bottom:2px;\n"
+"        padding-left: 4px;\n"
+"        padding-right: 4px;\n"
+"        color: #E3E3E3;\n"
+"        background-color: #1C1C1C;\n"
+"    }\n"
+"    QPushButton:hover {\n"
+"        background-color: #414956;\n"
+"        color: white;\n"
+"    }\n"
+"    QPushButton:pressed {\n"
+"        background-color: red;\n"
+"        background-color: #1C1C1C;\n"
+"        margin: 1 px;\n"
+"        margin-left: 2px;\n"
+"        margin-right: 2px;\n"
+"\n"
+"    }")
+        icon1 = QIcon()
+        icon1.addFile(u":/icons/developer_guide_16dp_9D9D9D_FILL0_wght400_GRAD0_opsz20.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.open_documentation_button.setIcon(icon1)
+        self.open_documentation_button.setIconSize(QSize(20, 20))
+
+        self.horizontalLayout_3.addWidget(self.open_documentation_button)
+
+        self.request_a_new_feature_button = QPushButton(self.frame_3)
+        self.request_a_new_feature_button.setObjectName(u"request_a_new_feature_button")
+        self.request_a_new_feature_button.setStyleSheet(u"\n"
 "    /* QPushButton default and hover styles */\n"
 "    QPushButton {\n"
 "\n"
@@ -300,10 +217,10 @@ class Ui_documentation_dialog(object):
 "    }")
         icon2 = QIcon()
         icon2.addFile(u":/icons/public_16dp_9D9D9D_FILL0_wght400_GRAD0_opsz20.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
-        self.github_page_button.setIcon(icon2)
-        self.github_page_button.setIconSize(QSize(20, 20))
+        self.request_a_new_feature_button.setIcon(icon2)
+        self.request_a_new_feature_button.setIconSize(QSize(20, 20))
 
-        self.horizontalLayout_3.addWidget(self.github_page_button)
+        self.horizontalLayout_3.addWidget(self.request_a_new_feature_button)
 
 
         self.verticalLayout.addWidget(self.frame_3)
@@ -321,10 +238,8 @@ class Ui_documentation_dialog(object):
         self.label_3.setText(QCoreApplication.translate("documentation_dialog", u"Use tooltips for understanding the program: \n"
 "For almost all buttons, an explanation appears when you hold your cursor over a button.", None))
         self.label_4.setText("")
-        self.online_help_loading_ediotor_button.setText(QCoreApplication.translate("documentation_dialog", u"Loading Editor", None))
-        self.online_help_soundevent_editor_button.setText(QCoreApplication.translate("documentation_dialog", u"SoundEvent Editor", None))
-        self.online_help_batchcreator_button.setText(QCoreApplication.translate("documentation_dialog", u"BatchCreator", None))
         self.report_a_bug_button.setText(QCoreApplication.translate("documentation_dialog", u"Report a bug", None))
-        self.github_page_button.setText(QCoreApplication.translate("documentation_dialog", u"Github Page", None))
+        self.open_documentation_button.setText(QCoreApplication.translate("documentation_dialog", u"Documentation", None))
+        self.request_a_new_feature_button.setText(QCoreApplication.translate("documentation_dialog", u"Request a feature", None))
     # retranslateUi
 
