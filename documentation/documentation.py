342be092c921f2d1e254e025f93e5719565620f0
dertwist <45437750+dertwist@users.noreply.github.com>
2024-09-07 12:58:03 +0200

    Documentation update

--- documentation/documentation.py
+++ documentation/documentation.py
@@ -9,25 +9,13 @@ class Documentation_Dialog(QDialog):
         self.ui.setupUi(self)
         self.ui.version.setText(f"Version: {version}")
         self.ui.report_a_bug_button.clicked.connect(self.open_report_bug_link)
-        self.ui.github_page_button.clicked.connect(self.open_github_page_link)
-        self.ui.online_help_batchcreator_button.clicked.connect(self.open_online_help_batchcreator_link)
-        self.ui.online_help_soundevent_editor_button.clicked.connect(self.open_online_help_soundevent_editor_link)
-        self.ui.online_help_loading_ediotor_button.clicked.connect(self.open_online_help_loading_ediotor)
+        self.ui.request_a_new_feature_button.clicked.connect(self.open_request_a_new_feature)
+        self.ui.open_documentation_button.clicked.connect(self.open_documentation)
 
     def open_report_bug_link(self):
         QDesktopServices.openUrl(QUrl("https://discord.gg/mMaub4jCBa"))
 
-    def open_github_page_link(self):
-        QDesktopServices.openUrl(QUrl("https://github.com/dertwist/Hammer5Tools"))
-
-    def open_online_help_batchcreator_link(self):
-        QDesktopServices.openUrl(
-            QUrl("https://fish-banana-d65.notion.site/BatchCreator-df8191edbba3490c988e00d820c9419e?pvs=4"))
-
-    def open_online_help_soundevent_editor_link(self):
-        QDesktopServices.openUrl(
-            QUrl("https://fish-banana-d65.notion.site/SoundEvent-Editor-32db6d9cb21e452abfaabf61988c9d54?pvs=4"))
-
-    def open_online_help_loading_ediotor(self):
-        QDesktopServices.openUrl(
-            QUrl("https://fish-banana-d65.notion.site/Loading-Editor-1dab2db30c404510aaa895ca834b14f3?pvs=4"))
+    def open_request_a_new_feature(self):
+        QDesktopServices.openUrl(QUrl("https://discord.gg/xygJgM7ZyP"))
+    def open_documentation(self):
+        QDesktopServices.openUrl(QUrl("https://twist-1.gitbook.io/hammer5tools"))
