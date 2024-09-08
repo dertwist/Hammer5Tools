f443a89a9ddb8e70ec2adccf360f889e3362ebd0
dertwist <45437750+dertwist@users.noreply.github.com>
2024-09-08 15:29:43 +0200

    Minor

--- smartprop_editor/variables/int.py
+++ smartprop_editor/variables/int.py
@@ -1,13 +1,6 @@
 from smartprop_editor.variables.ui_int import Ui_Widget
 
 from PySide6.QtWidgets import QWidget
-from PySide6.QtCore import Qt
-from soudevent_editor.properties.property_actions import PropertyActions
-from PySide6.QtWidgets import QMenu, QApplication
-from PySide6.QtCore import Qt, QMimeData
-from PySide6.QtGui import QCursor, QDrag, QAction
-import ast
-
 
 class Var_class_Int(QWidget):
     def __init__(self, default, min, max ):
@@ -16,13 +9,13 @@ class Var_class_Int(QWidget):
         self.ui.setupUi(self)
         self.setAcceptDrops(True)
         self.ui.min_checkBox.clicked.connect(lambda: self.checkbox_setEnabled(checkbox=self.ui.min_checkBox, spinbox=self.ui.min_spinBox))
+        self.ui.max_checkBox.clicked.connect(lambda: self.checkbox_setEnabled(checkbox=self.ui.max_checkBox, spinbox=self.ui.max_spinBox))
         print(default)
         if default == None:
             self.default = 0
         else:
             self.default = float(default)
         if min == None:
-            print('Min None')
             self.ui.min_checkBox.setChecked(False)
             self.ui.min_spinBox.setEnabled(False)
         else:
@@ -30,7 +23,6 @@ class Var_class_Int(QWidget):
             self.ui.min_checkBox.setChecked(True)
             self.ui.min_spinBox.setValue(self.min)
         if max == None:
-            print('Min None')
             self.ui.max_checkBox.setChecked(False)
             self.ui.max_spinBox.setEnabled(False)
         else:
