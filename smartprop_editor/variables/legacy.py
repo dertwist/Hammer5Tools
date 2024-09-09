ce1c1d5551acd505f55f6d507e1a3b5b57b35bbe
dertwist <45437750+dertwist@users.noreply.github.com>
2024-09-09 15:48:23 +0200

    legacy variable

--- smartprop_editor/variables/legacy.py
+++ smartprop_editor/variables/legacy.py
@@ -1,18 +1,27 @@
 from smartprop_editor.variables.ui_legacy import Ui_Widget
 
 from PySide6.QtWidgets import QWidget
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QMenu, QApplication
-from PySide6.QtCore import Qt, QMimeData
-from PySide6.QtGui import QCursor, QDrag, QAction
-import ast
+from PySide6.QtCore import Signal
 
 
 class Var_class_legacy(QWidget):
-    def __init__(self, var_value):
+    edited = Signal(str, str, str, str)
+    def __init__(self, default, min, max, model):
         super().__init__()
         self.ui = Ui_Widget()
         self.ui.setupUi(self)
         self.setAcceptDrops(True)
-        self.var_value = var_value
-        self.ui.value.setText(str(var_value))
+        self.min = None
+        self.max = None
+        self.model = None
+        if default == None:
+            self.default = ''
+        else:
+            self.default = str(default)
+        self.ui.value.setText(str(self.default))
+        self.ui.value.textChanged.connect(self.on_changed)
+
+
+    def on_changed(self):
+        self.default = self.ui.value.text()
+        self.edited.emit(self.default, self.min, self.max, str(self.model))
