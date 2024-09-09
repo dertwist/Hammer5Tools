23290d5f4e98b7a21dd60f2056d263d3fd6865f1
dertwist <45437750+dertwist@users.noreply.github.com>
2024-09-10 01:53:59 +0200

    Color var

---
+++ smartprop_editor/blank_vsmart.py
@@ -0,0 +1,11 @@
+blank_vsmart="""<!-- kv3 encoding:text:version{e21c7f3c-8a33-41c5-9977-a76d3a32aa0d} format:vrfunknown:version{5ab656f0-06de-478a-804e-489e82994fb5} -->
+{
+	generic_data_type = "CSmartPropRoot"
+	m_Children = 
+	[
+	]
+	m_Variables = 
+	[
+	]
+}
+"""
