02dc3e6af021a372e7cad67ae7220730f1a8d8f3
dertwist <45437750+dertwist@users.noreply.github.com>
2024-09-09 17:05:11 +0200

    convert h5t varialbes format to vsmart

--- smartprop_editor/variables/int.py
+++ smartprop_editor/variables/int.py
@@ -4,16 +4,42 @@ from PySide6.QtWidgets import QWidget
 from PySide6.QtCore import Signal
 
 class Var_class_Int(QWidget):
-    edited = Signal(float, float, float, str)
+    edited = Signal(int, int, int, str)
     def __init__(self, default, min, max, model):
         super().__init__()
         self.ui = Ui_Widget()
         self.ui.setupUi(self)
         self.setAcceptDrops(True)
         self.model = None
+
+
+
+
+        print(type(default), default, type(min), min, type(max), max, type(model), model)
+        if default == None:
+            self.default = int(0)
+        else:
+            self.default = int(default)
+        if min == None:
+            self.ui.min_checkBox.setChecked(False)
+            self.ui.min_spinBox.setEnabled(False)
+        else:
+            self.min = int(min)
+            print('Min:', self.min)
+            self.ui.min_checkBox.setChecked(True)
+            self.ui.min_spinBox.setValue(self.min)
+        if max == None:
+            self.ui.max_checkBox.setChecked(False)
+            self.ui.max_spinBox.setEnabled(False)
+        else:
+            self.max = int(max)
+            self.ui.max_checkBox.setChecked(True)
+            self.ui.max_spinBox.setValue(self.max)
+
+        self.ui.value_spinBox.setValue(self.default)
+
         self.ui.min_checkBox.clicked.connect(lambda: self.checkbox_setEnabled(checkbox=self.ui.min_checkBox, spinbox=self.ui.min_spinBox))
         self.ui.max_checkBox.clicked.connect(lambda: self.checkbox_setEnabled(checkbox=self.ui.max_checkBox, spinbox=self.ui.max_spinBox))
-
 
         self.ui.min_checkBox.stateChanged.connect(self.on_changed)
         self.ui.max_checkBox.stateChanged.connect(self.on_changed)
@@ -22,29 +48,6 @@ class Var_class_Int(QWidget):
         self.ui.min_spinBox.valueChanged.connect(self.on_changed)
         self.ui.max_spinBox.valueChanged.connect(self.on_changed)
         self.ui.value_spinBox.valueChanged.connect(self.on_changed)
-
-        if default == None:
-            self.default = int(0)
-        else:
-            self.default = float(default)
-        if min == None:
-            # self.min = 0
-            self.ui.min_checkBox.setChecked(False)
-            self.ui.min_spinBox.setEnabled(False)
-        else:
-            self.min = float(min)
-            self.ui.min_checkBox.setChecked(True)
-            self.ui.min_spinBox.setValue(self.min)
-        if max == None:
-            # self.max = 0
-            self.ui.max_checkBox.setChecked(False)
-            self.ui.max_spinBox.setEnabled(False)
-        else:
-            self.max = float(max)
-            self.ui.max_checkBox.setChecked(True)
-            self.ui.max_spinBox.setValue(self.max)
-
-        self.ui.value_spinBox.setValue(self.default)
     def checkbox_setEnabled(self, checkbox=None, spinbox=None):
         if checkbox.isChecked():
             spinbox.setEnabled(True)
