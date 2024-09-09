34703db487d6de2f40cb44b1542597a64279813f
dertwist <45437750+dertwist@users.noreply.github.com>
2024-09-10 01:53:59 +0200

    Color var

--- smartprop_editor/ui_main.py
+++ smartprop_editor/ui_main.py
@@ -86,7 +86,7 @@ class Ui_MainWindow(object):
         self.scrollArea.setWidgetResizable(True)
         self.scrollAreaWidgetContents = QWidget()
         self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 784, 700))
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 621, 700))
         self.scrollAreaWidgetContents.setStyleSheet(u"QWidget: {\n"
 "	border: 0px;\n"
 "}")
@@ -221,7 +221,7 @@ class Ui_MainWindow(object):
         self.variables_QscrollArea.setWidgetResizable(True)
         self.variables_scrollArea_widget = QWidget()
         self.variables_scrollArea_widget.setObjectName(u"variables_scrollArea_widget")
-        self.variables_scrollArea_widget.setGeometry(QRect(0, 0, 232, 434))
+        self.variables_scrollArea_widget.setGeometry(QRect(0, 0, 395, 434))
         self.variables_scrollArea_widget.setStyleSheet(u"")
         self.verticalLayout_2 = QVBoxLayout(self.variables_scrollArea_widget)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
@@ -444,6 +444,45 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_10.addLayout(self.explorer_layout)
 
+        self.compile_all_button = QPushButton(self.explorer_layout_widget)
+        self.compile_all_button.setObjectName(u"compile_all_button")
+        self.compile_all_button.setStyleSheet(u"\n"
+"    /* QPushButton default and hover styles */\n"
+"    QPushButton {\n"
+"\n"
+"        font: 600 10pt \"Segoe UI\";\n"
+"	\n"
+"\n"
+"        border: 2px solid black;\n"
+"        border-radius: 4px;\n"
+"        border-color: rgba(80, 80, 80, 255);\n"
+"        height:22px;\n"
+"        padding-top: 2px;\n"
+"        padding-bottom:2px;\n"
+"        padding-left: 4px;\n"
+"        padding-right: 4px;\n"
+"        color: #E3E3E3;\n"
+"        background-color: #1C1C1C;\n"
+"    }\n"
+"    QPushButton:hover {\n"
+"        background-color: #414956;\n"
+"        color: white;\n"
+"    }\n"
+"    QPushButton:pressed {\n"
+"        background-color: red;\n"
+"        background-color: #1C1C1C;\n"
+"        margin: 1 px;\n"
+"        margin-left: 2px;\n"
+"        margin-right: 2px;\n"
+"\n"
+"    }")
+        icon8 = QIcon()
+        icon8.addFile(u":/icons/logout_24dp_9D9D9D_FILL0_wght400_GRAD0_opsz24.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.compile_all_button.setIcon(icon8)
+        self.compile_all_button.setIconSize(QSize(24, 24))
+
+        self.verticalLayout_10.addWidget(self.compile_all_button)
+
         self.frame_4 = QFrame(self.explorer_layout_widget)
         self.frame_4.setObjectName(u"frame_4")
         self.frame_4.setMaximumSize(QSize(16777215, 32))
@@ -521,9 +560,9 @@ class Ui_MainWindow(object):
 "        margin-right: 2px;\n"
 "\n"
 "    }")
-        icon8 = QIcon()
-        icon8.addFile(u":/icons/file_open_16dp_9D9D9D_FILL0_wght400_GRAD0_opsz20.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
-        self.open_file_button.setIcon(icon8)
+        icon9 = QIcon()
+        icon9.addFile(u":/icons/file_open_16dp_9D9D9D_FILL0_wght400_GRAD0_opsz20.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.open_file_button.setIcon(icon9)
         self.open_file_button.setIconSize(QSize(24, 24))
 
         self.horizontalLayout_2.addWidget(self.open_file_button)
@@ -605,6 +644,13 @@ class Ui_MainWindow(object):
         self.variables_scroll_area_searchbar.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Search...", None))
         self.add_new_variable_button.setText(QCoreApplication.translate("MainWindow", u"...", None))
         self.dockWidget_10.setWindowTitle(QCoreApplication.translate("MainWindow", u"Explorer", None))
+#if QT_CONFIG(tooltip)
+        self.compile_all_button.setToolTip(QCoreApplication.translate("MainWindow", u"<html><head/><body><p>Ctrl + S</p></body></html>", None))
+#endif // QT_CONFIG(tooltip)
+        self.compile_all_button.setText(QCoreApplication.translate("MainWindow", u"Compile All", None))
+#if QT_CONFIG(shortcut)
+        self.compile_all_button.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+R", None))
+#endif // QT_CONFIG(shortcut)
 #if QT_CONFIG(tooltip)
         self.cerate_file_button.setToolTip(QCoreApplication.translate("MainWindow", u"<html><head/><body><p>Ctrl + N</p></body></html>", None))
 #endif // QT_CONFIG(tooltip)
