285af6650a8adba7aab0ebfa8fbfbe28d0c5c7bd
dertwist <45437750+dertwist@users.noreply.github.com>
2024-09-09 19:47:16 +0200

    class properties update

--- smartprop_editor/properties.py
+++ smartprop_editor/properties.py
@@ -1,12 +1,49 @@
+from PySide6.QtWidgets import QTreeWidgetItem, QTreeWidget
+import ast
+from PySide6.QtCore import Qt
 class Properties:
-    def __init__(self, tree, data=None):
+    def __init__(self, tree=QTreeWidget, data=None):
         self.tree = tree
-        self.data = data
+        self.data = ast.literal_eval(data)
+        print(type(data), data)
         for index in range(tree.topLevelItemCount()):
             item = tree.topLevelItem(index)
             if item.text(0) == 'Modifiers':
-                print('Found item with name Modifiers:', item)
+                self.clear_children(item)
+                if 'm_Modifiers' in self.data:
+                    for modifier in self.data['m_Modifiers']:
+                        modifier_item = self.new_item(item, modifier['_class'], '')
+                        for key, value in modifier.items():
+                            self.new_item(modifier_item, key, value, edit=True)
+
             if item.text(0) == 'ClassProperties':
-                print('Found item with name ClassProperties:', item)
+                self.clear_children(item)
+                for key, value in self.data.items():
+                    print('key', key)
+                    if key == 'm_Modifiers':
+                        pass
+                    elif key == 'm_SelectionCriteria':
+                        pass
+                    else:
+                        self.new_item(item, key, value, edit=True)
+
             if item.text(0) == 'SelectionCriteria':
-                print('Found item with name SelectionCriteria:', item)
+                self.clear_children(item)
+                if 'm_SelectionCriteria' in self.data:
+                    for modifier in self.data['m_SelectionCriteria']:
+                        modifier_item = self.new_item(item, modifier['_class'].replace('CSmartPropSelectionCriteria_', ''), '')
+                        for key, value in modifier.items():
+                            self.new_item(modifier_item, key, value, edit=True)
+
+    def clear_children(self, item):
+        while item.childCount() > 0:
+            child = item.child(0)
+            item.removeChild(child)
+    def new_item(self, item, name, value, edit=False):
+        new_child_item = QTreeWidgetItem(item)
+        new_child_item.setText(0, str(name))
+        new_child_item.setText(1, str(value))
+        if edit:
+            new_child_item.setFlags(new_child_item.flags() | Qt.ItemIsEditable)
+        item.addChild(new_child_item)
+        return new_child_item
