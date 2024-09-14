d49194936264ef49a19be00e28387d10c50a70ed
dertwist <45437750+dertwist@users.noreply.github.com>
2024-09-15 01:34:40 +0200

    hotkey editor begin

---
+++ hotkey_editor/objects.py
@@ -0,0 +1,59 @@
+actions = {
+    "ToolSelection": ["BakeTransform", "MergeMeshesByEdge", "ReplaceSelectionWithInstance", "RandomizeConfiguration"],
+    "Camera": [
+        "MoveCameraForward3D", "MoveCameraBackward3D", "MoveCameraLeft3D", "MoveCameraRight3D",
+        "MoveCameraUp3D", "MoveCameraDown3D", "PitchCameraUp", "PitchCameraDown",
+        "YawCameraLeft", "YawCameraRight", "IncreaseMoveSpeed", "DecreaseMoveSpeed",
+        "ResetMoveSpeed", "ZoomCameraIn", "ZoomCameraOut", "ZoomAllCamerasIn", "ZoomAllCamerasOut",
+        "MouseControlCamera", "MouseControlCamera3D", "MouseControlCamera2D", "MouseControlCamera3D_Toggle",
+        "HorizontalStrafeCamera", "VerticalStrafeCamera", "ToggleLockCameraHeight", "ZoomForwardBack3D",
+        "ZoomForwardBack2D", "PanCamera3D", "RotateAboutTarget3D", "MouseLook3D", "SnapCameraToSelection",
+        "CameraLookAtSelection"
+    ],
+    "MapView": ["ModelTraceLift", "RefreshRaytracedScene", "WriteRaytracedImage"],
+    "PaneContainer": [
+        "3D_Wireframe", "3D_HighContrastLighting", "3D_DirectLightingOnly", "3D_IndirectLightingOnly",
+        "3D_DirectPlusAmbientLighting", "3D_TagVis", "EngineView", "AssetView", "ObjectProperties",
+        "EntityIO", "TerrainGraph", "RTX_Visualization", "ToggleLightingOnly", "ToggleParticles", "ToggleFog",
+        "ToggleSkybox", "ToggleReflections", "ToggleSSAO", "ToggleWorklight", "ToggleFOW", "ToggleWaterFlow",
+        "ToggleFogFlow", "ToggleResolutionGate", "ToggleToggleGridOfThirds", "ToggleHighlightDeprecated",
+        "CreateFloatingCopy"
+    ],
+    "HammerEditorSession": [
+        "PasteKeys", "ConvertObjectsToModel", "ConvertObjectsToSmartProp", "ConvertToStaticCables",
+        "ConcertCablesToMesh", "SeperatePathSegments", "SeperatePathNodes", "MergePaths", "ReversePaths",
+        "ExportParticleSnapShot", "SendSelectedToModo", "InsertNewImagePlane", "RemovePaintData", "RemovePoseData",
+        "AttachObjectToView", "DetachObjectFromView", "SetHierarchyFromCurrentTransform", "SetTransformFromHierarchy",
+        "ToggleNaveAlwaysVisible", "ToggleLightingPreviewLock", "ToggleCordonsVisible", "SetManipulationModeTranslate",
+        "SetManipulationModeRotate", "SetManipulationModeScale", "SetManipulationModePivot", "ToolPolygon",
+        "ToolAssetSpray", "ToolStaticOVerlay", "ToolTextureProjection", "HideUnselected", "ToggleEntityNames",
+        "ToggleShowLightOverlap", "AddNewKey", "UpdateToCurrentVersion", "ConvertToStaticOVerlay", "RemapBlendData",
+        "ToggleShow3DGrid", "ToggleShow2DGrid", "GotoCoords", "TogglePrefabVisibility", "TogglePrefabWireframColor",
+        "ImportPrefab", "CollapseAllPrefabs", "CollapseSelectedPrefabs", "CreatePrefabFromSelection",
+        "CreatePrefabFromSelectionCentered", "ConvertInstanceToPrefab", "ReplaceInstanceWithPrefab", "EditSelectedPrefab",
+        "OpenPrefabs", "ReloadPrefabs", "SavePrefabs", "AddPrefabsToVersionControl", "CheckoutPrefabs",
+        "CreateWorldLayerFromSelection", "CreateTestNode", "CreateIsoSurface", "BakeAllLighting", "BakeAllCubemaps",
+        "ClearAllLighting", "BakeLightingOnSelection", "BakeCubemapsOnSelection", "ClearLightingOnSelection",
+        "ToggleBakedLightingUseRenderFarm", "CleanupUnusedBakeResources", "WriteCustomCubemap",
+        "EstimateLightingFromHDRSkybox", "ConvertToBarnDoorLights", "AddEdgeFadeToEnvProbes", "ShowRemapVertexDataDialog",
+        "ShowScreenshotDialog", "AlignCameraToSelection", "AlignSelectionToCamera", "LoadMapInEngine", "ReloadMapInEngine",
+        "LoadVisClusters", "DeleteVisClusters", "OpenAssetPaneDockWidget", "OpenMapViewPaneDockWidget", "SteamAudioPreviewScene",
+        "SteamAudioExportScene", "SteamAudioCreateProbeBox", "SteamAudioGenerateProbes", "SteamAudioBakeReverb",
+        "SteamAudioBakePaths", "ToggleSnapRotation"
+    ],
+    "HammerApp": [
+        "FileClose", "ClearRecentFiles", "VersionControlAdd", "VersionControlCheckIn", "VersionControlCheckOut",
+        "VersionControlCheckInAll", "VersionControlStatus", "ActivateNextEditorSession", "ActivatePreviousEditorSession",
+        "ReloadKeyBindings", "ResetLayout", "ToggleDisableUndoRedo", "ToggleTransformSpace", "ToggleTextureLock",
+        "ToggleTextureLockComponent", "ToggleSelectIntersecting", "ToggleDisplacementMapping", "ToggleFog", "EditReplace",
+        "CheckMapForProblems", "MapInformation", "EntityGallery", "NewMapFromTemplate", "SaveAsText", "ReloadFGDFiles",
+        "ImportFile", "ExportFile", "ExportSelected", "ToggleShowVisContributorsOnly", "ShowOutputsPopup", "ShowInputsPopup",
+        "ShowMapRelaysPopup", "PauseGame", "UnpauseGame", "ToggleModelAnimation", "ToggleForceLightsOn", "CycleViewDistance",
+        "ToggleShowGrass", "ToggleShowParticles", "ToggleShowLPVSampleGrid", "SetEditUVSet_All", "SetEditUVSet_UV1",
+        "SetEditUVSet_UV2"
+    ],
+    "GlobalHotkeys": [
+        "FocusChainDebug_More", "FocusChainDebug_Less", "FocusChainDebug_Forward", "FocusChainDebug_Back",
+        "FocusChainDebug_Toggle"
+    ]
+}
