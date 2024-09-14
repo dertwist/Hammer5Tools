100fe9501865f4bc197e69adabb9be3ceff3e763
dertwist <45437750+dertwist@users.noreply.github.com>
2024-09-14 17:50:34 +0200

    basic completer

--- smartprop_editor/properties_classes/legacy.py
+++ smartprop_editor/properties_classes/legacy.py
@@ -1,13 +1,13 @@
 import ast
 
 from smartprop_editor.properties_classes.ui_legacy import Ui_Widget
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QCompleter
 from PySide6.QtCore import Signal
 
 
 class PropertyLegacy(QWidget):
     edited = Signal()
-    def __init__(self, value_class, value):
+    def __init__(self, value_class, value, variables_scrollArea):
         super().__init__()
         self.ui = Ui_Widget()
         self.ui.setupUi(self)
@@ -18,6 +18,13 @@ class PropertyLegacy(QWidget):
         self.ui.value.setText(str(self.value))
         self.ui.value_label.setText(str(self.value_class))
         self.ui.value.textChanged.connect(self.on_changed)
+
+        self.variables_scrollArea = variables_scrollArea
+        variables = self.get_variables()
+        print(variables)
+        completer = QCompleter(variables)
+        self.ui.value.setCompleter(completer)
+
         self.change_value()
 
 
@@ -32,3 +39,11 @@ class PropertyLegacy(QWidget):
         except:
             pass
         self.value = {self.value_class: value}
+
+    def get_variables(self, search_term=None):
+        data_out = []
+        for i in range(self.variables_scrollArea.count()):
+            widget = self.variables_scrollArea.itemAt(i).widget()
+            if widget:
+                data_out.append(widget.name)
+        return data_out
