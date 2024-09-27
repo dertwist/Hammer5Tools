ae4caf9ff4a55c1644e14e5f03cc6f26dd0fb8b3
dertwist <45437750+dertwist@users.noreply.github.com>
2024-09-27 16:39:00 +0200

    version var update

--- main.py
+++ main.py
@@ -30,10 +30,10 @@ stop_discord_thread = threading.Event()
 
 LOCK_FILE = os.path.join(tempfile.gettempdir(), 'hammer5tools.lock')
 
-app_version = '2.1.7'
+app_version = '2.1.8'
 batchcreator_version = '1.2.2'
-soundevent_editor_version = '0.5.0'
-smartprop_editor_version = '0.7.1'
+soundevent_editor_version = '0.5.1'
+smartprop_editor_version = '0.7.2'
 
 
 import sys
