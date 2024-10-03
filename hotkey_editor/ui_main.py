f59a8530cb49660ae7c17c1eba8036b223e633e9
dertwist <45437750+dertwist@users.noreply.github.com>
2024-10-03 22:35:26 +0200

    hotkey editor ui update

--- hotkey_editor/ui_main.py
+++ hotkey_editor/ui_main.py
@@ -16,11 +16,11 @@ from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
     QIcon, QImage, QKeySequence, QLinearGradient,
     QPainter, QPalette, QPixmap, QRadialGradient,
     QTransform)
-from PySide6.QtWidgets import (QApplication, QComboBox, QDockWidget, QHBoxLayout,
+from PySide6.QtWidgets import (QApplication, QComboBox, QFrame, QHBoxLayout,
     QHeaderView, QKeySequenceEdit, QLabel, QLineEdit,
     QListWidget, QListWidgetItem, QMainWindow, QPushButton,
-    QSizePolicy, QTreeWidget, QTreeWidgetItem, QVBoxLayout,
-    QWidget)
+    QSizePolicy, QSplitter, QTreeWidget, QTreeWidgetItem,
+    QVBoxLayout, QWidget)
 import rc_resources
 
 class Ui_MainWindow(object):
@@ -38,143 +38,30 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName(u"centralwidget")
         self.verticalLayout_2 = QVBoxLayout(self.centralwidget)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.verticalLayout_2.setContentsMargins(9, 0, 9, 0)
-        self.label = QLabel(self.centralwidget)
-        self.label.setObjectName(u"label")
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.frame = QFrame(self.centralwidget)
+        self.frame.setObjectName(u"frame")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_5 = QVBoxLayout(self.frame)
+        self.verticalLayout_5.setObjectName(u"verticalLayout_5")
+        self.verticalLayout_5.setContentsMargins(0, 0, 0, 0)
+        self.splitter = QSplitter(self.frame)
+        self.splitter.setObjectName(u"splitter")
+        self.splitter.setOrientation(Qt.Horizontal)
+        self.splitter.setHandleWidth(8)
+        self.splitter.setChildrenCollapsible(False)
+        self.widget = QWidget(self.splitter)
+        self.widget.setObjectName(u"widget")
+        self.verticalLayout_4 = QVBoxLayout(self.widget)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.verticalLayout_4.setContentsMargins(0, 0, 0, 0)
+        self.label_3 = QLabel(self.widget)
+        self.label_3.setObjectName(u"label_3")
 
-        self.verticalLayout_2.addWidget(self.label)
+        self.verticalLayout_4.addWidget(self.label_3)
 
-        self.treeWidget = QTreeWidget(self.centralwidget)
-        __qtreewidgetitem = QTreeWidgetItem()
-        __qtreewidgetitem.setText(0, u"Context");
-        self.treeWidget.setHeaderItem(__qtreewidgetitem)
-        self.treeWidget.setObjectName(u"treeWidget")
-        self.treeWidget.header().setMinimumSectionSize(150)
-        self.treeWidget.header().setDefaultSectionSize(200)
-
-        self.verticalLayout_2.addWidget(self.treeWidget)
-
-        self.horizontalLayout_4 = QHBoxLayout()
-        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
-        self.lineEdit = QLineEdit(self.centralwidget)
-        self.lineEdit.setObjectName(u"lineEdit")
-
-        self.horizontalLayout_4.addWidget(self.lineEdit)
-
-        self.lineEdit_3 = QLineEdit(self.centralwidget)
-        self.lineEdit_3.setObjectName(u"lineEdit_3")
-
-        self.horizontalLayout_4.addWidget(self.lineEdit_3)
-
-        self.lineEdit_2 = QLineEdit(self.centralwidget)
-        self.lineEdit_2.setObjectName(u"lineEdit_2")
-
-        self.horizontalLayout_4.addWidget(self.lineEdit_2)
-
-
-        self.verticalLayout_2.addLayout(self.horizontalLayout_4)
-
-        self.horizontalLayout_6 = QHBoxLayout()
-        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
-        self.keySequenceEdit = QKeySequenceEdit(self.centralwidget)
-        self.keySequenceEdit.setObjectName(u"keySequenceEdit")
-
-        self.horizontalLayout_6.addWidget(self.keySequenceEdit)
-
-        self.cerate_file_button_4 = QPushButton(self.centralwidget)
-        self.cerate_file_button_4.setObjectName(u"cerate_file_button_4")
-        self.cerate_file_button_4.setMinimumSize(QSize(128, 0))
-        self.cerate_file_button_4.setStyleSheet(u"\n"
-"    /* QPushButton default and hover styles */\n"
-"    QPushButton {\n"
-"\n"
-"        font: 580 9pt \"Segoe UI\";\n"
-"	\n"
-"\n"
-"        border: 2px solid black;\n"
-"        border-radius: 2px;\n"
-"        border-color: rgba(80, 80, 80, 255);\n"
-"        height:22px;\n"
-"        padding-top: 2px;\n"
-"        padding-bottom:2px;\n"
-"        padding-left: 4px;\n"
-"        padding-right: 4px;\n"
-"        color: #E3E3E3;\n"
-"        background-color: #1C1C1C;\n"
-"    }\n"
-"    QPushButton:hover {\n"
-"        background-color: #414956;\n"
-"        color: white;\n"
-"    }\n"
-"    QPushButton:pressed {\n"
-"        background-color: red;\n"
-"        background-color: #1C1C1C;\n"
-"        margin: 1 px;\n"
-"        margin-left: 2px;\n"
-"        margin-right: 2px;\n"
-"\n"
-"    }")
-        icon = QIcon()
-        icon.addFile(u":/icons/save_16dp_9D9D9D_FILL0_wght400_GRAD0_opsz20.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
-        self.cerate_file_button_4.setIcon(icon)
-        self.cerate_file_button_4.setIconSize(QSize(20, 20))
-
-        self.horizontalLayout_6.addWidget(self.cerate_file_button_4)
-
-        self.cerate_file_button_3 = QPushButton(self.centralwidget)
-        self.cerate_file_button_3.setObjectName(u"cerate_file_button_3")
-        self.cerate_file_button_3.setMinimumSize(QSize(256, 0))
-        self.cerate_file_button_3.setStyleSheet(u"\n"
-"    /* QPushButton default and hover styles */\n"
-"    QPushButton {\n"
-"\n"
-"        font: 580 9pt \"Segoe UI\";\n"
-"	\n"
-"\n"
-"        border: 2px solid black;\n"
-"        border-radius: 2px;\n"
-"        border-color: rgba(80, 80, 80, 255);\n"
-"        height:22px;\n"
-"        padding-top: 2px;\n"
-"        padding-bottom:2px;\n"
-"        padding-left: 4px;\n"
-"        padding-right: 4px;\n"
-"        color: #E3E3E3;\n"
-"        background-color: #1C1C1C;\n"
-"    }\n"
-"    QPushButton:hover {\n"
-"        background-color: #414956;\n"
-"        color: white;\n"
-"    }\n"
-"    QPushButton:pressed {\n"
-"        background-color: red;\n"
-"        background-color: #1C1C1C;\n"
-"        margin: 1 px;\n"
-"        margin-left: 2px;\n"
-"        margin-right: 2px;\n"
-"\n"
-"    }")
-        icon1 = QIcon()
-        icon1.addFile(u":/icons/all_match_24dp_9D9D9D_FILL0_wght400_GRAD0_opsz24.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
-        self.cerate_file_button_3.setIcon(icon1)
-        self.cerate_file_button_3.setIconSize(QSize(20, 20))
-
-        self.horizontalLayout_6.addWidget(self.cerate_file_button_3)
-
-
-        self.verticalLayout_2.addLayout(self.horizontalLayout_6)
-
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.dockWidget = QDockWidget(MainWindow)
-        self.dockWidget.setObjectName(u"dockWidget")
-        self.dockWidget.setFeatures(QDockWidget.DockWidgetMovable)
-        self.dockWidgetContents = QWidget()
-        self.dockWidgetContents.setObjectName(u"dockWidgetContents")
-        self.dockWidgetContents.setMinimumSize(QSize(400, 0))
-        self.verticalLayout = QVBoxLayout(self.dockWidgetContents)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.comboBox = QComboBox(self.dockWidgetContents)
+        self.comboBox = QComboBox(self.widget)
         self.comboBox.addItem("")
         self.comboBox.setObjectName(u"comboBox")
         self.comboBox.setStyleSheet(u"QMenu {\n"
@@ -339,16 +226,22 @@ class Ui_MainWindow(object):
 "    outline: none; /* Remove outline */\n"
 "}")
 
-        self.verticalLayout.addWidget(self.comboBox)
+        self.verticalLayout_4.addWidget(self.comboBox)
 
-        self.presets_list = QListWidget(self.dockWidgetContents)
+        self.presets_list = QListWidget(self.widget)
         self.presets_list.setObjectName(u"presets_list")
 
-        self.verticalLayout.addWidget(self.presets_list)
+        self.verticalLayout_4.addWidget(self.presets_list)
+
+        self.current_preset = QLineEdit(self.widget)
+        self.current_preset.setObjectName(u"current_preset")
+        self.current_preset.setReadOnly(True)
+
+        self.verticalLayout_4.addWidget(self.current_preset)
 
         self.horizontalLayout_3 = QHBoxLayout()
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.open_file_as_button_2 = QPushButton(self.dockWidgetContents)
+        self.open_file_as_button_2 = QPushButton(self.widget)
         self.open_file_as_button_2.setObjectName(u"open_file_as_button_2")
         self.open_file_as_button_2.setStyleSheet(u"\n"
 "    /* QPushButton default and hover styles */\n"
@@ -380,14 +273,14 @@ class Ui_MainWindow(object):
 "        margin-right: 2px;\n"
 "\n"
 "    }")
-        icon2 = QIcon()
-        icon2.addFile(u":/icons/check_24dp_9D9D9D_FILL0_wght400_GRAD0_opsz24.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
-        self.open_file_as_button_2.setIcon(icon2)
+        icon = QIcon()
+        icon.addFile(u":/icons/check_24dp_9D9D9D_FILL0_wght400_GRAD0_opsz24.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.open_file_as_button_2.setIcon(icon)
         self.open_file_as_button_2.setIconSize(QSize(20, 20))
 
         self.horizontalLayout_3.addWidget(self.open_file_as_button_2)
 
-        self.cerate_file_button = QPushButton(self.dockWidgetContents)
+        self.cerate_file_button = QPushButton(self.widget)
         self.cerate_file_button.setObjectName(u"cerate_file_button")
         self.cerate_file_button.setStyleSheet(u"\n"
 "    /* QPushButton default and hover styles */\n"
@@ -419,18 +312,155 @@ class Ui_MainWindow(object):
 "        margin-right: 2px;\n"
 "\n"
 "    }")
-        icon3 = QIcon()
-        icon3.addFile(u":/icons/add_24dp_9D9D9D_FILL0_wght400_GRAD0_opsz24.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
-        self.cerate_file_button.setIcon(icon3)
+        icon1 = QIcon()
+        icon1.addFile(u":/icons/add_24dp_9D9D9D_FILL0_wght400_GRAD0_opsz24.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.cerate_file_button.setIcon(icon1)
         self.cerate_file_button.setIconSize(QSize(20, 20))
 
         self.horizontalLayout_3.addWidget(self.cerate_file_button)
 
 
-        self.verticalLayout.addLayout(self.horizontalLayout_3)
+        self.verticalLayout_4.addLayout(self.horizontalLayout_3)
 
-        self.dockWidget.setWidget(self.dockWidgetContents)
-        MainWindow.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, self.dockWidget)
+        self.splitter.addWidget(self.widget)
+        self.widget1 = QWidget(self.splitter)
+        self.widget1.setObjectName(u"widget1")
+        self.verticalLayout_3 = QVBoxLayout(self.widget1)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.label = QLabel(self.widget1)
+        self.label.setObjectName(u"label")
+
+        self.verticalLayout_3.addWidget(self.label)
+
+        self.treeWidget = QTreeWidget(self.widget1)
+        __qtreewidgetitem = QTreeWidgetItem()
+        __qtreewidgetitem.setText(0, u"Context");
+        self.treeWidget.setHeaderItem(__qtreewidgetitem)
+        self.treeWidget.setObjectName(u"treeWidget")
+        self.treeWidget.header().setMinimumSectionSize(150)
+        self.treeWidget.header().setDefaultSectionSize(200)
+
+        self.verticalLayout_3.addWidget(self.treeWidget)
+
+        self.horizontalLayout_4 = QHBoxLayout()
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.lineEdit = QLineEdit(self.widget1)
+        self.lineEdit.setObjectName(u"lineEdit")
+
+        self.horizontalLayout_4.addWidget(self.lineEdit)
+
+        self.lineEdit_3 = QLineEdit(self.widget1)
+        self.lineEdit_3.setObjectName(u"lineEdit_3")
+
+        self.horizontalLayout_4.addWidget(self.lineEdit_3)
+
+        self.lineEdit_2 = QLineEdit(self.widget1)
+        self.lineEdit_2.setObjectName(u"lineEdit_2")
+
+        self.horizontalLayout_4.addWidget(self.lineEdit_2)
+
+
+        self.verticalLayout_3.addLayout(self.horizontalLayout_4)
+
+        self.horizontalLayout_6 = QHBoxLayout()
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+        self.keySequenceEdit = QKeySequenceEdit(self.widget1)
+        self.keySequenceEdit.setObjectName(u"keySequenceEdit")
+
+        self.horizontalLayout_6.addWidget(self.keySequenceEdit)
+
+        self.cerate_file_button_4 = QPushButton(self.widget1)
+        self.cerate_file_button_4.setObjectName(u"cerate_file_button_4")
+        self.cerate_file_button_4.setMinimumSize(QSize(128, 0))
+        self.cerate_file_button_4.setStyleSheet(u"\n"
+"    /* QPushButton default and hover styles */\n"
+"    QPushButton {\n"
+"\n"
+"        font: 580 9pt \"Segoe UI\";\n"
+"	\n"
+"\n"
+"        border: 2px solid black;\n"
+"        border-radius: 2px;\n"
+"        border-color: rgba(80, 80, 80, 255);\n"
+"        height:22px;\n"
+"        padding-top: 2px;\n"
+"        padding-bottom:2px;\n"
+"        padding-left: 4px;\n"
+"        padding-right: 4px;\n"
+"        color: #E3E3E3;\n"
+"        background-color: #1C1C1C;\n"
+"    }\n"
+"    QPushButton:hover {\n"
+"        background-color: #414956;\n"
+"        color: white;\n"
+"    }\n"
+"    QPushButton:pressed {\n"
+"        background-color: red;\n"
+"        background-color: #1C1C1C;\n"
+"        margin: 1 px;\n"
+"        margin-left: 2px;\n"
+"        margin-right: 2px;\n"
+"\n"
+"    }")
+        icon2 = QIcon()
+        icon2.addFile(u":/icons/save_16dp_9D9D9D_FILL0_wght400_GRAD0_opsz20.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.cerate_file_button_4.setIcon(icon2)
+        self.cerate_file_button_4.setIconSize(QSize(20, 20))
+
+        self.horizontalLayout_6.addWidget(self.cerate_file_button_4)
+
+        self.cerate_file_button_3 = QPushButton(self.widget1)
+        self.cerate_file_button_3.setObjectName(u"cerate_file_button_3")
+        self.cerate_file_button_3.setMinimumSize(QSize(256, 0))
+        self.cerate_file_button_3.setStyleSheet(u"\n"
+"    /* QPushButton default and hover styles */\n"
+"    QPushButton {\n"
+"\n"
+"        font: 580 9pt \"Segoe UI\";\n"
+"	\n"
+"\n"
+"        border: 2px solid black;\n"
+"        border-radius: 2px;\n"
+"        border-color: rgba(80, 80, 80, 255);\n"
+"        height:22px;\n"
+"        padding-top: 2px;\n"
+"        padding-bottom:2px;\n"
+"        padding-left: 4px;\n"
+"        padding-right: 4px;\n"
+"        color: #E3E3E3;\n"
+"        background-color: #1C1C1C;\n"
+"    }\n"
+"    QPushButton:hover {\n"
+"        background-color: #414956;\n"
+"        color: white;\n"
+"    }\n"
+"    QPushButton:pressed {\n"
+"        background-color: red;\n"
+"        background-color: #1C1C1C;\n"
+"        margin: 1 px;\n"
+"        margin-left: 2px;\n"
+"        margin-right: 2px;\n"
+"\n"
+"    }")
+        icon3 = QIcon()
+        icon3.addFile(u":/icons/all_match_24dp_9D9D9D_FILL0_wght400_GRAD0_opsz24.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.cerate_file_button_3.setIcon(icon3)
+        self.cerate_file_button_3.setIconSize(QSize(20, 20))
+
+        self.horizontalLayout_6.addWidget(self.cerate_file_button_3)
+
+
+        self.verticalLayout_3.addLayout(self.horizontalLayout_6)
+
+        self.splitter.addWidget(self.widget1)
+
+        self.verticalLayout_5.addWidget(self.splitter)
+
+
+        self.verticalLayout_2.addWidget(self.frame)
+
+        MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
 
@@ -442,6 +472,24 @@ class Ui_MainWindow(object):
         self.actionSave.setText(QCoreApplication.translate("MainWindow", u"Save", None))
         self.actionSave_as.setText(QCoreApplication.translate("MainWindow", u"Save as", None))
         self.actionOpen.setText(QCoreApplication.translate("MainWindow", u"Open", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Keybindings presets", None))
+        self.comboBox.setItemText(0, QCoreApplication.translate("MainWindow", u"Hammer", None))
+
+        self.current_preset.setText(QCoreApplication.translate("MainWindow", u"Current hotkey preset", None))
+#if QT_CONFIG(tooltip)
+        self.open_file_as_button_2.setToolTip(QCoreApplication.translate("MainWindow", u"<html><head/><body><p>Ctrl + O</p></body></html>", None))
+#endif // QT_CONFIG(tooltip)
+        self.open_file_as_button_2.setText(QCoreApplication.translate("MainWindow", u"Set current", None))
+#if QT_CONFIG(shortcut)
+        self.open_file_as_button_2.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+O", None))
+#endif // QT_CONFIG(shortcut)
+#if QT_CONFIG(tooltip)
+        self.cerate_file_button.setToolTip(QCoreApplication.translate("MainWindow", u"<html><head/><body><p>Ctrl + N</p></body></html>", None))
+#endif // QT_CONFIG(tooltip)
+        self.cerate_file_button.setText(QCoreApplication.translate("MainWindow", u"New preset", None))
+#if QT_CONFIG(shortcut)
+        self.cerate_file_button.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+N", None))
+#endif // QT_CONFIG(shortcut)
         self.label.setText(QCoreApplication.translate("MainWindow", u"Keybindings editor", None))
         ___qtreewidgetitem = self.treeWidget.headerItem()
         ___qtreewidgetitem.setText(2, QCoreApplication.translate("MainWindow", u"Input", None));
@@ -462,23 +510,6 @@ class Ui_MainWindow(object):
         self.cerate_file_button_3.setText(QCoreApplication.translate("MainWindow", u"Save and restart the editor", None))
 #if QT_CONFIG(shortcut)
         self.cerate_file_button_3.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+N", None))
-#endif // QT_CONFIG(shortcut)
-        self.dockWidget.setWindowTitle(QCoreApplication.translate("MainWindow", u"Keybindings presets", None))
-        self.comboBox.setItemText(0, QCoreApplication.translate("MainWindow", u"hammer", None))
-
-#if QT_CONFIG(tooltip)
-        self.open_file_as_button_2.setToolTip(QCoreApplication.translate("MainWindow", u"<html><head/><body><p>Ctrl + O</p></body></html>", None))
-#endif // QT_CONFIG(tooltip)
-        self.open_file_as_button_2.setText(QCoreApplication.translate("MainWindow", u"Set current", None))
-#if QT_CONFIG(shortcut)
-        self.open_file_as_button_2.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+O", None))
-#endif // QT_CONFIG(shortcut)
-#if QT_CONFIG(tooltip)
-        self.cerate_file_button.setToolTip(QCoreApplication.translate("MainWindow", u"<html><head/><body><p>Ctrl + N</p></body></html>", None))
-#endif // QT_CONFIG(tooltip)
-        self.cerate_file_button.setText(QCoreApplication.translate("MainWindow", u"New preset", None))
-#if QT_CONFIG(shortcut)
-        self.cerate_file_button.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+N", None))
 #endif // QT_CONFIG(shortcut)
     # retranslateUi
 
