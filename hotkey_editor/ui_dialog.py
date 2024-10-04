3f1242a602762f1fea86982f342770b62c592a4b
dertwist <45437750+dertwist@users.noreply.github.com>
2024-10-04 15:23:32 +0200

    HotkeyEditor dialog

---
+++ hotkey_editor/ui_dialog.py
@@ -0,0 +1,402 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'dialog.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QAbstractButton, QApplication, QCheckBox, QComboBox,
+    QDialog, QDialogButtonBox, QFrame, QHBoxLayout,
+    QLineEdit, QSizePolicy, QVBoxLayout, QWidget)
+import rc_resources
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(420, 128)
+        Dialog.setMinimumSize(QSize(420, 128))
+        Dialog.setMaximumSize(QSize(420, 128))
+        icon = QIcon()
+        icon.addFile(u":/icons/keyboard.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        Dialog.setWindowIcon(icon)
+        self.verticalLayout = QVBoxLayout(Dialog)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.frame = QFrame(Dialog)
+        self.frame.setObjectName(u"frame")
+        self.frame.setMaximumSize(QSize(16777215, 32))
+        self.frame.setFrameShape(QFrame.NoFrame)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_2 = QHBoxLayout(self.frame)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.select_from_list = QCheckBox(self.frame)
+        self.select_from_list.setObjectName(u"select_from_list")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.select_from_list.sizePolicy().hasHeightForWidth())
+        self.select_from_list.setSizePolicy(sizePolicy)
+        self.select_from_list.setStyleSheet(u"\n"
+"    /* QPushButton default and hover styles */\n"
+"    QCheckBox {\n"
+"\n"
+"        font: 580 9pt \"Segoe UI\";\n"
+"	\n"
+"\n"
+"        border: 2px solid black;\n"
+"        border-radius: 2px;\n"
+"        border-color: rgba(80, 80, 80, 255);\n"
+"        height:22px;\n"
+"        padding-top: 2px;\n"
+"        padding-bottom:2px;\n"
+"        padding-left: 4px;\n"
+"        padding-right: 4px;\n"
+"        color: #E3E3E3;\n"
+"        background-color: #1C1C1C;\n"
+"    }\n"
+"    QCheckBox:hover {\n"
+"        background-color: #414956;\n"
+"        color: white;\n"
+"    }\n"
+"    QCheckBox:pressed {\n"
+"\n"
+"    }")
+
+        self.horizontalLayout_2.addWidget(self.select_from_list)
+
+        self.key_line = QLineEdit(self.frame)
+        self.key_line.setObjectName(u"key_line")
+        self.key_line.setReadOnly(True)
+
+        self.horizontalLayout_2.addWidget(self.key_line)
+
+        self.list = QComboBox(self.frame)
+        self.list.setObjectName(u"list")
+        self.list.setStyleSheet(u"QMenu {\n"
+"background-color: red;\n"
+" }\n"
+"\n"
+"QMenu::item {\n"
+"    padding: 5px 10px;\n"
+"    font: 580 8pt \"Segoe UI\";\n"
+"    border: 2px solid black;\n"
+"    border-radius: 4px;\n"
+"    border-color: rgba(80, 80, 80, 255);\n"
+"    color: #E3E3E3;\n"
+"}\n"
+"QMenu::item:selected {\n"
+"    background-color: #414956;\n"
+"    color: white;\n"
+"}\n"
+"QWidget {\n"
+"    background-color: #151515;\n"
+"    outline: none;\n"
+"}\n"
+"\n"
+"QWidget:item:checked {\n"
+"    background-color: #151515;\n"
+"    color: white;\n"
+"}\n"
+"\n"
+"QWidget:item:selected {\n"
+"    background-color: #414956;\n"
+"    color: white;\n"
+"    border: 0px;\n"
+"}\n"
+"\n"
+"\n"
+"\n"
+"QMenu {\n"
+"    background-color: #1d1d1f;\n"
+"    color: #FFFFFF;\n"
+"    border: 1px solid #555555;\n"
+"    /* padding-top: 5px; */\n"
+"    /* padding-bottom: 5px; */\n"
+"    border: 2px solid black;\n"
+"    border-radius: 4px;\n"
+"    border-color: rgba(80, 80, 80, 255);\n"
+"}\n"
+"\n"
+"QMenu::item {\n"
+"    font: 580 8pt \"Segoe UI\";\n"
+"    border-top: 2p"
+                        "x solid black;\n"
+"    border: 0px;\n"
+"    border-radius: 4px;\n"
+"    border-color: rgba(80, 80, 80, 255);\n"
+"    padding-left: 12px;\n"
+"    padding-right: 12px;\n"
+"    color: #E3E3E3;\n"
+"}\n"
+"\n"
+"QMenu::item:selected {\n"
+"    background-color: #666666;\n"
+"    color: #FFFFFF;\n"
+"}\n"
+"\n"
+"QMenu::separator {\n"
+"    height: 1px;\n"
+"    background: #AAAAAA;\n"
+"    margin: 5px 0;\n"
+"}\n"
+"\n"
+"QMenu::indicator {\n"
+"    width: 13px;\n"
+"    height: 13px;\n"
+"}\n"
+"\n"
+"QMenu::indicator:checked {\n"
+"    image: url(:/images/checkmark.png);\n"
+"}\n"
+"\n"
+"QMenu::indicator:unchecked {\n"
+"    image: url(:/images/empty.png);\n"
+"}\n"
+"\n"
+"\n"
+"QComboBox {\n"
+"    font: 580 8pt \"Segoe UI\";\n"
+"    border: 2px solid black;\n"
+"    border-radius: 4px;\n"
+"    border-color: rgba(80, 80, 80, 255);\n"
+"    height: 12px;\n"
+"    padding-top: 5px;\n"
+"    padding-bottom: 5px;\n"
+"    color: #E3E3E3;\n"
+"    background-color: #1C1C1C;\n"
+"    padding-left: 5px;\n"
+"}\n"
+"\n"
+"QComboBox:hover {\n"
+"    backgr"
+                        "ound-color: #414956;\n"
+"    color: white;\n"
+"}\n"
+"\n"
+"QComboBox:pressed {\n"
+"    background-color: #2E2F30;\n"
+"    color: white;\n"
+"}\n"
+"\n"
+"QComboBox:item {\n"
+"    font: 580 8pt \"Segoe UI\";\n"
+"    color: #E3E3E3;\n"
+"    padding-left: 5px;\n"
+"    background-color: #1C1C1C;\n"
+"    border-style: none;\n"
+"}\n"
+"\n"
+"QComboBox::drop-down {\n"
+"    color: #E3E3E3;\n"
+"    padding: 2px;\n"
+"    background: url(://icons/arrow_drop_down_16dp.svg) no-repeat center;\n"
+"    border-bottom: 0px solid black;\n"
+"    border-top: 0px solid black;\n"
+"    border-right: 0px;\n"
+"    border-left: 2px solid;\n"
+"    margin-left: 5px;\n"
+"    padding: 5px;\n"
+"    width: 7px;\n"
+"    border-color: rgba(80, 80, 80, 255);\n"
+"    background-color: #1C1C1C;\n"
+"}\n"
+"\n"
+"\n"
+"QComboBox QAbstractItemView {\n"
+"    border: 2px solid gray;\n"
+"    border-color: rgba(80, 80, 80, 255);\n"
+"    selection-background-color: #414956;\n"
+"    background-color: #1C1C1C;\n"
+"}\n"
+"\n"
+"\n"
+"QComboBox QAbstractItemView::item {\n"
+""
+                        "    height: 16px; /* Set the height of each item */\n"
+"    padding: 4px; /* Add padding to each item */\n"
+"    padding-left: 5px;\n"
+"    padding-right: 5px;\n"
+"    color: #ff8a8a8a;\n"
+"    border-style: none;\n"
+"    border-bottom: 0.5px solid black;\n"
+"    border-color: rgba(255, 255, 255, 10);\n"
+"}\n"
+"\n"
+"\n"
+"QComboBox QAbstractItemView::item:selected {\n"
+"    height: 16px; /* Set the height of each item */\n"
+"    padding: 4px; /* Add padding to each item */\n"
+"    padding-left: 5px;\n"
+"    padding-right: 5px;\n"
+"    background-color: #414956;\n"
+"    color: white;\n"
+"    border: none; /* Remove border */\n"
+"    outline: none; /* Remove outline */\n"
+"}")
+
+        self.horizontalLayout_2.addWidget(self.list)
+
+
+        self.verticalLayout.addWidget(self.frame)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.ctrl = QCheckBox(Dialog)
+        self.ctrl.setObjectName(u"ctrl")
+        self.ctrl.setStyleSheet(u"\n"
+"    /* QPushButton default and hover styles */\n"
+"    QCheckBox {\n"
+"\n"
+"        font: 580 9pt \"Segoe UI\";\n"
+"	\n"
+"\n"
+"        border: 2px solid black;\n"
+"        border-radius: 2px;\n"
+"        border-color: rgba(80, 80, 80, 255);\n"
+"        height:22px;\n"
+"        padding-top: 2px;\n"
+"        padding-bottom:2px;\n"
+"        padding-left: 4px;\n"
+"        padding-right: 4px;\n"
+"        color: #E3E3E3;\n"
+"        background-color: #1C1C1C;\n"
+"    }\n"
+"    QCheckBox:hover {\n"
+"        background-color: #414956;\n"
+"        color: white;\n"
+"    }\n"
+"    QCheckBox:pressed {\n"
+"\n"
+"    }")
+
+        self.horizontalLayout.addWidget(self.ctrl)
+
+        self.shift = QCheckBox(Dialog)
+        self.shift.setObjectName(u"shift")
+        self.shift.setStyleSheet(u"\n"
+"    /* QPushButton default and hover styles */\n"
+"    QCheckBox {\n"
+"\n"
+"        font: 580 9pt \"Segoe UI\";\n"
+"	\n"
+"\n"
+"        border: 2px solid black;\n"
+"        border-radius: 2px;\n"
+"        border-color: rgba(80, 80, 80, 255);\n"
+"        height:22px;\n"
+"        padding-top: 2px;\n"
+"        padding-bottom:2px;\n"
+"        padding-left: 4px;\n"
+"        padding-right: 4px;\n"
+"        color: #E3E3E3;\n"
+"        background-color: #1C1C1C;\n"
+"    }\n"
+"    QCheckBox:hover {\n"
+"        background-color: #414956;\n"
+"        color: white;\n"
+"    }\n"
+"    QCheckBox:pressed {\n"
+"\n"
+"    }")
+
+        self.horizontalLayout.addWidget(self.shift)
+
+        self.alt = QCheckBox(Dialog)
+        self.alt.setObjectName(u"alt")
+        self.alt.setStyleSheet(u"\n"
+"    /* QPushButton default and hover styles */\n"
+"    QCheckBox {\n"
+"\n"
+"        font: 580 9pt \"Segoe UI\";\n"
+"	\n"
+"\n"
+"        border: 2px solid black;\n"
+"        border-radius: 2px;\n"
+"        border-color: rgba(80, 80, 80, 255);\n"
+"        height:22px;\n"
+"        padding-top: 2px;\n"
+"        padding-bottom:2px;\n"
+"        padding-left: 4px;\n"
+"        padding-right: 4px;\n"
+"        color: #E3E3E3;\n"
+"        background-color: #1C1C1C;\n"
+"    }\n"
+"    QCheckBox:hover {\n"
+"        background-color: #414956;\n"
+"        color: white;\n"
+"    }\n"
+"    QCheckBox:pressed {\n"
+"\n"
+"    }")
+
+        self.horizontalLayout.addWidget(self.alt)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.buttonBox = QDialogButtonBox(Dialog)
+        self.buttonBox.setObjectName(u"buttonBox")
+        self.buttonBox.setStyleSheet(u"\n"
+"    /* QPushButton default and hover styles */\n"
+"    QPushButton {\n"
+"\n"
+"        font: 580 9pt \"Segoe UI\";\n"
+"	\n"
+"\n"
+"        border: 2px solid black;\n"
+"        border-radius: 2px;\n"
+"        border-color: rgba(80, 80, 80, 255);\n"
+"        height:22px;\n"
+"        padding-top: 2px;\n"
+"        padding-bottom:2px;\n"
+"        padding-left: 4px;\n"
+"        padding-right: 4px;\n"
+"        color: #E3E3E3;\n"
+"        background-color: #1C1C1C;\n"
+"    }\n"
+"    QPushButton:hover {\n"
+"        background-color: #414956;\n"
+"        color: white;\n"
+"    }\n"
+"    QPushButton:pressed {\n"
+"        background-color: red;\n"
+"        background-color: #1C1C1C;\n"
+"        margin: 1 px;\n"
+"        margin-left: 2px;\n"
+"        margin-right: 2px;\n"
+"\n"
+"    }")
+        self.buttonBox.setOrientation(Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QDialogButtonBox.Cancel|QDialogButtonBox.Ok)
+        self.buttonBox.setCenterButtons(False)
+
+        self.verticalLayout.addWidget(self.buttonBox)
+
+
+        self.retranslateUi(Dialog)
+        self.buttonBox.accepted.connect(Dialog.accept)
+        self.buttonBox.rejected.connect(Dialog.reject)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Change keybinding", None))
+        self.select_from_list.setText(QCoreApplication.translate("Dialog", u"Select from list", None))
+        self.key_line.setPlaceholderText(QCoreApplication.translate("Dialog", u"Press a key", None))
+        self.ctrl.setText(QCoreApplication.translate("Dialog", u"Ctrl", None))
+        self.shift.setText(QCoreApplication.translate("Dialog", u"Shift", None))
+        self.alt.setText(QCoreApplication.translate("Dialog", u"Alt", None))
+    # retranslateUi
+
