f8279beed0ab384095c08c4df4a0d10fa7a83933
dertwist <45437750+dertwist@users.noreply.github.com>
2024-10-04 19:32:19 +0200

    HotkeyEditor serializing

--- hotkey_editor/format.py
+++ hotkey_editor/format.py
@@ -1,6 +1,10 @@
+import os.path
+
 import keyvalues3 as kv3
 
 class HotkeysOpen():
     def __init__(self, filepath):
         self.data = kv3.read(filepath)
 
+file = kv3.read(os.path.normpath("D:\\CG\\Projects\\Other\\Hammer5Tools\\hotkeys\\hammer\\test.txt"))
+print(file.value)
