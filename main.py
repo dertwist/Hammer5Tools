acf8adb5150dd4a6f71d6f09b0980846e7e4c61f
juste <quizzically.twist@gmail.com>
2024-10-16 08:01:43 +0200

    Version update

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ stop_discord_thread = threading.Event()
 
 LOCK_FILE = os.path.join(tempfile.gettempdir(), 'hammer5tools.lock')
 
-app_version = '2.2.3'
+app_version = '2.3.0'
 batchcreator_version = '1.2.2'
 soundevent_editor_version = '0.5.1'
 smartprop_editor_version = '0.8.0'
