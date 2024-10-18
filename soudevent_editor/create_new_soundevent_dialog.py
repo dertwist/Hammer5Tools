121531d312bd348659a50943e73499b958104d63
dertwist <45437750+dertwist@users.noreply.github.com>
2024-10-18 08:59:21 +0200

    Soundevent editor cleanup

--- soudevent_editor/create_new_soundevent_dialog.py
+++
@@ -1,8 +0,0 @@
-from soudevent_editor.ui_create_new_soundevent_dialog import Ui_Dialog
-from PySide6.QtWidgets import QDialog
-class CreateNewSoundEventOptions_Dialog(QDialog):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.ui = Ui_Dialog()
-        self.ui.setupUi(self)
-        self.setModal(True)
