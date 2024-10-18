a501b528e4d27bb9d99fd8eb614a9f8ebb9ad788
dertwist <45437750+dertwist@users.noreply.github.com>
2024-10-18 23:26:37 +0200

    compile

--- compile_ui.py
+++ compile_ui.py
@@ -8,7 +8,7 @@ for root, dirs, files in os.walk('.'):
             filename = os.path.basename(file)
             filename = os.path.splitext(filename)[0]
             # output_file = os.path.join(root, file.replace('.ui', '.py'))
-            output_file = os.path.join(root, f'ui_{file}.py')
+            output_file = os.path.join(root, f'ui_{filename}.py')
             subprocess.run(['pyside6-uic', os.path.join(root, file), '-o', output_file])
 
 # Compile the specific UI file 'ui_input.ui'
