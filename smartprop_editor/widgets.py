c1078b975b55d13fa6cee0db4afcbbd75553d48c
dertwist <45437750+dertwist@users.noreply.github.com>
2024-10-19 01:36:18 +0200

    combobox varialbes

--- smartprop_editor/widgets.py
+++ smartprop_editor/widgets.py
@@ -21,3 +21,28 @@ class ComboboxDynamicItems(QComboBox):
 
     def wheelEvent(self, event):
         event.ignore()
+
+class ComboboxVariables(ComboboxDynamicItems):
+    def __init__(self, parent=None, layout=None):
+        super().__init__(parent)
+        self.variables_scrollArea = layout
+        self.items = None
+
+    def updateItems(self):
+        self.items = []
+        for item in self.get_variables():
+            self.items.append(item['name'])
+
+        current = self.currentText()
+        self.clear()
+        self.addItems(self.items)
+        if current in self.items:
+            self.setCurrentText(current)
+    def get_variables(self):
+        data_out = []
+        for i in range(self.variables_scrollArea.count()):
+            widget = self.variables_scrollArea.itemAt(i).widget()
+            if widget:
+                var = {'name': widget.name, 'class': widget.var_class}
+                data_out.append(var)
+        return data_out
