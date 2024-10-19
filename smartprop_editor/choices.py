4788e4bf1d95ed5c361631c9e8371b169ade5a92
dertwist <45437750+dertwist@users.noreply.github.com>
2024-10-19 17:45:21 +0200

    Variable combobox fix default value setting (wrong signal)

--- smartprop_editor/choices.py
+++ smartprop_editor/choices.py
@@ -54,37 +54,86 @@ class AddChoice():
         item.setText(1,str(value))
         item.setFlags(item.flags() | Qt.ItemIsEditable)
         parent.addChild(item)
-        # Combobox
+        # Combobox var
         combobox = ComboboxVariables(layout=self.variables_scrollArea)
         combobox.setCurrentText(name)
         combobox.addItem(name)
 
-        widget = VariableWidget()
-        parent.treeWidget().setItemWidget(item, 1, widget)
+        # Init Widget
+        # widget = VariableWidget()
+        # parent.treeWidget().setItemWidget(item, 1, widget)
 
-        combobox.clicked.connect(lambda : self.variable_edit_line(type, item))
+        combobox.changed.connect(lambda value_dict: self.variable_edit_line(value_dict, parent=item))
         self.tree.setItemWidget(item, 0, combobox)
-    def variable_edit_line(self, type, parent):
+    def variable_edit_line(self, value_dict, parent):
         """Select widget basing on the variable type"""
+        print(value_dict)
+        type = value_dict['class'].lower()
         if type in var_choice_identification_bool:
             debug(f'Var choice type bool')
         elif type in var_choice_identification_float:
             debug(f'Var choice type float')
+            widget = VariableFloat(value=value_dict['m_default'], type=value_dict['class'])
+            parent.treeWidget().setItemWidget(parent,1, widget)
         elif type in var_choice_identification_int:
             debug(f'Var choice type int')
+            widget = VariableFloat(value=value_dict['m_default'], type=value_dict['class'])
+            parent.treeWidget().setItemWidget(parent,1, widget)
         else:
             debug(f'Var choice type is generic ({type})')
+            widget = VariableWidget(value=value_dict['m_default'], type=value_dict['class'])
+            parent.treeWidget().setItemWidget(parent,1, widget)
 
 class VariableWidget(QWidget):
-    def __init__(self, value=None, type=None, parent=None):
+    def __init__(self, value=None, type=None):
         super().__init__()
-        self.layout = QHBoxLayout()  # Create a QVBoxLayout for the layout
-        self.layout.setContentsMargins(0,0,0,0)
+        self.data = {'m_Value': value, 'm_DataType': type}
+        self.setupUI()
+
+    def setupUI(self):
+        self.layout = QHBoxLayout()
+        self.layout.setContentsMargins(0, 0, 0, 0)
         editline = QLineEdit()
-        slider = QSlider()
-        slider.setOrientation(Qt.Orientation.Horizontal)
+        editline.setFocusPolicy(Qt.StrongFocus)
         self.layout.addWidget(editline)
-        self.layout.addWidget(slider)
-        self.setLayout(self.layout)  # Set the layout for the VariableWidget
+        self.setLayout(self.layout)
 
 
+class VariableFloat(VariableWidget):
+    def __init__(self, value=0.0, type=None):
+        super().__init__()
+        self.data = {'m_Value': value, 'm_DataType': type}
+        self.setupUI()
+    def setupUI(self):
+        self.layout = QHBoxLayout()
+        self.layout.setContentsMargins(0, 0, 0, 0)
+
+        self.editline = QLineEdit()
+        self.editline.setMaximumWidth(64)
+        self.slider = QSlider(Qt.Horizontal)
+        self.slider.setMinimum(0)
+        self.slider.setMaximum(1000)
+        self.slider.setFocusPolicy(Qt.StrongFocus)
+        self.editline.setFocusPolicy(Qt.StrongFocus)
+
+        self.layout.addWidget(self.editline)
+        self.layout.addWidget(self.slider)
+
+        self.editline.setText(str(self.data['m_Value']))
+        self.slider.setValue(int(self.data['m_Value'] * 100))
+        self.editline.textChanged.connect(self.on_editline_changed)
+        self.slider.valueChanged.connect(self.on_slider_changed)
+        self.setLayout(self.layout)
+
+    def on_editline_changed(self, text_value):
+        try:
+            float_value = float(text_value)
+        except ValueError:
+            float_value = 0
+        self.slider.setValue(int(float_value * 100))
+
+    def on_slider_changed(self, slider_value):
+        print(slider_value, self.editline.text())
+        float_value = slider_value / 100.0
+        # self.editline.setText(str(float_value))
+        self.editline.setText('a')
--- smartprop_editor/variables/combobox.py
+++ smartprop_editor/variables/combobox.py
@@ -2,7 +2,7 @@ from smartprop_editor.variables.ui_combobox import Ui_Widget
 
 from PySide6.QtWidgets import QWidget
 from PySide6.QtCore import Signal
-
+from preferences import debug
 
 class Var_class_combobox(QWidget):
     edited = Signal(str, str, str, str)
@@ -21,7 +21,7 @@ class Var_class_combobox(QWidget):
 
         self.ui.value.addItems(elements)
         self.ui.value.setCurrentText(str(self.default))
-        self.ui.value.editTextChanged.connect(self.on_changed)
+        self.ui.value.currentTextChanged.connect(self.on_changed)
 
 
     def on_changed(self):
