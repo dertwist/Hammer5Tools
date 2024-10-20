c3f9da7514b117e606cbe262810096b745d77630
dertwist <45437750+dertwist@users.noreply.github.com>
2024-10-20 13:23:45 +0200

    Loading erditor compiling

--- common.py
+++ common.py
@@ -1,3 +1,8 @@
+from preferences import get_cs2_path
+import os
+import subprocess
+import threading
+
 editor_info = {
     'editor_info':
     [{'Info':
@@ -6,3 +11,15 @@ editor_info = {
     'Steam': 'https://steamcommunity.com/id/der_twist',
     'Twitter': 'https://twitter.com/der_twist'}
     ]}
+
+
+def compile(input_file):
+    # Define a function to run compile in a thread
+    def compile(input_file):
+        resourcecompiler = os.path.join(get_cs2_path(), 'game', 'bin', 'win64', 'resourcecompiler.exe')
+        process = subprocess.Popen([resourcecompiler, '-i', input_file], shell=True)
+        process.wait()
+
+    # Create a new thread for the compile function
+    thread = threading.Thread(target=compile, args=(input_file,))
+    thread.start()
