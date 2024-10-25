cf89d1d160089ce886e482ce57358fbb0045ed6b
dertwist <45437750+dertwist@users.noreply.github.com>
2024-10-25 21:56:38 +0200

    [SmartPropEditor] serialization and deserialization of kv3

--- find_and_replace/ui_main.py
+++ find_and_replace/ui_main.py
@@ -16,14 +16,18 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QAbstractButton, QApplication, QCheckBox, QDialog,
-    QDialogButtonBox, QHBoxLayout, QLineEdit, QPlainTextEdit,
-    QSizePolicy, QVBoxLayout, QWidget)
+    QDialogButtonBox, QFrame, QHBoxLayout, QLineEdit,
+    QPlainTextEdit, QSizePolicy, QVBoxLayout, QWidget)
+import resources_rc
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         if not Dialog.objectName():
             Dialog.setObjectName(u"Dialog")
         Dialog.resize(931, 755)
+        icon = QIcon()
+        icon.addFile(u":/icons/appicon.ico", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        Dialog.setWindowIcon(icon)
         self.verticalLayout_2 = QVBoxLayout(Dialog)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.verticalLayout = QVBoxLayout()
@@ -40,15 +44,26 @@ class Ui_Dialog(object):
 
         self.horizontalLayout_3 = QHBoxLayout()
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.match_case_checkBox = QCheckBox(Dialog)
+        self.frame = QFrame(Dialog)
+        self.frame.setObjectName(u"frame")
+        self.frame.setMinimumSize(QSize(320, 0))
+        self.frame.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Shadow.Raised)
+        self.horizontalLayout_4 = QHBoxLayout(self.frame)
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.horizontalLayout_4.setContentsMargins(0, 0, 0, 0)
+        self.match_case_checkBox = QCheckBox(self.frame)
         self.match_case_checkBox.setObjectName(u"match_case_checkBox")
 
-        self.horizontalLayout_3.addWidget(self.match_case_checkBox)
+        self.horizontalLayout_4.addWidget(self.match_case_checkBox)
 
-        self.whole_words_checkBox = QCheckBox(Dialog)
+        self.whole_words_checkBox = QCheckBox(self.frame)
         self.whole_words_checkBox.setObjectName(u"whole_words_checkBox")
 
-        self.horizontalLayout_3.addWidget(self.whole_words_checkBox)
+        self.horizontalLayout_4.addWidget(self.whole_words_checkBox)
+
+
+        self.horizontalLayout_3.addWidget(self.frame)
 
         self.find_text_lineEdit = QLineEdit(Dialog)
         self.find_text_lineEdit.setObjectName(u"find_text_lineEdit")
@@ -82,11 +97,11 @@ class Ui_Dialog(object):
     # setupUi
 
     def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Duplicate Element With Replace", None))
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Find and replace", None))
         self.match_case_checkBox.setText(QCoreApplication.translate("Dialog", u"Match Case", None))
         self.whole_words_checkBox.setText(QCoreApplication.translate("Dialog", u"Whole words", None))
         self.find_text_lineEdit.setText("")
-        self.find_text_lineEdit.setPlaceholderText(QCoreApplication.translate("Dialog", u"Find text", None))
-        self.replace_with_lineEdit.setPlaceholderText(QCoreApplication.translate("Dialog", u"Replace With", None))
+        self.find_text_lineEdit.setPlaceholderText(QCoreApplication.translate("Dialog", u"Find", None))
+        self.replace_with_lineEdit.setPlaceholderText(QCoreApplication.translate("Dialog", u"Replace", None))
     # retranslateUi
 
