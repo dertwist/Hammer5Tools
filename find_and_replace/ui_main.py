3b90852ded79f44d340f5fb76da37b3d224bb4c1
dertwist <45437750+dertwist@users.noreply.github.com>
2024-10-25 16:56:44 +0200

    [FindAndReplace] Basic functional

--- find_and_replace/ui_main.py
+++ find_and_replace/ui_main.py
@@ -17,7 +17,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QAbstractButton, QApplication, QCheckBox, QDialog,
     QDialogButtonBox, QHBoxLayout, QLineEdit, QPlainTextEdit,
-    QPushButton, QSizePolicy, QVBoxLayout, QWidget)
+    QSizePolicy, QVBoxLayout, QWidget)
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
@@ -33,36 +33,6 @@ class Ui_Dialog(object):
 
         self.verticalLayout.addWidget(self.viewport_QplainText)
 
-        self.horizontalLayout = QHBoxLayout()
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.match_case_checkBox = QCheckBox(Dialog)
-        self.match_case_checkBox.setObjectName(u"match_case_checkBox")
-
-        self.horizontalLayout.addWidget(self.match_case_checkBox)
-
-        self.whole_words_checkBox = QCheckBox(Dialog)
-        self.whole_words_checkBox.setObjectName(u"whole_words_checkBox")
-
-        self.horizontalLayout.addWidget(self.whole_words_checkBox)
-
-        self.only_selection_checkBox = QCheckBox(Dialog)
-        self.only_selection_checkBox.setObjectName(u"only_selection_checkBox")
-
-        self.horizontalLayout.addWidget(self.only_selection_checkBox)
-
-        self.user_ignore_list_checkBox = QCheckBox(Dialog)
-        self.user_ignore_list_checkBox.setObjectName(u"user_ignore_list_checkBox")
-
-        self.horizontalLayout.addWidget(self.user_ignore_list_checkBox)
-
-        self.edit_ignore_list_button = QPushButton(Dialog)
-        self.edit_ignore_list_button.setObjectName(u"edit_ignore_list_button")
-
-        self.horizontalLayout.addWidget(self.edit_ignore_list_button)
-
-
-        self.verticalLayout.addLayout(self.horizontalLayout)
-
         self.horizontalLayout_2 = QHBoxLayout()
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
 
@@ -70,6 +40,16 @@ class Ui_Dialog(object):
 
         self.horizontalLayout_3 = QHBoxLayout()
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.match_case_checkBox = QCheckBox(Dialog)
+        self.match_case_checkBox.setObjectName(u"match_case_checkBox")
+
+        self.horizontalLayout_3.addWidget(self.match_case_checkBox)
+
+        self.whole_words_checkBox = QCheckBox(Dialog)
+        self.whole_words_checkBox.setObjectName(u"whole_words_checkBox")
+
+        self.horizontalLayout_3.addWidget(self.whole_words_checkBox)
+
         self.find_text_lineEdit = QLineEdit(Dialog)
         self.find_text_lineEdit.setObjectName(u"find_text_lineEdit")
 
@@ -105,9 +85,6 @@ class Ui_Dialog(object):
         Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Duplicate Element With Replace", None))
         self.match_case_checkBox.setText(QCoreApplication.translate("Dialog", u"Match Case", None))
         self.whole_words_checkBox.setText(QCoreApplication.translate("Dialog", u"Whole words", None))
-        self.only_selection_checkBox.setText(QCoreApplication.translate("Dialog", u"Only selection", None))
-        self.user_ignore_list_checkBox.setText(QCoreApplication.translate("Dialog", u"Use ignore list", None))
-        self.edit_ignore_list_button.setText(QCoreApplication.translate("Dialog", u"Edit Ignore list", None))
         self.find_text_lineEdit.setText("")
         self.find_text_lineEdit.setPlaceholderText(QCoreApplication.translate("Dialog", u"Find text", None))
         self.replace_with_lineEdit.setPlaceholderText(QCoreApplication.translate("Dialog", u"Replace With", None))
