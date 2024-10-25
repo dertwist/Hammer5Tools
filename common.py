55c10f9b48201918b3934529544b4e4bdc23257e
dertwist <45437750+dertwist@users.noreply.github.com>
2024-10-25 21:56:38 +0200

    [SmartPropEditor] serialization and deserialization of kv3

--- common.py
+++ common.py
@@ -37,37 +37,3 @@ def JsonToKv3(input):
         return kv3.textwriter.encode(input)
     else:
         raise ValueError('[JsonToKv3] Invalid input type: Input should be a dictionary')
-
-data = """		_class = "CSmartPropElement_Group"
-		m_bEnabled = true
-		m_Modifiers = 
-		[
-			
-			{
-				_class = "CSmartPropFilter_VariableValue"
-				m_bEnabled = true
-				m_VariableComparison = 
-				{
-					m_Name = "negative"
-					m_Value = true
-					m_Comparison = "EQUAL"
-				}
-			},
-			
-			{
-				_class = "CSmartPropOperation_SetVariableFloat"
-				m_bEnabled = true
-				m_VariableName = "switcher"
-				m_VariableValue = 
-				{
-					m_Expression = "-1"
-				}
-			},
-		]
-		m_SelectionCriteria = 
-		[
-		]
-		m_sLabel = "SetVariable_01"
-		"""
-
-print(type(Kv3ToJson(data)), Kv3ToJson(data))
