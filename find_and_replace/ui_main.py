1f2f4be4f3b5bf41d618f7377208d0a2f59f9051
dertwist <45437750+dertwist@users.noreply.github.com>
2024-10-25 12:49:37 +0200

    [FindAndReplace] Added ignore list button and checkbox. Removed regex

--- find_and_replace/ui_main.py
+++ find_and_replace/ui_main.py
@@ -17,7 +17,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QAbstractButton, QApplication, QCheckBox, QDialog,
     QDialogButtonBox, QHBoxLayout, QLineEdit, QPlainTextEdit,
-    QSizePolicy, QVBoxLayout, QWidget)
+    QPushButton, QSizePolicy, QVBoxLayout, QWidget)
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
@@ -40,36 +40,31 @@ class Ui_Dialog(object):
 
         self.horizontalLayout.addWidget(self.match_case_checkBox)
 
-        self.regex_checkBox = QCheckBox(Dialog)
-        self.regex_checkBox.setObjectName(u"regex_checkBox")
-
-        self.horizontalLayout.addWidget(self.regex_checkBox)
-
         self.whole_words_checkBox = QCheckBox(Dialog)
         self.whole_words_checkBox.setObjectName(u"whole_words_checkBox")
 
         self.horizontalLayout.addWidget(self.whole_words_checkBox)
+
+        self.only_selection_checkBox = QCheckBox(Dialog)
+        self.only_selection_checkBox.setObjectName(u"only_selection_checkBox")
+
+        self.horizontalLayout.addWidget(self.only_selection_checkBox)
+
+        self.user_ignore_list_checkBox = QCheckBox(Dialog)
+        self.user_ignore_list_checkBox.setObjectName(u"user_ignore_list_checkBox")
+
+        self.horizontalLayout.addWidget(self.user_ignore_list_checkBox)
+
+        self.edit_ignore_list_button = QPushButton(Dialog)
+        self.edit_ignore_list_button.setObjectName(u"edit_ignore_list_button")
+
+        self.horizontalLayout.addWidget(self.edit_ignore_list_button)
 
 
         self.verticalLayout.addLayout(self.horizontalLayout)
 
         self.horizontalLayout_2 = QHBoxLayout()
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.serach_backward_checkBox = QCheckBox(Dialog)
-        self.serach_backward_checkBox.setObjectName(u"serach_backward_checkBox")
-
-        self.horizontalLayout_2.addWidget(self.serach_backward_checkBox)
-
-        self.search_from_start_checkBox = QCheckBox(Dialog)
-        self.search_from_start_checkBox.setObjectName(u"search_from_start_checkBox")
-
-        self.horizontalLayout_2.addWidget(self.search_from_start_checkBox)
-
-        self.serach_selection_checkBox = QCheckBox(Dialog)
-        self.serach_selection_checkBox.setObjectName(u"serach_selection_checkBox")
-
-        self.horizontalLayout_2.addWidget(self.serach_selection_checkBox)
-
 
         self.verticalLayout.addLayout(self.horizontalLayout_2)
 
@@ -109,11 +104,10 @@ class Ui_Dialog(object):
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Duplicate Element With Replace", None))
         self.match_case_checkBox.setText(QCoreApplication.translate("Dialog", u"Match Case", None))
-        self.regex_checkBox.setText(QCoreApplication.translate("Dialog", u"Regex", None))
         self.whole_words_checkBox.setText(QCoreApplication.translate("Dialog", u"Whole words", None))
-        self.serach_backward_checkBox.setText(QCoreApplication.translate("Dialog", u"Search backward", None))
-        self.search_from_start_checkBox.setText(QCoreApplication.translate("Dialog", u"Search from start", None))
-        self.serach_selection_checkBox.setText(QCoreApplication.translate("Dialog", u"Search selection", None))
+        self.only_selection_checkBox.setText(QCoreApplication.translate("Dialog", u"Only selection", None))
+        self.user_ignore_list_checkBox.setText(QCoreApplication.translate("Dialog", u"Use ignore list", None))
+        self.edit_ignore_list_button.setText(QCoreApplication.translate("Dialog", u"Edit Ignore list", None))
         self.find_text_lineEdit.setText("")
         self.find_text_lineEdit.setPlaceholderText(QCoreApplication.translate("Dialog", u"Find text", None))
         self.replace_with_lineEdit.setPlaceholderText(QCoreApplication.translate("Dialog", u"Replace With", None))
