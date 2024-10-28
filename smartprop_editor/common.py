6e5ba3407267680f4b1ee31a7ae76f352396674a
dertwist <45437750+dertwist@users.noreply.github.com>
2024-10-28 23:01:38 +0100

    [SmartPropEditor] m_ParameterName variable display, assign id for each element on loading

--- smartprop_editor/common.py
+++ smartprop_editor/common.py
@@ -1,4 +1,8 @@
 from smartprop_editor.objects import element_prefix
+global m_sLabel
+global m_sLabel_list
+m_sLabel = ""
+m_sLabel_list = [""]
 def get_clean_class_name(input):
     if element_prefix in input:
         return input.replace(element_prefix, '')
