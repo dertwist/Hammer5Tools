1e2b52cb9530b3477433137a5471540f7369e564
dertwist <45437750+dertwist@users.noreply.github.com>
2024-10-28 20:53:40 +0100

    [SmartPropEditor] Created common file for qt stylesheet, new item model for hierarchy items. get_element_id_value outputs dict

---
+++ qt_styles/common.py
@@ -0,0 +1,41 @@
+qt_stylesheet_colors = {
+    'background_neutral': '#151515',
+    'background_Primary': '#1C1C1C',
+    'background_Secondary': '#1d1d1f',
+    'text_primary': '#E3E3E3',
+    'stroke': '#363639',
+    'SelectedFill': '#414956',
+    'text_Neutral': '#9D9D9D',
+    'text_Neutral_second': '#6D6D6D',
+    'pressed': '#606C77'
+}
+qt_stylesheet_classes = {
+    'label':
+        """
+        QLabel {
+        font-size: 8pt;
+        font-family: "Segoe UI";
+        border-top: 0px;
+        border-left: 0px;
+        border-right: 0px;
+        border-bottom: 2px solid rgba(80, 80, 80, 255);
+        border-radius: 0px;
+        padding: 2px;
+        color: #6D6D6D;
+        background-color: #9D9D9D;
+        """,
+    'label_second':
+        """
+        QLabel {
+        font-size: 8pt;
+        font-family: "Segoe UI";
+        border-top: 0px;
+        border-left: 0px;
+        border-right: 0px;
+        border-bottom: 2px solid rgba(80, 80, 80, 255);
+        border-radius: 0px;
+        padding: 2px;
+        color: #6D6D6D;
+        background-color: #242424;
+        """
+}
