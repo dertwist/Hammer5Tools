395ce158539a4f3defbaa16a05d8d28a972eb517
dertwist <45437750+dertwist@users.noreply.github.com>
2024-10-29 09:10:21 +0100

    [SmartPropEditor] Set label

--- smartprop_editor/_common.py
+++ smartprop_editor/_common.py
@@ -8,3 +8,26 @@ def get_clean_class_name(input):
         return input.replace(element_prefix, '')
     else:
         return input
+#=================================================================<  Label  >===============================================================
+
+def add_label_list(label):
+    global m_sLabel
+    global m_sLabel_list
+    if label in m_sLabel_list:
+        pass
+    else:
+        m_sLabel_list.append(label)
+
+def set_Label(label):
+    global m_sLabel
+    global m_sLabel_list
+    m_sLabel = label
+    return label
+def get_label(_class)
+    global m_sLabel
+    global m_sLabel_list
+    base_name = get_clean_class_name(_class)
+    if base_name in m_sLabel_list:
+        name = f"{base_name}_{}"
+    pass
+
