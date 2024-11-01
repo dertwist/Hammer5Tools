0619674cbc9201d801d6c0c6d87a6052247722dc
dertwist <45437750+dertwist@users.noreply.github.com>
2024-11-01 16:54:06 +0100

    [SmartPropEditor] Choices element id update

--- smartprop_editor/vsmart.py
+++ smartprop_editor/vsmart.py
@@ -204,5 +204,5 @@ class VsmartSave:
                         variables.append(out)
                 options.append({'m_Name': option_child.text(0), 'm_VariableValues': variables})
             # Temp m_nElementID. Needs to be rewritten
-            m_Choices.append({'_class': 'CSmartPropChoice', 'm_Name': child.text(0), 'm_Options': options, 'm_DefaultOption': widget.currentText(), 'm_nElementID': set_ElementID()})
+            m_Choices.append({'_class': 'CSmartPropChoice', 'm_Name': child.text(0), 'm_Options': options, 'm_DefaultOption': widget.currentText(), 'm_nElementID': set_ElementID(force=True)})
         return m_Choices
