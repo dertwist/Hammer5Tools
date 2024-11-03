0da92645a2e3cb50ecbf90f3c6067cdf1aa00371
dertwist <45437750+dertwist@users.noreply.github.com>
2024-11-03 15:10:57 +0100

    Moved all files related to the program to src folder

---
+++ dev/compile_ui.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+import threading
+
+def compile_ui(file, output_file):
+    subprocess.run(['pyside6-uic', file, '-o', output_file])
+    print(file,output_file)
+
+# Function to compile UI files with progress report
+def compile_with_progress(file, output_file):
+    print(f'Compiling {file}...')
+    compile_ui(file, output_file)
+    print(f'{file} compiled successfully.')
+
+# Search for UI files in the project directory and compile them using PySide6
+for root, dirs, files in os.walk('../src'):
+    for file in files:
+        if file.endswith('.ui') and file != 'ui_input.ui':
+            filename = os.path.basename(file)
+            filename = os.path.splitext(filename)[0]
+            output_file = os.path.join(root, f'ui_{filename}.py')
+
+            # Create a thread for each UI file compilation
+            thread = threading.Thread(target=compile_with_progress, args=(os.path.join(root, file), output_file))
+            thread.start()
+
+# Compile the specific UI file 'ui_input.ui'
+subprocess.run(['pyside6-uic', 'ui_input.ui', '-o', 'ui_input.py'])
+
+subprocess.run(['pyside6-rcc', '.\\resources.qrc', '-o', '.\\resources_rc.py'])
---
+++ src/smartprop_editor/variables/ui_color.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'color.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QFrame, QHBoxLayout, QLabel,
+    QPushButton, QSizePolicy, QVBoxLayout, QWidget)
+
+class Ui_Widget(object):
+    def setupUi(self, Widget):
+        if not Widget.objectName():
+            Widget.setObjectName(u"Widget")
+        Widget.resize(790, 32)
+        Widget.setMinimumSize(QSize(0, 0))
+        Widget.setMaximumSize(QSize(16777215, 97))
+        Widget.setStyleSheet(u".QWidget {\n"
+"    font: 580 10pt \"Segoe UI\";\n"
+"    border: 2px solid black;\n"
+"    border-radius: 0px;\n"
+"    border-left: 0px;\n"
+"    border-right: 0px;\n"
+"	border-top: 0px;\n"
+"    border-color: rgba(50, 50, 50, 255);\n"
+"    padding: 8px;\n"
+"    padding-left: 6px;\n"
+"    padding-right: 6px;\n"
+"    color: #E3E3E3;\n"
+"    background-color: #1C1C1C;\n"
+"}\n"
+"\n"
+".QWidget::hover {\n"
+"}\n"
+".QWidget::selected {\n"
+"    background-color: #414956;\n"
+"}")
+        self.verticalLayout = QVBoxLayout(Widget)
+        self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.frame = QFrame(Widget)
+        self.frame.setObjectName(u"frame")
+        self.frame.setMaximumSize(QSize(16777215, 32))
+        self.frame.setStyleSheet(u".QFrame {\n"
+"    font: 580 10pt \"Segoe UI\";\n"
+"    border: 2px solid black;\n"
+"    border-radius: 0px;\n"
+"    border-left: 0px;\n"
+"    border-right: 0px;\n"
+"	border-top: 0px;\n"
+"    border-color: rgba(50, 50, 50, 255);\n"
+"    color: #E3E3E3;\n"
+"    background-color: #1C1C1C;\n"
+"}\n"
+"\n"
+".QFrame::hover {\n"
+"}\n"
+".QFrame::selected {\n"
+"    background-color: #414956;\n"
+"}")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.frame.setLineWidth(0)
+        self.horizontalLayout_2 = QHBoxLayout(self.frame)
+        self.horizontalLayout_2.setSpacing(16)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.label = QLabel(self.frame)
+        self.label.setObjectName(u"label")
+        self.label.setStyleSheet(u"border:0px;\n"
+"background-color: rgba(255, 255, 255, 0);\n"
+"font: 8pt \"Segoe UI\";\n"
+"\n"
+"")
+
+        self.horizontalLayout_2.addWidget(self.label)
+
+        self.color = QPushButton(self.frame)
+        self.color.setObjectName(u"color")
+        self.color.setStyleSheet(u"background-color: rgb(238, 0, 255);\n"
+"padding:4px;\n"
+"border:0px;\n"
+"border: 2px solid translucent;\n"
+"border-color: rgba(80, 80, 80, 100);")
+
+        self.horizontalLayout_2.addWidget(self.color)
+
+
+        self.verticalLayout.addWidget(self.frame)
+
+
+        self.retranslateUi(Widget)
+
+        QMetaObject.connectSlotsByName(Widget)
+    # setupUi
+
+    def retranslateUi(self, Widget):
+        Widget.setWindowTitle(QCoreApplication.translate("Widget", u"Form", None))
+        self.label.setText(QCoreApplication.translate("Widget", u"Value", None))
+        self.color.setText("")
+    # retranslateUi
+
