377dbabe7f790fd2edfdfd249eec35ae27783666
dertwist <45437750+dertwist@users.noreply.github.com>
2024-11-03 15:10:30 +0100

    Moved all files related to the program to src folder

---
+++ src/documentation/documentation.py
@@ -0,0 +1,21 @@
+from src.documentation.ui_documentation_dialog import Ui_documentation_dialog
+from PySide6.QtWidgets import QDialog
+from PySide6.QtCore import QUrl
+from PySide6.QtGui import QDesktopServices
+class Documentation_Dialog(QDialog):
+    def __init__(self, version, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_documentation_dialog()
+        self.ui.setupUi(self)
+        self.ui.version.setText(f"Version: {version}")
+        self.ui.report_a_bug_button.clicked.connect(self.open_report_bug_link)
+        self.ui.request_a_new_feature_button.clicked.connect(self.open_request_a_new_feature)
+        self.ui.open_documentation_button.clicked.connect(self.open_documentation)
+
+    def open_report_bug_link(self):
+        QDesktopServices.openUrl(QUrl("https://discord.gg/mMaub4jCBa"))
+
+    def open_request_a_new_feature(self):
+        QDesktopServices.openUrl(QUrl("https://discord.gg/xygJgM7ZyP"))
+    def open_documentation(self):
+        QDesktopServices.openUrl(QUrl("https://twist-1.gitbook.io/hammer5tools"))
