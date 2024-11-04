20e9213788cd07fb28360718cc666fdca403b78e
dertwist <45437750+dertwist@users.noreply.github.com>
2024-11-04 10:50:59 +0100

    [SoundEventEditor] ui update

--- src/soudevent_editor/ui_preset_manager.py
+++ src/soudevent_editor/ui_preset_manager.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'main.ui'
+## Form generated from reading UI file 'preset_manager.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.7.2
 ##
@@ -16,17 +16,16 @@ from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
     QIcon, QImage, QKeySequence, QLinearGradient,
     QPainter, QPalette, QPixmap, QRadialGradient,
     QTransform)
-from PySide6.QtWidgets import (QAbstractItemView, QApplication, QCheckBox, QDockWidget,
-    QFrame, QHBoxLayout, QHeaderView, QLabel,
-    QLineEdit, QMainWindow, QPushButton, QSizePolicy,
-    QTreeWidget, QTreeWidgetItem, QVBoxLayout, QWidget)
-import resources_rc
+from PySide6.QtWidgets import (QApplication, QDockWidget, QFrame, QHBoxLayout,
+    QLabel, QMainWindow, QPushButton, QSizePolicy,
+    QVBoxLayout, QWidget)
+import src.resources_rc
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(1490, 941)
+        MainWindow.resize(1240, 941)
         MainWindow.setStyleSheet(u"background-color: #1C1C1C;")
         self.actionCreateNewsmartprop = QAction(MainWindow)
         self.actionCreateNewsmartprop.setObjectName(u"actionCreateNewsmartprop")
@@ -91,57 +90,31 @@ class Ui_MainWindow(object):
         self.verticalLayout_6.addWidget(self.frame)
 
         MainWindow.setCentralWidget(self.centralwidget)
-        self.dockWidget_4 = QDockWidget(MainWindow)
-        self.dockWidget_4.setObjectName(u"dockWidget_4")
-        self.dockWidget_4.setFeatures(QDockWidget.DockWidgetFeature.DockWidgetMovable)
-        self.dockWidget_4.setAllowedAreas(Qt.DockWidgetArea.BottomDockWidgetArea|Qt.DockWidgetArea.LeftDockWidgetArea|Qt.DockWidgetArea.RightDockWidgetArea)
-        self.dockWidgetContents_2 = QWidget()
-        self.dockWidgetContents_2.setObjectName(u"dockWidgetContents_2")
-        self.verticalLayout_8 = QVBoxLayout(self.dockWidgetContents_2)
-        self.verticalLayout_8.setObjectName(u"verticalLayout_8")
-        self.verticalLayout_8.setContentsMargins(0, 0, 0, 0)
-        self.frame_2 = QFrame(self.dockWidgetContents_2)
-        self.frame_2.setObjectName(u"frame_2")
-        self.frame_2.setFrameShape(QFrame.Shape.StyledPanel)
-        self.frame_2.setFrameShadow(QFrame.Shadow.Raised)
-        self.frame_2.setLineWidth(0)
-        self.verticalLayout_3 = QVBoxLayout(self.frame_2)
-        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
-        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
-        self.hierarchy_search_bar_widget = QLineEdit(self.frame_2)
-        self.hierarchy_search_bar_widget.setObjectName(u"hierarchy_search_bar_widget")
+        self.dockWidget_10 = QDockWidget(MainWindow)
+        self.dockWidget_10.setObjectName(u"dockWidget_10")
+        self.dockWidget_10.setFeatures(QDockWidget.DockWidgetFeature.DockWidgetMovable)
+        self.dockWidget_10.setAllowedAreas(Qt.DockWidgetArea.BottomDockWidgetArea|Qt.DockWidgetArea.LeftDockWidgetArea|Qt.DockWidgetArea.RightDockWidgetArea)
+        self.explorer_layout_widget = QWidget()
+        self.explorer_layout_widget.setObjectName(u"explorer_layout_widget")
+        self.verticalLayout_10 = QVBoxLayout(self.explorer_layout_widget)
+        self.verticalLayout_10.setObjectName(u"verticalLayout_10")
+        self.verticalLayout_10.setContentsMargins(0, 0, 0, 0)
+        self.explorer_layout = QVBoxLayout()
+        self.explorer_layout.setObjectName(u"explorer_layout")
 
-        self.verticalLayout_3.addWidget(self.hierarchy_search_bar_widget)
+        self.verticalLayout_10.addLayout(self.explorer_layout)
 
-        self.hierarchy_widget = QTreeWidget(self.frame_2)
-        __qtreewidgetitem = QTreeWidgetItem()
-        __qtreewidgetitem.setText(0, u"Name");
-        self.hierarchy_widget.setHeaderItem(__qtreewidgetitem)
-        self.hierarchy_widget.setObjectName(u"hierarchy_widget")
-        self.hierarchy_widget.setDragEnabled(True)
-        self.hierarchy_widget.setDragDropOverwriteMode(True)
-        self.hierarchy_widget.setDragDropMode(QAbstractItemView.DragDropMode.InternalMove)
-        self.hierarchy_widget.setDefaultDropAction(Qt.DropAction.MoveAction)
-        self.hierarchy_widget.setAlternatingRowColors(True)
-        self.hierarchy_widget.setSelectionMode(QAbstractItemView.SelectionMode.ExtendedSelection)
-        self.hierarchy_widget.setUniformRowHeights(True)
-        self.hierarchy_widget.setAllColumnsShowFocus(False)
-        self.hierarchy_widget.setWordWrap(False)
-        self.hierarchy_widget.setHeaderHidden(False)
-        self.hierarchy_widget.setColumnCount(3)
-        self.hierarchy_widget.header().setVisible(True)
-        self.hierarchy_widget.header().setCascadingSectionResizes(False)
-        self.hierarchy_widget.header().setMinimumSectionSize(45)
-        self.hierarchy_widget.header().setDefaultSectionSize(135)
-        self.hierarchy_widget.header().setProperty("showSortIndicator", False)
-        self.hierarchy_widget.header().setStretchLastSection(True)
-
-        self.verticalLayout_3.addWidget(self.hierarchy_widget)
-
-        self.open_preset_manager_button = QPushButton(self.frame_2)
-        self.open_preset_manager_button.setObjectName(u"open_preset_manager_button")
-        self.open_preset_manager_button.setEnabled(True)
-        self.open_preset_manager_button.setStyleSheet(u"\n"
+        self.frame_3 = QFrame(self.explorer_layout_widget)
+        self.frame_3.setObjectName(u"frame_3")
+        self.frame_3.setMaximumSize(QSize(16777215, 32))
+        self.frame_3.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame_3.setFrameShadow(QFrame.Shadow.Raised)
+        self.horizontalLayout_4 = QHBoxLayout(self.frame_3)
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.horizontalLayout_4.setContentsMargins(0, 0, 0, 0)
+        self.save_file_button_2 = QPushButton(self.frame_3)
+        self.save_file_button_2.setObjectName(u"save_file_button_2")
+        self.save_file_button_2.setStyleSheet(u"\n"
 "    /* QPushButton default and hover styles */\n"
 "    QPushButton {\n"
 "\n"
@@ -172,106 +145,11 @@ class Ui_MainWindow(object):
 "\n"
 "    }")
         icon7 = QIcon()
-        icon7.addFile(u":/icons/schema_24dp_9D9D9D_FILL0_wght400_GRAD0_opsz24.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
-        self.open_preset_manager_button.setIcon(icon7)
-        self.open_preset_manager_button.setIconSize(QSize(20, 20))
+        icon7.addFile(u":/icons/add_24dp_9D9D9D_FILL0_wght400_GRAD0_opsz24.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.save_file_button_2.setIcon(icon7)
+        self.save_file_button_2.setIconSize(QSize(20, 20))
 
-        self.verticalLayout_3.addWidget(self.open_preset_manager_button)
-
-
-        self.verticalLayout_8.addWidget(self.frame_2)
-
-        self.dockWidget_4.setWidget(self.dockWidgetContents_2)
-        MainWindow.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, self.dockWidget_4)
-        self.dockWidget_10 = QDockWidget(MainWindow)
-        self.dockWidget_10.setObjectName(u"dockWidget_10")
-        self.dockWidget_10.setFeatures(QDockWidget.DockWidgetFeature.DockWidgetMovable)
-        self.dockWidget_10.setAllowedAreas(Qt.DockWidgetArea.BottomDockWidgetArea|Qt.DockWidgetArea.LeftDockWidgetArea|Qt.DockWidgetArea.RightDockWidgetArea)
-        self.explorer_layout_widget = QWidget()
-        self.explorer_layout_widget.setObjectName(u"explorer_layout_widget")
-        self.verticalLayout_10 = QVBoxLayout(self.explorer_layout_widget)
-        self.verticalLayout_10.setObjectName(u"verticalLayout_10")
-        self.verticalLayout_10.setContentsMargins(0, 0, 0, 0)
-        self.explorer_layout = QVBoxLayout()
-        self.explorer_layout.setObjectName(u"explorer_layout")
-
-        self.verticalLayout_10.addLayout(self.explorer_layout)
-
-        self.frame_3 = QFrame(self.explorer_layout_widget)
-        self.frame_3.setObjectName(u"frame_3")
-        self.frame_3.setMaximumSize(QSize(16777215, 32))
-        self.frame_3.setFrameShape(QFrame.Shape.StyledPanel)
-        self.frame_3.setFrameShadow(QFrame.Shadow.Raised)
-        self.horizontalLayout_4 = QHBoxLayout(self.frame_3)
-        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
-        self.horizontalLayout_4.setContentsMargins(0, 0, 0, 0)
-        self.save_file_button = QPushButton(self.frame_3)
-        self.save_file_button.setObjectName(u"save_file_button")
-        self.save_file_button.setStyleSheet(u"\n"
-"    /* QPushButton default and hover styles */\n"
-"    QPushButton {\n"
-"\n"
-"        font: 580 9pt \"Segoe UI\";\n"
-"	\n"
-"\n"
-"        border: 2px solid black;\n"
-"        border-radius: 2px;\n"
-"        border-color: rgba(80, 80, 80, 255);\n"
-"        height:22px;\n"
-"        padding-top: 2px;\n"
-"        padding-bottom:2px;\n"
-"        padding-left: 4px;\n"
-"        padding-right: 4px;\n"
-"        color: #E3E3E3;\n"
-"        background-color: #1C1C1C;\n"
-"    }\n"
-"    QPushButton:hover {\n"
-"        background-color: #414956;\n"
-"        color: white;\n"
-"    }\n"
-"    QPushButton:pressed {\n"
-"        background-color: red;\n"
-"        background-color: #1C1C1C;\n"
-"        margin: 1 px;\n"
-"        margin-left: 2px;\n"
-"        margin-right: 2px;\n"
-"\n"
-"    }")
-        icon8 = QIcon()
-        icon8.addFile(u":/icons/save_16dp_9D9D9D_FILL0_wght400_GRAD0_opsz20.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
-        self.save_file_button.setIcon(icon8)
-        self.save_file_button.setIconSize(QSize(20, 20))
-
-        self.horizontalLayout_4.addWidget(self.save_file_button)
-
-        self.realtime_save_checkbox = QCheckBox(self.frame_3)
-        self.realtime_save_checkbox.setObjectName(u"realtime_save_checkbox")
-        self.realtime_save_checkbox.setStyleSheet(u"\n"
-"    /* QPushButton default and hover styles */\n"
-"    QCheckBox {\n"
-"\n"
-"        font: 580 10pt \"Segoe UI\";\n"
-"\n"
-"        border: 2px solid black;\n"
-"        border-radius: 2px;\n"
-"        border-color: rgba(80, 80, 80, 255);\n"
-"        height:22px;\n"
-"        padding-top: 2px;\n"
-"        padding-bottom:2px;\n"
-"        padding-left: 4px;\n"
-"        padding-right: 4px;\n"
-"        color: #E3E3E3;\n"
-"        background-color: #1C1C1C;\n"
-"    }\n"
-"    QCheckBox:hover {\n"
-"        background-color: #414956;\n"
-"        color: white;\n"
-"    }\n"
-"    QCheckBox:pressed {\n"
-"\n"
-"    }")
-
-        self.horizontalLayout_4.addWidget(self.realtime_save_checkbox)
+        self.horizontalLayout_4.addWidget(self.save_file_button_2)
 
 
         self.verticalLayout_10.addWidget(self.frame_3)
@@ -316,51 +194,12 @@ class Ui_MainWindow(object):
 "        margin-right: 2px;\n"
 "\n"
 "    }")
-        icon9 = QIcon()
-        icon9.addFile(u":/icons/file_open_16dp_9D9D9D_FILL0_wght400_GRAD0_opsz20.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
-        self.output_button.setIcon(icon9)
+        icon8 = QIcon()
+        icon8.addFile(u":/icons/folder_open.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.output_button.setIcon(icon8)
         self.output_button.setIconSize(QSize(20, 20))
 
         self.horizontalLayout_2.addWidget(self.output_button)
-
-        self.reload_button = QPushButton(self.frame_4)
-        self.reload_button.setObjectName(u"reload_button")
-        self.reload_button.setStyleSheet(u"\n"
-"    /* QPushButton default and hover styles */\n"
-"    QPushButton {\n"
-"\n"
-"        font: 580 9pt \"Segoe UI\";\n"
-"	\n"
-"\n"
-"        border: 2px solid black;\n"
-"        border-radius: 2px;\n"
-"        border-color: rgba(80, 80, 80, 255);\n"
-"        height:22px;\n"
-"        padding-top: 2px;\n"
-"        padding-bottom:2px;\n"
-"        padding-left: 4px;\n"
-"        padding-right: 4px;\n"
-"        color: #E3E3E3;\n"
-"        background-color: #1C1C1C;\n"
-"    }\n"
-"    QPushButton:hover {\n"
-"        background-color: #414956;\n"
-"        color: white;\n"
-"    }\n"
-"    QPushButton:pressed {\n"
-"        background-color: red;\n"
-"        background-color: #1C1C1C;\n"
-"        margin: 1 px;\n"
-"        margin-left: 2px;\n"
-"        margin-right: 2px;\n"
-"\n"
-"    }")
-        icon10 = QIcon()
-        icon10.addFile(u":/icons/sync_24dp_9D9D9D_FILL0_wght400_GRAD0_opsz24.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
-        self.reload_button.setIcon(icon10)
-        self.reload_button.setIconSize(QSize(20, 20))
-
-        self.horizontalLayout_2.addWidget(self.reload_button)
 
 
         self.verticalLayout_10.addWidget(self.frame_4)
@@ -387,40 +226,20 @@ class Ui_MainWindow(object):
 #endif // QT_CONFIG(tooltip)
         self.actionFormat_serttings.setText(QCoreApplication.translate("MainWindow", u"Format serttings", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"Properties", None))
-        self.dockWidget_4.setWindowTitle(QCoreApplication.translate("MainWindow", u"Hierarchy", None))
-        self.hierarchy_search_bar_widget.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Search...", None))
-        ___qtreewidgetitem = self.hierarchy_widget.headerItem()
-        ___qtreewidgetitem.setText(2, QCoreApplication.translate("MainWindow", u"Note", None));
-        ___qtreewidgetitem.setText(1, QCoreApplication.translate("MainWindow", u"Data", None));
-#if QT_CONFIG(tooltip)
-        self.open_preset_manager_button.setToolTip(QCoreApplication.translate("MainWindow", u"<html><head/><body><p>Save and compile Ctrl + S</p></body></html>", None))
-#endif // QT_CONFIG(tooltip)
-        self.open_preset_manager_button.setText(QCoreApplication.translate("MainWindow", u"Preset manager", None))
-#if QT_CONFIG(shortcut)
-        self.open_preset_manager_button.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+S", None))
-#endif // QT_CONFIG(shortcut)
         self.dockWidget_10.setWindowTitle(QCoreApplication.translate("MainWindow", u"Explorer", None))
 #if QT_CONFIG(tooltip)
-        self.save_file_button.setToolTip(QCoreApplication.translate("MainWindow", u"<html><head/><body><p>Save soundevents_addon.vsndevts Ctrl + S</p></body></html>", None))
+        self.save_file_button_2.setToolTip(QCoreApplication.translate("MainWindow", u"<html><head/><body><p>Save soundevents_addon.vsndevts Ctrl + S</p></body></html>", None))
 #endif // QT_CONFIG(tooltip)
-        self.save_file_button.setText(QCoreApplication.translate("MainWindow", u"Save", None))
+        self.save_file_button_2.setText(QCoreApplication.translate("MainWindow", u"New", None))
 #if QT_CONFIG(shortcut)
-        self.save_file_button.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+S", None))
+        self.save_file_button_2.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+S", None))
 #endif // QT_CONFIG(shortcut)
-        self.realtime_save_checkbox.setText(QCoreApplication.translate("MainWindow", u"Realtime save", None))
 #if QT_CONFIG(tooltip)
-        self.output_button.setToolTip(QCoreApplication.translate("MainWindow", u"<html><head/><body><p>Open soundevents_addon.vsndevts</p></body></html>", None))
+        self.output_button.setToolTip(QCoreApplication.translate("MainWindow", u"<html><head/><body><p><br/></p></body></html>", None))
 #endif // QT_CONFIG(tooltip)
-        self.output_button.setText(QCoreApplication.translate("MainWindow", u"Output", None))
+        self.output_button.setText(QCoreApplication.translate("MainWindow", u"Open Folder", None))
 #if QT_CONFIG(shortcut)
         self.output_button.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+N", None))
-#endif // QT_CONFIG(shortcut)
-#if QT_CONFIG(tooltip)
-        self.reload_button.setToolTip(QCoreApplication.translate("MainWindow", u"<html><head/><body><p>Reload soundevents_addon.vsndevts Ctrl + R</p></body></html>", None))
-#endif // QT_CONFIG(tooltip)
-        self.reload_button.setText(QCoreApplication.translate("MainWindow", u"Reload", None))
-#if QT_CONFIG(shortcut)
-        self.reload_button.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+Shift+S", None))
 #endif // QT_CONFIG(shortcut)
     # retranslateUi
 
