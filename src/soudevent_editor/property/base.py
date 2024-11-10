5b739ff9f0171d671828c4acb852505916d8ae34
dertwist <45437750+dertwist@users.noreply.github.com>
2024-11-10 21:51:07 +0100

    [SoundEventEditor] Populate properties

--- src/soudevent_editor/property/base.py
+++ src/soudevent_editor/property/base.py
@@ -1,24 +1,35 @@
-from PySide6.QtWidgets import QWidget, QLabel
+from PySide6.QtWidgets import QWidget, QLabel, QHBoxLayout
 from src.widgets import FloatWidget
 
 class SoundEventEditorPropertyBase(QWidget):
-    def __init__(self, parent=None, label_text: str = None):
+    def __init__(self, parent=None, label_text: str = None, value: dict = None):
         """Base property class. There is only a label widget and a frame. New widget can be replaced or added"""
         super().__init__(parent)
+        self.init_root_layout()
+
         self.init_label(label_text)
+        self.init_widget()
+        # Value variable
+        self.value = value
 
     def init_root_layout(self):
         """Adding a root layout in which should be placed all widgets that would be in this class and from encapsulation. Not recommended to overwrite this function"""
+        self.root_layout = QHBoxLayout()
+        self.setLayout(self.root_layout)
     def init_label(self, label_text):
         """Adding received text to the label widget"""
         if label_text is None:
             label_text = "Label"
         label_instance = QLabel()
         label_instance.setText(label_text)
-        self.layout().addWidget(label_instance)
+        self.root_layout.addWidget(label_instance)
     def add_property_widget(self, widget):
         """Adding property widget to the root"""
+        self.root_layout.addWidget(widget)
+    def init_widget(self):
+        """Function to overwriting."""
 
 class SoundEventEditorPropertyFloat(SoundEventEditorPropertyBase):
-    def __init__(self, parent=None, label_text: str = None):
-        super().__init__(parent)
+    def init_widget(self):
+        widget = FloatWidget()
+        self.add_property_widget(widget)
