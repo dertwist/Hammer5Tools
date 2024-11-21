dd18af77dc107b4d029765861741bac4a79d2d5f
dertwist <45437750+dertwist@users.noreply.github.com>
2024-11-21 11:15:21 +0100

    [SoundEventEditor] Property popup meny and pasting properties

--- src/soudevent_editor/objects.py
+++ src/soudevent_editor/objects.py
@@ -1,54 +1,51 @@
 soundevent_editor_properties = [
     # Float
-    {'volume': 1},
-    {'delay': 1},
-    {'pitch': 1},
-    {'volume_random_min': -0.5},
-    {'volume_random_max': 0.5},
-    {'pitch_random_min': -0.5},
-    {'pitch_random_max': -0.5},
-    {'randomize_position_min_radius': 2000},
-    {'randomize_position_max_radius': 3000},
-    {'retrigger_interval_min': 10},
-    {'retrigger_interval_max': 30},
-    {'occlusion_intensity': 0.0},
-    {'distance_effect_mix': 0.0},
-    {'reverb_wet': 1.0},
+    {'Volume': {'volume': 1}},
+    {'Delay': {'delay': 1}},
+    {'Pitch': {'pitch': 1}},
+    {'Volume Random Min': {'volume_random_min': -0.5}},
+    {'Volume Random Max': {'volume_random_max': 0.5}},
+    {'Pitch Random Min': {'pitch_random_min': -0.5}},
+    {'Pitch Random Max': {'pitch_random_max': -0.5}},
+    {'Randomize Position Min Radius': {'randomize_position_min_radius': 2000}},
+    {'Randomize Position Max Radius': {'randomize_position_max_radius': 3000}},
+    {'Retrigger Interval Min': {'retrigger_interval_min': 10}},
+    {'Retrigger Interval Max': {'retrigger_interval_max': 30}},
+    {'Occlusion Intensity': {'occlusion_intensity': 0.0}},
+    {'Distance Effect Mix': {'distance_effect_mix': 0.0}},
+    {'Reverb Wet': {'reverb_wet': 1.0}},
 
     # Files
-    {'vsnd_files_track_01': []},
+    {'Sound Files track': {'vsnd_files_track_01': []}},
 
     # Vector3
-    {'position': [0,0,0]},
+    {'Position': {'position': [0, 0, 0]}},
 
     # Bool
-    {'enable_child_events': True},
-    {'enable_retrigger': True},
-    {'restrict_source_reverb': True},
-    {'randomize_position_hemisphere': False},
-    {'use_distance_unfiltered_stereo_mapping_curve': True},
-    {'use_distance_volume_mapping_curve': True},
-    {'use_time_volume_mapping_curve': True},
-    {'use_time_volume_mapping_curve': False},
-    {'override_dsp_preset': True},
-    {'set_child_position': True},
-    {'position_relative_to_player': False},
-    {'use_world_position': False},
+    {'Enable Child Events': {'enable_child_events': True}},
+    {'Enable Retrigger': {'enable_retrigger': True}},
+    {'Restrict Source Reverb': {'restrict_source_reverb': True}},
+    {'Randomize Position Hemisphere': {'randomize_position_hemisphere': False}},
+    {'Use Distance Unfiltered Stereo Mapping Curve': {'use_distance_unfiltered_stereo_mapping_curve': True}},
+    {'Use Distance Volume Mapping Curve': {'use_distance_volume_mapping_curve': True}},
+    {'Use Time Volume Mapping Curve': {'use_time_volume_mapping_curve': True}},
+    {'Override Dsp Preset': {'override_dsp_preset': True}},
+    {'Set Child Position': {'set_child_position': True}},
+    {'Position Relative To Player': {'position_relative_to_player': False}},
+    {'Use World Position': {'use_world_position': False}},
 
     # Combobox
-    {'dsp_preset': 'reverb_9_mediumChamber'},
-    {'soundevent_01': ''},
-    {'mixgroup': 'Amb_Common'},
-    {'type': 'csgo_mega'},
-    {'base': 'amb.base'},
+    {'Dsp Preset': {'dsp_preset': 'reverb_9_mediumChamber'}},
+    {'Soundevent 01': {'soundevent_01': ''}},
+    {'Mixgroup': {'mixgroup': 'Amb_Common'}},
+    {'Type': {'type': 'csgo_mega'}},
+    {'Base': {'base': 'amb.base'}},
 
     # Curve
-    {'distance_volume_mapping_curve': [[97.14286, 1.0, -0.001763, -0.001763, 2.0, 3.0], [2000.0, 0.0, -0.000526, -0.000526, 1.0, 1.0]]},
-    {'fadetime_volume_mapping_curve': [[0.0, 1.0, -1.223776, -1.223776, 2.0, 3.0], [0.691429, 0.0, 0.0, 0.0, 2.0, 3.0]]},
-    {'distance_unfiltered_stereo_mapping_curve': [[0.0, 0.0, 0.0, 0.0, 2.0, 3.0], [300.0, 0.0, 0.0, 0.0, 2.0, 3.0]]},
-    {'time_volume_mapping_curve': [[0.0, 0.0, 0.0, 0.0, 2.0, 3.0], [1.0, 1.0, 0.0, 0.0, 2.0, 3.0]]},
-
-
+    {'Distance Volume Mapping Curve': {'distance_volume_mapping_curve': [[97.14286, 1.0, -0.001763, -0.001763, 2.0, 3.0], [2000.0, 0.0, -0.000526, -0.000526, 1.0, 1.0]]}},
+    {'Fadetime Volume Mapping Curve': {'fadetime_volume_mapping_curve': [[0.0, 1.0, -1.223776, -1.223776, 2.0, 3.0], [0.691429, 0.0, 0.0, 0.0, 2.0, 3.0]]}},
+    {'Distance Unfiltered Stereo Mapping Curve': {'distance_unfiltered_stereo_mapping_curve': [[0.0, 0.0, 0.0, 0.0, 2.0, 3.0], [300.0, 0.0, 0.0, 0.0, 2.0, 3.0]]}},
+    {'Time Volume Mapping Curve': {'time_volume_mapping_curve': [[0.0, 0.0, 0.0, 0.0, 2.0, 3.0], [1.0, 1.0, 0.0, 0.0, 2.0, 3.0]]}},
 ]
 
 soundevent_blank = {}
