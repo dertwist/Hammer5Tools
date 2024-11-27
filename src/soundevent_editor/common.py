f545014091be46adc2ac859f57fa4d1742043ef6
dertwist <45437750+dertwist@users.noreply.github.com>
2024-11-27 12:16:20 +0100

    [SoundEventEditor] Vsnd case fix

--- src/soundevent_editor/common.py
+++ src/soundevent_editor/common.py
@@ -1,8 +1,18 @@
 import os
-from src.preferences import get_cs2_path, get_addon_name
+from src.preferences import get_cs2_path, get_addon_name, get_addon_dir
 
 soundevents_source_path_default_cs2_template = os.path.join(get_cs2_path(), 'content', 'csgo_addons', 'addon_template', 'soundevents','soundevents_addon.vsndevts')
 sounds_source_path_default_cs2_template = os.path.join(get_cs2_path(), 'content', 'csgo_addons', 'addon_template', 'sounds')
 # Destination
 addon_vsndevts = os.path.join(get_cs2_path(), 'content', 'csgo_addons', get_addon_name(), 'soundevents','soundevents_addon.vsndevts')
 addon_sounds = os.path.join(get_cs2_path(), 'content', 'csgo_addons', get_addon_name(), 'sounds')
+
+def vsnd_case_convert(__value):
+    __value_root, _ = os.path.splitext(__value)
+    __value = __value_root + ".vsnd"
+    __value.replace('\\', '/')
+    return __value
+def vsnd_filepath_convert(__value):
+    __value = vsnd_case_convert(os.path.relpath(__value, get_addon_dir()))
+    __value.replace('\\', '/')
+    return __value
