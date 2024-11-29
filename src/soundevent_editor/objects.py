4b929a447c1b5468a91f211727e3b2194115db27
dertwist <45437750+dertwist@users.noreply.github.com>
2024-11-29 23:38:05 +0100

    vsnd_duration fixed

--- src/soundevent_editor/objects.py
+++ src/soundevent_editor/objects.py
@@ -15,7 +15,7 @@ soundevent_editor_properties = [
     {'Distance Effect Mix': {'distance_effect_mix': 0.0}},
     {'Reverb Wet': {'reverb_wet': 1.0}},
     {'Occlusion Frequency Scale': {'occlusion_frequency_scale ': 1.0}},
-    {'Vsnd Duration': {'vsnd_duration ': 10}},
+    {'Vsnd Duration': {'vsnd_duration': 10}},
 
 
     # Files
