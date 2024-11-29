46d67eb7ec4456b6ee7f2761680122bec446be66
dertwist <45437750+dertwist@users.noreply.github.com>
2024-11-29 15:45:48 +0100

    Removed tkinter, rollback

--- src/updater.py
+++ src/updater.py
@@ -6,20 +6,22 @@ import zipfile
 import io
 from tqdm import tqdm
 import webbrowser
-import tkinter as tk
-from tkinter import messagebox
+from colorama import init, Fore, Style
+
+# Initialize colorama
+init(autoreset=True)
 
 version = 'v3.0.0'
-print(f'Updater version: {version}')
+print(Fore.CYAN + f'Updater version: {version}')
 
 def kill_main_app():
     processes = [process for process in psutil.process_iter() if process.name() == 'Hammer5Tools.exe']
     if processes:
         for process in processes:
             process.kill()
-            print('Hammer5Tools.exe process killed successfully.')
+            print(Fore.GREEN + 'Hammer5Tools.exe process killed successfully.')
     else:
-        print('Hammer5Tools.exe process not found.')
+        print(Fore.YELLOW + 'Hammer5Tools.exe process not found.')
 
 def download_and_extract(url, path):
     response = requests.get(url, stream=True)
@@ -34,9 +36,9 @@ def download_and_extract(url, path):
 
         with zipfile.ZipFile(zip_file, 'r') as zip_ref:
             zip_ref.extractall(path)
-        print('Download successful.')
+        print(Fore.GREEN + 'Download successful.')
     else:
-        print('Failed to download the update')
+        print(Fore.RED + 'Failed to download the update')
         return False
     return True
 
@@ -60,25 +62,25 @@ def update_files(path, path_program):
                 shutil.rmtree(rem_path)
             else:
                 os.remove(rem_path)
-            print(f'Removed: {rem_path}')
+            print(Fore.GREEN + f'Removed: {rem_path}')
             shutil.move(item, rem_path)
-            print(f'Moved: {item} to {rem_path}')
+            print(Fore.GREEN + f'Moved: {item} to {rem_path}')
         except Exception as e:
-            print(f'Error with removing: {rem_path}, {e}')
+            print(Fore.RED + f'Error with removing: {rem_path}, {e}')
             success = False
 
     try:
         os.remove(os.path.join(path_program, 'Hammer5Tools.exe'))
-        print('Hammer5Tools.exe Removed')
+        print(Fore.GREEN + 'Hammer5Tools.exe Removed')
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(Fore.RED + f"An error occurred: {e}")
         success = False
 
     try:
         shutil.move(os.path.join(path, 'Hammer5Tools.exe'), os.path.join(path_program, 'Hammer5Tools.exe'))
-        print('Hammer5Tools.exe Moved')
+        print(Fore.GREEN + 'Hammer5Tools.exe Moved')
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(Fore.RED + f"An error occurred: {e}")
         success = False
 
     return success
@@ -86,7 +88,7 @@ def update_files(path, path_program):
 def main():
     kill_main_app()
 
-    def init_paths (dev=False):
+    def init_paths(dev=False):
         if dev:
             path = 'hammer5tools/DOWNLOAD_TMP'
             path_program = os.path.normpath("D:/CG/Projects/Other/Hammer5Tools/hammer5tools")
@@ -106,7 +108,7 @@ def main():
         updater_version = file.read().strip()
 
     if updater_version > version:
-        print('A newer version of updater is available. Please update your program manually.')
+        print(Fore.YELLOW + 'A newer version of updater is available. Please update your program manually.')
         webbrowser.open('https://github.com/dertwist/Hammer5Tools/releases/latest')
         shutil.rmtree(path)
         input('Press Enter to close updater...')
@@ -115,19 +117,15 @@ def main():
     if os.path.exists(path):
         success = update_files(path, path_program)
         if success:
-            print("Successful update")
+            print(Fore.GREEN + "Successful update")
             os.startfile(os.path.join(path_program, 'Hammer5Tools.exe'))
         else:
-            print("Update was unsuccessful, try to update manually")
+            print(Fore.RED + "Update was unsuccessful, try to update manually")
             input('Press Enter to close updater...')
         shutil.rmtree(path)
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    root.withdraw()  # Hide the main window
     try:
         main()
     except Exception as e:
-        messagebox.showerror("Error", f"An error occurred: {e}")
-    finally:
-        root.destroy()
+        print(Fore.RED + f"An error occurred: {e}")
