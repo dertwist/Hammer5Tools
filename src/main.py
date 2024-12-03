f415b8566b394b8b397adfcaa7a93c22fa3d50d2
dertwist <45437750+dertwist@users.noreply.github.com>
2024-12-03 17:10:18 +0100

    Launch options update

--- src/main.py
+++ src/main.py
@@ -26,7 +26,7 @@ stop_discord_thread = threading.Event()
 LOCK_FILE = os.path.join(tempfile.gettempdir(), 'hammer5tools.lock')
 
 # Versions
-app_version = '3.0.1'
+app_version = '3.1.0'
 batchcreator_version = '1.2.2'
 soundevent_editor_version = '2.0.0'
 smartprop_editor_version = '0.9.4'
