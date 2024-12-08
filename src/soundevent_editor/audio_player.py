a5998b434234844904da6f40d5ed4768800fd6a8
dertwist <45437750+dertwist@users.noreply.github.com>
2024-12-08 22:15:38 +0100

    audioplayer update

--- src/soundevent_editor/audio_player.py
+++ src/soundevent_editor/audio_player.py
@@ -16,6 +16,8 @@ class AudioPlayer(QWidget):
         self.audio_output = QAudioOutput()
         self.audio_player.setAudioOutput(self.audio_output)
 
+        self.duration = "00:00"
+
         self.init_ui()
         self.setup_connections()
         self.filepath = None
@@ -24,33 +26,22 @@ class AudioPlayer(QWidget):
             self.set_audiopath(file_path)
 
     def init_ui(self):
-        self.layout = QVBoxLayout(self)
 
-        self.play_button = QPushButton("Play")
-        self.stop_button = QPushButton("Stop")
-        self.loop_checkbox = QCheckBox("Loop")
-        self.timeline_slider = QSlider(Qt.Horizontal)
         self.current_time_label = QLabel("00:00")
         self.total_time_label = QLabel("00:00")
 
         self.loop_enabled = get_config_bool('SoundEventEditor', 'AudioPlayerLoop')
-        self.loop_checkbox.setChecked(self.loop_enabled)
+        self.ui.loop_checkbox.setChecked(self.loop_enabled)
 
-        self.layout.addWidget(self.play_button)
-        self.layout.addWidget(self.stop_button)
-        self.layout.addWidget(self.loop_checkbox)
-        self.layout.addWidget(self.timeline_slider)
-        self.layout.addWidget(self.current_time_label)
-        self.layout.addWidget(self.total_time_label)
 
         self.timer = QTimer(self)
         self.timer.setInterval(1000)
 
     def setup_connections(self):
-        self.play_button.clicked.connect(self.play_sound)
-        self.stop_button.clicked.connect(self.stop_sound)
-        self.loop_checkbox.stateChanged.connect(self.toggle_loop)
-        self.timeline_slider.sliderMoved.connect(self.seek_position)
+        self.ui.play_button.clicked.connect(self.play_sound)
+        self.ui.stop_button.clicked.connect(self.stop_sound)
+        self.ui.loop_checkbox.stateChanged.connect(self.toggle_loop)
+        self.ui.timeline_slider.sliderMoved.connect(self.seek_position)
         self.audio_player.positionChanged.connect(self.update_position)
         self.audio_player.durationChanged.connect(self.update_duration)
         self.audio_player.mediaStatusChanged.connect(self.handle_media_status)
@@ -61,6 +52,8 @@ class AudioPlayer(QWidget):
             self.audio_player.setSource(self.filepath)
             self.audio_player.play()
             self.timer.start()
+            self.update_time_labels()
+            self.duration = self.format_time(self.audio_player.duration())
 
     def set_audiopath(self, path):
         self.filepath = QUrl.fromLocalFile(path)
@@ -71,22 +64,21 @@ class AudioPlayer(QWidget):
 
     def toggle_loop(self, state):
         self.loop_enabled = state == Qt.Checked
-        set_config_bool('SoundEventEditor', 'AudioPlayerLoop', self.loop_checkbox.isChecked())
-        print(get_config_bool('SoundEventEditor', 'AudioPlayerLoop'))
+        set_config_bool('SoundEventEditor', 'AudioPlayerLoop', self.ui.loop_checkbox.isChecked())
 
     def seek_position(self, position):
         self.audio_player.setPosition(position)
 
     def update_position(self, position):
-        self.timeline_slider.setValue(position)
+        self.ui.timeline_slider.setValue(position)
 
     def update_duration(self, duration):
-        self.timeline_slider.setRange(0, duration)
-        self.total_time_label.setText(self.format_time(duration))
+        self.ui.timeline_slider.setRange(0, duration)
+        self.duration = self.format_time(duration)
 
     def update_time_labels(self):
         current_time = self.audio_player.position()
-        self.current_time_label.setText(self.format_time(current_time))
+        self.ui.time.setText(f"{self.format_time(current_time)} : {self.duration}")
 
     def handle_media_status(self, status):
         if status == QMediaPlayer.EndOfMedia and self.loop_enabled:
