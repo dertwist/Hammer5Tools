6c843dbd33c7f0cf55b76167d9b68a98d41bb7b6
dertwist <45437750+dertwist@users.noreply.github.com>
2024-12-09 10:02:37 +0100

    [SPE] Updated float property

--- src/smartprop_editor/properties_classes/float.py
+++ src/smartprop_editor/properties_classes/float.py
@@ -1,16 +1,17 @@
 import ast
 import re
-from smartprop_editor.properties_classes.ui_float import Ui_Widget
+from src.smartprop_editor.properties_classes.ui_float import Ui_Widget
 from src.completer.main import CompletingPlainTextEdit
 from PySide6.QtWidgets import QSizePolicy, QSpacerItem, QHBoxLayout, QWidget
 from PySide6.QtCore import Signal
-from smartprop_editor.objects import expression_completer
-from widgets import FloatWidget
+from src.smartprop_editor.objects import expression_completer
+from src.widgets import FloatWidget, ComboboxVariablesWidget
 
 
 class PropertyFloat(QWidget):
     edited = Signal()
-    def __init__(self, value_class, value, variables_scrollArea, int_bool=False,slider_range=[0,0]):
+
+    def __init__(self, value_class, value, variables_scrollArea, int_bool=False, slider_range=[0, 0]):
         super().__init__()
         self.ui = Ui_Widget()
         self.ui.setupUi(self)
@@ -19,7 +20,6 @@ class PropertyFloat(QWidget):
         self.value = value
         self.int_bool = int_bool
         self.variables_scrollArea = variables_scrollArea
-
 
         # Float widget setup
         self.float_widget = FloatWidget(slider_range=slider_range, int_output=int_bool)
@@ -31,40 +31,52 @@ class PropertyFloat(QWidget):
         self.spacer = QWidget()
         spacer_layout = QHBoxLayout()
         spacer_layout.addSpacerItem(spacer_item)
-        spacer_layout.setContentsMargins(0,0,0,0)
+        spacer_layout.setContentsMargins(0, 0, 0, 0)
         self.spacer.setLayout(spacer_layout)
         self.spacer.setStyleSheet('border:None;')
-        self.spacer.setContentsMargins(0,0,0,0)
+        self.spacer.setContentsMargins(0, 0, 0, 0)
         self.ui.layout.addWidget(self.spacer)
 
+        # Set styles based on int_bool
         if self.int_bool:
             self.ui.logic_switch.setItemText(1, 'Int')
             self.ui.property_class.setStyleSheet("""
-                        border:0px;
-            background-color: rgba(255, 255, 255, 0);
-            font: 8pt "Segoe UI";
-            padding-right: 16px;
-            
-            color: rgb(108, 135, 255);
-                        """)
+                border:0px;
+                background-color: rgba(255, 255, 255, 0);
+                font: 8pt "Segoe UI";
+                padding-right: 16px;
+                color: rgb(108, 135, 255);
+            """)
         else:
             self.ui.property_class.setStyleSheet("""
-                        border:0px;
-            background-color: rgba(255, 255, 255, 0);
-            font: 8pt "Segoe UI";
-            padding-right: 16px;
+                border:0px;
+                background-color: rgba(255, 255, 255, 0);
+                font: 8pt "Segoe UI";
+                padding-right: 16px;
+                color: rgb(181, 255, 239);
+            """)
 
-            color: rgb(181, 255, 239);
-                        """)
-
-
+        # Process value_class
         output = re.sub(r'm_fl|m_n|m_b|m_', '', self.value_class)
         output = re.sub(r'([a-z0-9])([A-Z])', r'\1 \2', output)
-
         self.ui.property_class.setText(output)
         self.ui.logic_switch.currentTextChanged.connect(self.on_changed)
 
-        # EditLine
+
+        # Variable setup
+        self.variable = ComboboxVariablesWidget(layout=self.variables_scrollArea, filter_types=['Int', 'Float'])
+        self.variable.setFixedWidth(256)
+        layout = QHBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(self.variable)
+        layout.addSpacerItem(QSpacerItem(0, 0, QSizePolicy.Expanding, QSizePolicy.Minimum))
+        self.variable_frame = QWidget()
+        self.variable_frame.setLayout(layout)
+        self.variable.combobox.changed.connect(self.on_changed)
+        self.ui.layout.insertWidget(2, self.variable_frame)
+
+
+        # EditLine setup
         self.text_line = CompletingPlainTextEdit()
         self.text_line.completion_tail = ''
         self.text_line.OnlyFloat = True
@@ -76,6 +88,7 @@ class PropertyFloat(QWidget):
         self.text_line.setPlainText('0')
 
 
+        # Initialize value
         if isinstance(value, dict):
             if 'm_Expression' in value:
                 self.ui.logic_switch.setCurrentIndex(3)
@@ -84,10 +97,11 @@ class PropertyFloat(QWidget):
             if 'm_SourceName' in value:
                 self.ui.logic_switch.setCurrentIndex(2)
                 self.var_value = value['m_SourceName']
-                self.text_line.setPlainText(str(self.var_value))
+                self.variable.combobox.set_variable(str(self.var_value))
         elif isinstance(value, float) or isinstance(value, int):
             self.ui.logic_switch.setCurrentIndex(1)
             self.float_widget.set_value(value)
+
         self.on_changed()
 
     def logic_switch(self):
@@ -95,14 +109,22 @@ class PropertyFloat(QWidget):
             self.text_line.OnlyFloat = False
             self.text_line.hide()
             self.float_widget.hide()
+            self.variable_frame.hide()
             self.spacer.show()
         elif self.ui.logic_switch.currentIndex() == 1:
             self.text_line.hide()
             self.float_widget.show()
+            self.variable_frame.hide()
+            self.spacer.hide()
+        elif self.ui.logic_switch.currentIndex() == 2:
+            self.text_line.hide()
+            self.float_widget.hide()
+            self.variable_frame.show()
             self.spacer.hide()
         else:
             self.text_line.OnlyFloat = False
             self.text_line.show()
+            self.variable_frame.hide()
             self.float_widget.hide()
             self.spacer.hide()
 
@@ -126,7 +148,7 @@ class PropertyFloat(QWidget):
                 self.value = {self.value_class: float(value)}
         # Variable
         elif self.ui.logic_switch.currentIndex() == 2:
-            value = self.text_line.toPlainText()
+            value = self.variable.combobox.get_variable()
             try:
                 value = ast.literal_eval(value)
             except:
@@ -142,7 +164,6 @@ class PropertyFloat(QWidget):
             self.value = {self.value_class: {'m_Expression': str(value)}}
 
     def get_variables(self, search_term=None):
-        self.variables_scrollArea
         data_out = []
         for i in range(self.variables_scrollArea.count()):
             widget = self.variables_scrollArea.itemAt(i).widget()
