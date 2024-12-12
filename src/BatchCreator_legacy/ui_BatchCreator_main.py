e1fcc05cb440080c0acab5a864767820a71d2adb
dertwist <45437750+dertwist@users.noreply.github.com>
2024-12-12 18:10:11 +0100

    audio player removed fixed width. Batchcreator added varialbes to the context menu

--- src/BatchCreator_legacy/ui_BatchCreator_main.py
+++ src/BatchCreator_legacy/ui_BatchCreator_main.py
@@ -168,46 +168,6 @@ class Ui_BatchCreator_MainWindow(object):
         self.horizontalLayout = QHBoxLayout(self.frame_2)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-        self.create_file = QPushButton(self.frame_2)
-        self.create_file.setObjectName(u"create_file")
-        self.create_file.setMinimumSize(QSize(0, 18))
-        self.create_file.setStyleSheet(u"\n"
-"    /* QPushButton default and hover styles */\n"
-"    QPushButton {\n"
-"\n"
-"        font: 600 10pt \"Segoe UI\";\n"
-"	\n"
-"\n"
-"        border: 2px solid black;\n"
-"        border-radius: 4px;\n"
-"        border-color: rgba(80, 80, 80, 255);\n"
-"        height:22px;\n"
-"        padding-top: 2px;\n"
-"        padding-bottom:2px;\n"
-"        padding-left: 4px;\n"
-"        padding-right: 4px;\n"
-"        color: #E3E3E3;\n"
-"        background-color: #1C1C1C;\n"
-"    }\n"
-"    QPushButton:hover {\n"
-"        background-color: #414956;\n"
-"        color: white;\n"
-"    }\n"
-"    QPushButton:pressed {\n"
-"        background-color: red;\n"
-"        background-color: #1C1C1C;\n"
-"        margin: 1 px;\n"
-"        margin-left: 2px;\n"
-"        margin-right: 2px;\n"
-"\n"
-"    }")
-        icon = QIcon()
-        icon.addFile(u":/icons/add_24dp_9D9D9D_FILL0_wght400_GRAD0_opsz24.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
-        self.create_file.setIcon(icon)
-        self.create_file.setIconSize(QSize(20, 20))
-
-        self.horizontalLayout.addWidget(self.create_file)
-
         self.open_button = QPushButton(self.frame_2)
         self.open_button.setObjectName(u"open_button")
         self.open_button.setMinimumSize(QSize(0, 18))
@@ -241,9 +201,9 @@ class Ui_BatchCreator_MainWindow(object):
 "        margin-right: 2px;\n"
 "\n"
 "    }")
-        icon1 = QIcon()
-        icon1.addFile(u":/icons/file_open_16dp_9D9D9D_FILL0_wght400_GRAD0_opsz20.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
-        self.open_button.setIcon(icon1)
+        icon = QIcon()
+        icon.addFile(u":/icons/file_open_16dp_9D9D9D_FILL0_wght400_GRAD0_opsz20.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.open_button.setIcon(icon)
         self.open_button.setIconSize(QSize(20, 20))
 
         self.horizontalLayout.addWidget(self.open_button)
@@ -281,12 +241,52 @@ class Ui_BatchCreator_MainWindow(object):
 "        margin-right: 2px;\n"
 "\n"
 "    }")
-        icon2 = QIcon()
-        icon2.addFile(u":/icons/save_16dp_9D9D9D_FILL0_wght400_GRAD0_opsz20.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
-        self.save_button.setIcon(icon2)
+        icon1 = QIcon()
+        icon1.addFile(u":/icons/save_16dp_9D9D9D_FILL0_wght400_GRAD0_opsz20.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.save_button.setIcon(icon1)
         self.save_button.setIconSize(QSize(20, 20))
 
         self.horizontalLayout.addWidget(self.save_button)
+
+        self.create_file = QPushButton(self.frame_2)
+        self.create_file.setObjectName(u"create_file")
+        self.create_file.setMinimumSize(QSize(0, 18))
+        self.create_file.setStyleSheet(u"\n"
+"    /* QPushButton default and hover styles */\n"
+"    QPushButton {\n"
+"\n"
+"        font: 600 10pt \"Segoe UI\";\n"
+"	\n"
+"\n"
+"        border: 2px solid black;\n"
+"        border-radius: 4px;\n"
+"        border-color: rgba(80, 80, 80, 255);\n"
+"        height:22px;\n"
+"        padding-top: 2px;\n"
+"        padding-bottom:2px;\n"
+"        padding-left: 4px;\n"
+"        padding-right: 4px;\n"
+"        color: #E3E3E3;\n"
+"        background-color: #1C1C1C;\n"
+"    }\n"
+"    QPushButton:hover {\n"
+"        background-color: #414956;\n"
+"        color: white;\n"
+"    }\n"
+"    QPushButton:pressed {\n"
+"        background-color: red;\n"
+"        background-color: #1C1C1C;\n"
+"        margin: 1 px;\n"
+"        margin-left: 2px;\n"
+"        margin-right: 2px;\n"
+"\n"
+"    }")
+        icon2 = QIcon()
+        icon2.addFile(u":/icons/add_24dp_9D9D9D_FILL0_wght400_GRAD0_opsz24.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.create_file.setIcon(icon2)
+        self.create_file.setIconSize(QSize(20, 20))
+
+        self.horizontalLayout.addWidget(self.create_file)
 
 
         self.verticalLayout_5.addWidget(self.frame_2)
@@ -419,9 +419,9 @@ class Ui_BatchCreator_MainWindow(object):
         self.assets_name_template.setText(QCoreApplication.translate("BatchCreator_MainWindow", u"Asset name", None))
         self.dockWidget.setWindowTitle(QCoreApplication.translate("BatchCreator_MainWindow", u"Explorer", None))
         self.groupBox_2.setTitle(QCoreApplication.translate("BatchCreator_MainWindow", u"File", None))
-        self.create_file.setText(QCoreApplication.translate("BatchCreator_MainWindow", u"Create", None))
         self.open_button.setText(QCoreApplication.translate("BatchCreator_MainWindow", u"Open", None))
         self.save_button.setText(QCoreApplication.translate("BatchCreator_MainWindow", u"Save", None))
+        self.create_file.setText(QCoreApplication.translate("BatchCreator_MainWindow", u"Create", None))
         self.groupBox.setTitle(QCoreApplication.translate("BatchCreator_MainWindow", u"Process", None))
         self.process_options_button.setText(QCoreApplication.translate("BatchCreator_MainWindow", u"Options", None))
         self.process_all_button.setText(QCoreApplication.translate("BatchCreator_MainWindow", u"Process", None))
