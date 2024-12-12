ae76ab51e47bdcca3c96a0436baad5613927cd84
dertwist <45437750+dertwist@users.noreply.github.com>
2024-12-12 19:18:12 +0100

    batchcreator added return button

--- src/BatchCreator_legacy/ui_BatchCreator_main.py
+++ src/BatchCreator_legacy/ui_BatchCreator_main.py
@@ -347,6 +347,53 @@ class Ui_BatchCreator_MainWindow(object):
 
         self.horizontalLayout_5.addWidget(self.process_options_button)
 
+        self.return_button = QPushButton(self.frame_3)
+        self.return_button.setObjectName(u"return_button")
+        self.return_button.setEnabled(False)
+        self.return_button.setMinimumSize(QSize(0, 18))
+        self.return_button.setStyleSheet(u"\n"
+"    /* QPushButton default and hover styles */\n"
+"    QPushButton {\n"
+"\n"
+"        font: 600 10pt \"Segoe UI\";\n"
+"	\n"
+"\n"
+"        border: 2px solid black;\n"
+"        border-radius: 4px;\n"
+"        border-color: rgba(80, 80, 80, 255);\n"
+"        height:22px;\n"
+"        padding-top: 2px;\n"
+"        padding-bottom:2px;\n"
+"        padding-left: 4px;\n"
+"        padding-right: 4px;\n"
+"        color: #E3E3E3;\n"
+"        background-color: #1C1C1C;\n"
+"    }\n"
+"    QPushButton:hover {\n"
+"        background-color: #414956;\n"
+"        color: white;\n"
+"    }\n"
+"    QPushButton:pressed {\n"
+"        background-color: red;\n"
+"        background-color: #1C1C1C;\n"
+"        margin: 1 px;\n"
+"        margin-left: 2px;\n"
+"        margin-right: 2px;\n"
+"	\n"
+"\n"
+"    }\n"
+"QPushButton:disabled {\n"
+"color: gray;\n"
+" background-color: #2C2C2C;\n"
+"}")
+        icon4 = QIcon()
+        icon4.addFile(u":/icons/undo_24dp_9D9D9D_FILL0_wght400_GRAD0_opsz24.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.return_button.setIcon(icon4)
+        self.return_button.setIconSize(QSize(20, 20))
+        self.return_button.setCheckable(False)
+
+        self.horizontalLayout_5.addWidget(self.return_button)
+
         self.process_all_button = QPushButton(self.frame_3)
         self.process_all_button.setObjectName(u"process_all_button")
         self.process_all_button.setMinimumSize(QSize(0, 18))
@@ -380,9 +427,9 @@ class Ui_BatchCreator_MainWindow(object):
 "        margin-right: 2px;\n"
 "\n"
 "    }")
-        icon4 = QIcon()
-        icon4.addFile(u":/icons/tab_move_24dp_9D9D9D_FILL0_wght400_GRAD0_opsz24.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
-        self.process_all_button.setIcon(icon4)
+        icon5 = QIcon()
+        icon5.addFile(u":/icons/tab_move_24dp_9D9D9D_FILL0_wght400_GRAD0_opsz24.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.process_all_button.setIcon(icon5)
         self.process_all_button.setIconSize(QSize(20, 20))
 
         self.horizontalLayout_5.addWidget(self.process_all_button)
@@ -424,6 +471,7 @@ class Ui_BatchCreator_MainWindow(object):
         self.create_file.setText(QCoreApplication.translate("BatchCreator_MainWindow", u"Create", None))
         self.groupBox.setTitle(QCoreApplication.translate("BatchCreator_MainWindow", u"Process", None))
         self.process_options_button.setText(QCoreApplication.translate("BatchCreator_MainWindow", u"Options", None))
+        self.return_button.setText(QCoreApplication.translate("BatchCreator_MainWindow", u"Return", None))
         self.process_all_button.setText(QCoreApplication.translate("BatchCreator_MainWindow", u"Process", None))
         self.extension_lineEdit.setPlaceholderText(QCoreApplication.translate("BatchCreator_MainWindow", u"File extension (vmdl, vmat etc).", None))
     # retranslateUi
