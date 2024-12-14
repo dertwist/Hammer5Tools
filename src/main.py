0da4abb198d681f927048eafc798bc7a19142532
dertwist <45437750+dertwist@users.noreply.github.com>
2024-12-14 17:00:14 +0100

    Update check updates dialog

--- src/main.py
+++ src/main.py
@@ -26,7 +26,7 @@ stop_discord_thread = threading.Event()
 LOCK_FILE = os.path.join(tempfile.gettempdir(), 'hammer5tools.lock')
 
 # Versions
-app_version = '3.2.0'
+app_version = '3.4.0'
 
 
 class Notification(QMessageBox):
