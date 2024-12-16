d5634e61fbdbae0a003c61dc8fe749685a56cda4
dertwist <45437750+dertwist@users.noreply.github.com>
2024-12-16 13:56:51 +0100

    update stylesheets for batchcreator and improving format reading and writing. Broken drag and drop variables

--- src/BatchCreator_legacy/objects.py
+++ src/BatchCreator_legacy/objects.py
@@ -4,9 +4,9 @@ default_file = {
             'ignore_list': '',
             'custom_files': [],
             'custom_output': 'relative_path',
-            'load_from_the_folder': 'True',
-            'algorithm': '0',
-            'output_to_the_folder': 'True',
+            'load_from_the_folder': True,
+            'output_to_the_folder': True,
+            'algorithm': 0,
             'ignore_extensions': 'blend,vmdl,vmat,vsmart, blend1'
         }
     }
