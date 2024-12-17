dd886a9d5e1456e4586f330e38fc63b5a69179c8
dertwist <45437750+dertwist@users.noreply.github.com>
2024-12-17 10:31:31 +0100

    moved extension key to process

--- src/batch_creator/process.py
+++ src/batch_creator/process.py
@@ -1,5 +1,7 @@
 import os
 import json
+
+from src.batch_creator.objects import default_file
 from src.preferences import get_addon_name, get_cs2_path
 from src.batch_creator.common import extract_base_names, extract_base_names_underscore
 from PySide6.QtWidgets import QMessageBox
@@ -8,8 +10,8 @@ def perform_batch_processing(file_path, process, preview):
     base_directory = os.path.join(get_cs2_path(), 'content', 'csgo_addons', get_addon_name())
     batch_directory = os.path.splitext(file_path)[0]
     relative_batch_path = os.path.relpath(batch_directory, base_directory).replace('\\', '/')
-    algorithm = int(process.get('algorithm', 0))
-    file_extension = get_file_extension(file_path)
+    algorithm = int(process.get('algorithm', default_file['process']['algorithm']))
+    file_extension = process.get('extension', default_file['process']['extension'])
     ignore_extensions = [ext.strip() for ext in process.get('ignore_extensions', '').split(',')]
 
     files_to_process = search_files(batch_directory, algorithm, ignore_extensions, process) if process.get('load_from_the_folder') else process.get('custom_files', [])
@@ -24,19 +26,11 @@ def perform_batch_processing(file_path, process, preview):
         execute_file_creation(files_to_process, output_directory, relative_batch_path, file_extension, created_files, file_path)
         return created_files
 
-def get_file_extension(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            data = json.load(file)
-            return data.get('FILE', {}).get('extension', 'vmdl')
-    except (json.JSONDecodeError, FileNotFoundError):
-        return 'vmdl'
-
 def execute_file_creation(files, output_path, relative_path, extension, created_files, template_file):
     try:
         with open(template_file, 'r') as file:
             data = json.load(file)
-            content_template = data.get('FILE', {}).get('content', '')
+            content_template = data.get('file', {}).get('content', '')
     except Exception as e:
         QMessageBox.critical(None, "Processing Error", f"Error reading batch file: {e}")
         return
