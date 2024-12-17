918871a3ae9f9f9d1ea26e0ac81a4a5628849442
dertwist <45437750+dertwist@users.noreply.github.com>
2024-12-17 11:33:41 +0100

    batch creator dialog stylesheet update

--- src/batch_creator/ui_dialog.py
+++ src/batch_creator/ui_dialog.py
@@ -34,38 +34,36 @@ class Ui_BatchCreator_process_Dialog(object):
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.frame_4 = QFrame(BatchCreator_process_Dialog)
         self.frame_4.setObjectName(u"frame_4")
-        self.frame_4.setFrameShape(QFrame.StyledPanel)
-        self.frame_4.setFrameShadow(QFrame.Raised)
+        self.frame_4.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame_4.setFrameShadow(QFrame.Shadow.Raised)
         self.horizontalLayout = QHBoxLayout(self.frame_4)
         self.horizontalLayout.setSpacing(6)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
         self.frame_3 = QFrame(self.frame_4)
         self.frame_3.setObjectName(u"frame_3")
-        self.frame_3.setFrameShape(QFrame.StyledPanel)
-        self.frame_3.setFrameShadow(QFrame.Raised)
+        self.frame_3.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame_3.setFrameShadow(QFrame.Shadow.Raised)
         self.verticalLayout_3 = QVBoxLayout(self.frame_3)
         self.verticalLayout_3.setSpacing(6)
         self.verticalLayout_3.setObjectName(u"verticalLayout_3")
         self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
         self.frame_5 = QFrame(self.frame_3)
         self.frame_5.setObjectName(u"frame_5")
-        self.frame_5.setFrameShape(QFrame.StyledPanel)
-        self.frame_5.setFrameShadow(QFrame.Raised)
+        self.frame_5.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame_5.setFrameShadow(QFrame.Shadow.Raised)
         self.horizontalLayout_3 = QHBoxLayout(self.frame_5)
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
         self.horizontalLayout_3.setContentsMargins(0, 0, 0, 0)
         self.select_files_to_process_button = QPushButton(self.frame_5)
         self.select_files_to_process_button.setObjectName(u"select_files_to_process_button")
-        self.select_files_to_process_button.setStyleSheet(u"\n"
-"    /* QPushButton default and hover styles */\n"
-"    QPushButton {\n"
+        self.select_files_to_process_button.setStyleSheet(u"    QPushButton {\n"
 "\n"
-"        font: 600 10pt \"Segoe UI\";\n"
+"        font: 580 9pt \"Segoe UI\";\n"
 "	\n"
 "\n"
 "        border: 2px solid black;\n"
-"        border-radius: 4px;\n"
+"        border-radius: 2px;\n"
 "        border-color: rgba(80, 80, 80, 255);\n"
 "        height:22px;\n"
 "        padding-top: 2px;\n"
@@ -86,42 +84,44 @@ class Ui_BatchCreator_process_Dialog(object):
 "        margin-left: 2px;\n"
 "        margin-right: 2px;\n"
 "\n"
-"    }")
+"    }\n"
+"QPushButton:disabled {\n"
+"color: gray;\n"
+" background-color: #2C2C2C;\n"
+"}")
 
         self.horizontalLayout_3.addWidget(self.select_files_to_process_button)
 
         self.load_from_the_folder_checkBox = QCheckBox(self.frame_5)
         self.load_from_the_folder_checkBox.setObjectName(u"load_from_the_folder_checkBox")
-        self.load_from_the_folder_checkBox.setFocusPolicy(Qt.NoFocus)
+        self.load_from_the_folder_checkBox.setFocusPolicy(Qt.FocusPolicy.NoFocus)
         self.load_from_the_folder_checkBox.setStyleSheet(u"QCheckBox {\n"
-"    font: 700 10pt \"Segoe UI\";\n"
-"    border: 2px solid black;\n"
-"    border-radius: 4px;\n"
-"    border-color: rgba(80, 80, 80, 255);\n"
-"    height:14px;\n"
-"    padding-top: 2px;\n"
-"    padding-bottom:2px;\n"
-"    color: #E3E3E3;\n"
-"    background-color: #1C1C1C;\n"
-"    padding-left: 4px;\n"
-"}\n"
 "\n"
-"QCheckBox::indicator:unchecked {\n"
-"    image: url(://icons/check_box_outline_blank_16dp_9D9D9D_FILL0_wght400_GRAD0_opsz20.svg);\n"
-"}\n"
+"        font: 580 10pt \"Segoe UI\";\n"
 "\n"
-"QCheckBox::indicator:checked {\n"
-"    image: url(://icons/select_check_box_16dp_9D9D9D_FILL0_wght400_GRAD0_opsz20.svg);\n"
-"}\n"
+"        border: 2px solid black;\n"
+"        border-radius: 2px;\n"
+"        border-color: rgba(80, 80, 80, 255);\n"
+"        height:22px;\n"
+"        padding-top: 2px;\n"
+"        padding-bottom:2px;\n"
+"        padding-left: 4px;\n"
+"        padding-right: 4px;\n"
+"        color: #E3E3E3;\n"
+"        background-color: #1C1C1C;\n"
+"    }\n"
+"    QCheckBox:hover {\n"
+"        background-color: #414956;\n"
+"        color: white;\n"
+"    }\n"
+"    QCheckBox:pressed {\n"
 "\n"
-"/* -------------------------- */\n"
+"    }\n"
 "\n"
-"\n"
-"QCheckBox:hover {\n"
-"    background-color: #414956;\n"
-"    color: white;\n"
-"}\n"
-"")
+"QCheckBox:disabled {\n"
+"color: gray;\n"
+" background-color: #2C2C2C;\n"
+"}")
         self.load_from_the_folder_checkBox.setIconSize(QSize(20, 20))
         self.load_from_the_folder_checkBox.setChecked(True)
 
@@ -150,29 +150,27 @@ class Ui_BatchCreator_process_Dialog(object):
 
         self.frame_2 = QFrame(self.frame_4)
         self.frame_2.setObjectName(u"frame_2")
-        self.frame_2.setFrameShape(QFrame.StyledPanel)
-        self.frame_2.setFrameShadow(QFrame.Raised)
+        self.frame_2.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame_2.setFrameShadow(QFrame.Shadow.Raised)
         self.verticalLayout_2 = QVBoxLayout(self.frame_2)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
         self.frame_6 = QFrame(self.frame_2)
         self.frame_6.setObjectName(u"frame_6")
-        self.frame_6.setFrameShape(QFrame.StyledPanel)
-        self.frame_6.setFrameShadow(QFrame.Raised)
+        self.frame_6.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame_6.setFrameShadow(QFrame.Shadow.Raised)
         self.horizontalLayout_4 = QHBoxLayout(self.frame_6)
         self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
         self.horizontalLayout_4.setContentsMargins(0, 0, 0, 0)
         self.choose_output_button = QPushButton(self.frame_6)
         self.choose_output_button.setObjectName(u"choose_output_button")
-        self.choose_output_button.setStyleSheet(u"\n"
-"    /* QPushButton default and hover styles */\n"
-"    QPushButton {\n"
+        self.choose_output_button.setStyleSheet(u"    QPushButton {\n"
 "\n"
-"        font: 600 10pt \"Segoe UI\";\n"
+"        font: 580 9pt \"Segoe UI\";\n"
 "	\n"
 "\n"
 "        border: 2px solid black;\n"
-"        border-radius: 4px;\n"
+"        border-radius: 2px;\n"
 "        border-color: rgba(80, 80, 80, 255);\n"
 "        height:22px;\n"
 "        padding-top: 2px;\n"
@@ -193,41 +191,43 @@ class Ui_BatchCreator_process_Dialog(object):
 "        margin-left: 2px;\n"
 "        margin-right: 2px;\n"
 "\n"
-"    }")
+"    }\n"
+"QPushButton:disabled {\n"
+"color: gray;\n"
+" background-color: #2C2C2C;\n"
+"}")
 
         self.horizontalLayout_4.addWidget(self.choose_output_button)
 
         self.output_to_the_folder_checkBox = QCheckBox(self.frame_6)
         self.output_to_the_folder_checkBox.setObjectName(u"output_to_the_folder_checkBox")
         self.output_to_the_folder_checkBox.setStyleSheet(u"QCheckBox {\n"
-"    font: 700 10pt \"Segoe UI\";\n"
-"    border: 2px solid black;\n"
-"    border-radius: 4px;\n"
-"    border-color: rgba(80, 80, 80, 255);\n"
-"    height:14px;\n"
-"    padding-top: 2px;\n"
-"    padding-bottom:2px;\n"
-"    color: #E3E3E3;\n"
-"    background-color: #1C1C1C;\n"
-"    padding-left: 4px;\n"
-"}\n"
 "\n"
-"QCheckBox::indicator:unchecked {\n"
-"    image: url(://icons/check_box_outline_blank_16dp_9D9D9D_FILL0_wght400_GRAD0_opsz20.svg);\n"
-"}\n"
+"        font: 580 10pt \"Segoe UI\";\n"
 "\n"
-"QCheckBox::indicator:checked {\n"
-"    image: url(://icons/select_check_box_16dp_9D9D9D_FILL0_wght400_GRAD0_opsz20.svg);\n"
-"}\n"
+"        border: 2px solid black;\n"
+"        border-radius: 2px;\n"
+"        border-color: rgba(80, 80, 80, 255);\n"
+"        height:22px;\n"
+"        padding-top: 2px;\n"
+"        padding-bottom:2px;\n"
+"        padding-left: 4px;\n"
+"        padding-right: 4px;\n"
+"        color: #E3E3E3;\n"
+"        background-color: #1C1C1C;\n"
+"    }\n"
+"    QCheckBox:hover {\n"
+"        background-color: #414956;\n"
+"        color: white;\n"
+"    }\n"
+"    QCheckBox:pressed {\n"
 "\n"
-"/* -------------------------- */\n"
+"    }\n"
 "\n"
-"\n"
-"QCheckBox:hover {\n"
-"    background-color: #414956;\n"
-"    color: white;\n"
-"}\n"
-"")
+"QCheckBox:disabled {\n"
+"color: gray;\n"
+" background-color: #2C2C2C;\n"
+"}")
         self.output_to_the_folder_checkBox.setIconSize(QSize(20, 20))
         self.output_to_the_folder_checkBox.setChecked(True)
 
@@ -249,8 +249,8 @@ class Ui_BatchCreator_process_Dialog(object):
 
         self.frame_7 = QFrame(BatchCreator_process_Dialog)
         self.frame_7.setObjectName(u"frame_7")
-        self.frame_7.setFrameShape(QFrame.StyledPanel)
-        self.frame_7.setFrameShadow(QFrame.Raised)
+        self.frame_7.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame_7.setFrameShadow(QFrame.Shadow.Raised)
         self.horizontalLayout_5 = QHBoxLayout(self.frame_7)
         self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
         self.horizontalLayout_5.setContentsMargins(0, 0, 0, 0)
@@ -269,8 +269,8 @@ class Ui_BatchCreator_process_Dialog(object):
 
         self.frame_8 = QFrame(BatchCreator_process_Dialog)
         self.frame_8.setObjectName(u"frame_8")
-        self.frame_8.setFrameShape(QFrame.StyledPanel)
-        self.frame_8.setFrameShadow(QFrame.Raised)
+        self.frame_8.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame_8.setFrameShadow(QFrame.Shadow.Raised)
         self.horizontalLayout_6 = QHBoxLayout(self.frame_8)
         self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
         self.horizontalLayout_6.setContentsMargins(0, 0, 0, 0)
@@ -294,8 +294,8 @@ class Ui_BatchCreator_process_Dialog(object):
 
         self.frame = QFrame(BatchCreator_process_Dialog)
         self.frame.setObjectName(u"frame")
-        self.frame.setFrameShape(QFrame.StyledPanel)
-        self.frame.setFrameShadow(QFrame.Raised)
+        self.frame.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Shadow.Raised)
         self.horizontalLayout_2 = QHBoxLayout(self.frame)
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
         self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
@@ -390,15 +390,13 @@ class Ui_BatchCreator_process_Dialog(object):
 
         self.process_button = QPushButton(self.frame)
         self.process_button.setObjectName(u"process_button")
-        self.process_button.setStyleSheet(u"\n"
-"    /* QPushButton default and hover styles */\n"
-"    QPushButton {\n"
+        self.process_button.setStyleSheet(u"    QPushButton {\n"
 "\n"
-"        font: 600 10pt \"Segoe UI\";\n"
+"        font: 580 9pt \"Segoe UI\";\n"
 "	\n"
 "\n"
 "        border: 2px solid black;\n"
-"        border-radius: 4px;\n"
+"        border-radius: 2px;\n"
 "        border-color: rgba(80, 80, 80, 255);\n"
 "        height:22px;\n"
 "        padding-top: 2px;\n"
@@ -419,7 +417,11 @@ class Ui_BatchCreator_process_Dialog(object):
 "        margin-left: 2px;\n"
 "        margin-right: 2px;\n"
 "\n"
-"    }")
+"    }\n"
+"QPushButton:disabled {\n"
+"color: gray;\n"
+" background-color: #2C2C2C;\n"
+"}")
         icon2 = QIcon()
         icon2.addFile(u":/icons/tab_move_24dp_9D9D9D_FILL0_wght400_GRAD0_opsz24.svg", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
         self.process_button.setIcon(icon2)
