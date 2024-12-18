8c7e32fe8d6d17e787950f19c820d3e6be71c405
dertwist <45437750+dertwist@users.noreply.github.com>
2024-12-18 20:00:46 +0100

    batch creator process base interpretation fix

--- src/batch_creator/common.py
+++ src/batch_creator/common.py
@@ -1,8 +1,2 @@
 import json
 from src.batch_creator.objects import default_file
-
-def extract_base_names(names):
-    return set(name.split('_')[0] for name in names)
-
-def extract_base_names_underscore(names):
-    return set(name.rsplit('_', 1)[0] if '_' in name else name for name in names)
