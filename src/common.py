ad784f419537fda672e8e7cf57dc33ff7b0c6166
dertwist <45437750+dertwist@users.noreply.github.com>
2024-12-24 13:30:55 +0100

    switched to app_dir = os.getcwd()

--- src/common.py
+++ src/common.py
@@ -49,7 +49,7 @@ editor_info = {
     }
 
 # Paths
-app_dir = os.path.dirname(os.path.abspath(__file__))
+app_dir = os.getcwd()
 SoundEventEditor_Preset_Path = os.path.join(app_dir, "SoundEventEditor", "Presets")
 SmartPropEditor_Preset_Path = os.path.join(app_dir, "SmartPropEditor", "Presets")
 Presets_Path = os.path.join(app_dir, "presets")
