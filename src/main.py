933ffc74064630f2fba9d3bdf19654db0d3748a4
dertwist <45437750+dertwist@users.noreply.github.com>
2025-01-04 03:56:43 +0100

    asset group maker

--- src/main.py
+++ src/main.py
@@ -40,7 +40,7 @@ stop_discord_thread = threading.Event()
 LOCK_FILE = os.path.join(tempfile.gettempdir(), 'hammer5tools.lock')
 
 # Versions
-app_version = '3.7.2'
+app_version = '3.8.0'
 
 
 class DevWidget(QWidget):
