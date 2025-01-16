91d6a21e5576ff8d7edd9ee6a630dd97fb504e68
dertwist <45437750+dertwist@users.noreply.github.com>
2025-01-16 14:02:15 +0100

    CurveWidget removed pan view and zoom

--- dev/curve_volume/CurveWidget-master/main.py
+++ dev/curve_volume/CurveWidget-master/main.py
@@ -1,6 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow, QDialog, QVBoxLayout, QPushButton, QGraphicsView, QGraphicsScene, QGraphicsEllipseItem, QGraphicsLineItem
-from PySide6.QtGui import QPainter, QPen, QColor, QMouseEvent, QFont
+from PySide6.QtGui import QPainter, QPen, QColor, QFont, QMouseEvent
 from PySide6.QtCore import Qt, QPointF
 
 class Curve:
@@ -23,11 +23,6 @@ class CurveWidget(QGraphicsView):
         self.setScene(self.scene)
 
         self.curve = Curve()
-        self.zoom_factor = 1.0
-        self.pan_offset = QPointF(0, 0)
-        self.drag_start_pos = QPointF()
-        self.is_dragging_dot = [-1] * 4
-
         self.setRenderHint(QPainter.Antialiasing)
         self.setViewportUpdateMode(QGraphicsView.FullViewportUpdate)
 
@@ -37,13 +32,10 @@ class CurveWidget(QGraphicsView):
         font.setPointSize(8)
         self.debug_text.setFont(font)
 
+        self.selected_dot_index = None
+
     def setCurve(self, curve):
         self.curve = curve
-        self.update_scene()
-
-    def normalizeView(self):
-        self.zoom_factor = 1.0
-        self.pan_offset = QPointF(0, 0)
         self.update_scene()
 
     def update_scene(self):
@@ -55,68 +47,55 @@ class CurveWidget(QGraphicsView):
         for i in range(0, 1001):
             t = i / 1000.0
             point = self.curve.evaluate(t)
-            x = point.x() * width * self.zoom_factor + self.pan_offset.x()
-            y = (1 - point.y()) * height * self.zoom_factor + self.pan_offset.y()
+            x = point.x() * width
+            y = (1 - point.y()) * height
             if 0 <= x <= width and 0 <= y <= height:
                 self.scene.addLine(x - 1, y, x + 1, y, pen)
 
         dot_radius = 6
         pen = QPen(QColor(150, 150, 40), 2)
         for i, dot in enumerate(self.curve.dots):
-            x = dot.x() * width * self.zoom_factor + self.pan_offset.x()
-            y = (1 - dot.y()) * height * self.zoom_factor + self.pan_offset.y()
+            x = dot.x() * width
+            y = (1 - dot.y()) * height
             ellipse = QGraphicsEllipseItem(x - dot_radius, y - dot_radius, 2 * dot_radius, 2 * dot_radius)
-            ellipse.setBrush(QColor(255, 100, 100) if self.is_dragging_dot[i] != -1 else QColor(150, 150, 150))
+            ellipse.setBrush(QColor(255, 100, 100))
             self.scene.addItem(ellipse)
 
             if i == 2 or i == 3:
-                anchor_x = self.curve.dots[i - 2].x() * width * self.zoom_factor + self.pan_offset.x()
-                anchor_y = (1 - self.curve.dots[i - 2].y()) * height * self.zoom_factor + self.pan_offset.y()
+                anchor_x = self.curve.dots[i - 2].x() * width
+                anchor_y = (1 - self.curve.dots[i - 2].y()) * height
                 line = QGraphicsLineItem(anchor_x, anchor_y, x, y)
                 line.setPen(pen)
                 self.scene.addItem(line)
 
-    def wheelEvent(self, event):
-        zoom_in = event.angleDelta().y() > 0
-        zoom_factor = 1.1 if zoom_in else 1 / 1.1
-        self.zoom_factor *= zoom_factor
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
         self.update_scene()
 
-    def mousePressEvent(self, event):
-        if event.button() == Qt.LeftButton:
-            for i, dot in enumerate(self.curve.dots):
-                width = self.width()
-                height = self.height()
-                x = dot.x() * width * self.zoom_factor + self.pan_offset.x()
-                y = (1 - dot.y()) * height * self.zoom_factor + self.pan_offset.y()
-                if (event.position() - QPointF(x, y)).manhattanLength() < 20:
-                    self.is_dragging_dot[i] = i
-                    self.setCursor(Qt.ClosedHandCursor)
-                    return
-
-            self.drag_start_pos = event.position()
-            self.setCursor(Qt.ClosedHandCursor)
-
-    def mouseMoveEvent(self, event):
+    def mousePressEvent(self, event: QMouseEvent):
+        pos = event.position()
         width = self.width()
         height = self.height()
-        if any(drag_idx != -1 for drag_idx in self.is_dragging_dot):
-            for i, dragging in enumerate(self.is_dragging_dot):
-                if dragging != -1:
-                    x = (event.position().x() - self.pan_offset.x()) / (width * self.zoom_factor)
-                    y = 1 - (event.position().y() - self.pan_offset.y()) / (height * self.zoom_factor)
-                    self.curve.dots[i] = QPointF(max(0, min(1, x)), max(0, min(1, y)))
-                    self.update_scene()
-        elif event.buttons() & Qt.LeftButton:
-            delta = event.position() - self.drag_start_pos
-            self.pan_offset += delta
-            self.drag_start_pos = event.position()
+        bounding_box_size = 16
+        for i, dot in enumerate(self.curve.dots):
+            x = dot.x() * width
+            y = (1 - dot.y()) * height
+            if (x - bounding_box_size <= pos.x() <= x + bounding_box_size) and (y - bounding_box_size <= pos.y() <= y + bounding_box_size):
+                self.selected_dot_index = i
+                break
+
+    def mouseMoveEvent(self, event: QMouseEvent):
+        if self.selected_dot_index is not None:
+            pos = event.position()
+            width = self.width()
+            height = self.height()
+            x = pos.x() / width
+            y = 1 - (pos.y() / height)
+            self.curve.dots[self.selected_dot_index] = QPointF(x, y)
             self.update_scene()
 
-    def mouseReleaseEvent(self, event):
-        if event.button() == Qt.LeftButton:
-            self.is_dragging_dot = [-1] * 4
-            self.setCursor(Qt.OpenHandCursor)
+    def mouseReleaseEvent(self, event: QMouseEvent):
+        self.selected_dot_index = None
 
 class CurveWidgetDialog(QDialog):
     def __init__(self, parent=None):
@@ -133,7 +112,7 @@ class CurveWidgetDialog(QDialog):
 
         normalize_button = QPushButton("Normalize View", self)
         layout.addWidget(normalize_button)
-        normalize_button.clicked.connect(self.curve_widget.normalizeView)
+        normalize_button.clicked.connect(self.curve_widget.update_scene)
 
 class MainWindow(QMainWindow):
     def __init__(self):
