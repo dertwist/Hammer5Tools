284ba966931263d4d2b57f38222b374e5341711d
dertwist <45437750+dertwist@users.noreply.github.com>
2025-01-16 10:29:58 +0100

    Renamed folder properties_classes to property in src/smartprop_editor directory.

--- src/smartprop_editor/property/combobox.py
+++ src/smartprop_editor/property/combobox.py
@@ -1,49 +1,49 @@
 import ast
 import re
-from src.smartprop_editor.properties.ui_color import Ui_Widget
-from src.completer.main import CompletingPlainTextEdit
-from PySide6.QtWidgets import QWidget, QColorDialog
-from PySide6.QtCore import Signal
-from PySide6.QtGui import QColor
-from src.qt_styles.qt_global_stylesheet import QT_Stylesheet_global
-
-import ast
-import re
+from src.smartprop_editor.property.ui_combobox import Ui_Widget
 from src.completer.main import CompletingPlainTextEdit
 from PySide6.QtWidgets import QSizePolicy, QSpacerItem, QHBoxLayout, QWidget
 from PySide6.QtCore import Signal
-from src.smartprop_editor.objects import expression_completer
-from src.widgets import FloatWidget, ComboboxVariablesWidget
+from src.widgets import ComboboxVariablesWidget
 
 
-class PropertyColor(QWidget):
+class PropertyCombobox(QWidget):
     edited = Signal()
-    def __init__(self, value_class, value, variables_scrollArea):
+    def __init__(self, value_class, value, variables_scrollArea, items, filter_types):
         super().__init__()
         self.ui = Ui_Widget()
         self.ui.setupUi(self)
         self.setAcceptDrops(False)
         self.value_class = value_class
         self.value = value
-
-        self.color = [255, 255, 255]
-        self.ui.logic_switch.setCurrentIndex(0)
-
         self.variables_scrollArea = variables_scrollArea
 
-        self.dialog = QColorDialog()
-        self.dialog.setStyleSheet(QT_Stylesheet_global)
 
-
-        output = re.sub(r'm_fl|m_n|m_b|m_s|m_', '', self.value_class)
+        output = re.sub(r'm_fl|m_n|m_b|m_', '', self.value_class)
         output = re.sub(r'([a-z0-9])([A-Z])', r'\1 \2', output)
 
         self.ui.property_class.setText(output)
         self.ui.logic_switch.currentTextChanged.connect(self.on_changed)
+        self.ui.logic_switch.setItemText(1, output)
+
+        self.ui.value.addItems(items)
+        self.ui.value.currentTextChanged.connect(self.on_changed)
+
+
+        # Spacer frame
+        spacer_item = QSpacerItem(0, 0, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        self.spacer = QWidget()
+        spacer_layout = QHBoxLayout()
+        spacer_layout.addSpacerItem(spacer_item)
+        spacer_layout.setContentsMargins(0, 0, 0, 0)
+        self.spacer.setLayout(spacer_layout)
+        self.spacer.setStyleSheet('border:None;')
+        self.spacer.setContentsMargins(0, 0, 0, 0)
+        self.ui.layout.addWidget(self.spacer)
 
 
         # Variable setup
-        self.variable = ComboboxVariablesWidget(variables_layout=self.variables_scrollArea, filter_types=['Color'])
+        self.variable = ComboboxVariablesWidget(variables_layout=self.variables_scrollArea, filter_types=filter_types)
         layout = QHBoxLayout()
         layout.setContentsMargins(0, 0, 0, 0)
         layout.addWidget(self.variable)
@@ -58,79 +58,64 @@ class PropertyColor(QWidget):
         self.ui.layout.insertWidget(2, self.variable_frame)
 
 
+
         # EditLine
         self.text_line = CompletingPlainTextEdit()
         self.text_line.completion_tail = ''
-        self.text_line.setPlaceholderText('Variable name, RGB or expression')
-        self.ui.layout.insertWidget(2, self.text_line)
+        self.text_line.setPlaceholderText('Variable name, float or expression')
+        self.ui.layout.insertWidget(3, self.text_line)
         self.text_line.textChanged.connect(self.on_changed)
+
+        self.ui.logic_switch.setCurrentIndex(0)
+        self.text_line.setPlainText('')
 
         if isinstance(value, dict):
             if 'm_Expression' in value:
                 self.ui.logic_switch.setCurrentIndex(3)
                 self.var_value = value['m_Expression']
                 self.text_line.setPlainText(self.var_value)
-                self.color = [255, 255, 255]
-            elif 'm_SourceName' in value:
+            if 'm_SourceName' in value:
                 self.ui.logic_switch.setCurrentIndex(2)
-                self.color = [255, 255 ,255]
                 self.var_value = value['m_SourceName']
                 self.variable.combobox.set_variable(str(self.var_value))
-            else:
-                print('Could not parse given input data')
-                self.color = [255, 255, 255]
-        elif isinstance(value, list):
+        elif isinstance(value, str):
             self.ui.logic_switch.setCurrentIndex(1)
-            self.color = value
-
-        self.ui.value.clicked.connect(self.open_dialog)
+            self.ui.value.setCurrentText(value)
 
         self.on_changed()
 
-    def open_dialog(self):
-        color_dialog = self.dialog
-        selected_color = color_dialog.getColor(QColor(*self.color))
-        if selected_color.isValid():
-            color = selected_color.getRgb()[:3]
-            self.ui.value.setStyleSheet(f"""background-color: rgb{color};
-                padding:4px;
-                border:0px;
-                border: 2px solid translucent;
-                border-color: rgba(80, 80, 80, 100);
-                """)
-            print("RGB Color:", color)
-            self.color = list(color)
-            self.on_changed()
+
 
 
     def logic_switch(self):
         if self.ui.logic_switch.currentIndex() == 0:
+            self.text_line.OnlyFloat = False
             self.text_line.hide()
             self.ui.value.hide()
             self.variable_frame.hide()
+            self.spacer.show()
         elif self.ui.logic_switch.currentIndex() == 1:
             self.text_line.hide()
             self.ui.value.show()
             self.variable_frame.hide()
+            self.spacer.hide()
         elif self.ui.logic_switch.currentIndex() == 2:
             self.text_line.hide()
             self.ui.value.hide()
             self.variable_frame.show()
+            self.spacer.hide()
         else:
+            self.text_line.OnlyFloat = False
             self.text_line.show()
+            self.variable_frame.hide()
             self.ui.value.hide()
+            self.spacer.hide()
 
     def on_changed(self):
         self.logic_switch()
         variables = self.get_variables()
         self.text_line.completions.setStringList(variables)
         self.change_value()
-        self.ui.value.setStyleSheet(f"""background-color: rgb{tuple(self.color)};
-            padding:4px;
-            border:0px;
-            border: 2px solid translucent;
-            border-color: rgba(80, 80, 80, 100);
-            """)
         self.edited.emit()
     def change_value(self):
         # Default
@@ -138,11 +123,15 @@ class PropertyColor(QWidget):
             self.value = None
         #Float or int
         elif self.ui.logic_switch.currentIndex() == 1:
-            self.value = {self.value_class: self.color}
+            self.value = {self.value_class: self.ui.value.currentText()}
         # Variable
         elif self.ui.logic_switch.currentIndex() == 2:
             value = self.variable.combobox.get_variable()
-            self.value = {self.value_class: {'m_SourceName': value}}
+            try:
+                value = ast.literal_eval(value)
+            except:
+                pass
+            self.value = {self.value_class: {'m_SourceName': str(value)}}
         # Expression
         elif self.ui.logic_switch.currentIndex() == 3:
             value = self.text_line.toPlainText()
@@ -150,9 +139,10 @@ class PropertyColor(QWidget):
                 value = ast.literal_eval(value)
             except:
                 pass
-            self.value = {self.value_class: {'m_Expression': value}}
+            self.value = {self.value_class: {'m_Expression': str(value)}}
 
     def get_variables(self, search_term=None):
+        self.variables_scrollArea
         data_out = []
         for i in range(self.variables_scrollArea.count()):
             widget = self.variables_scrollArea.itemAt(i).widget()
--- src/smartprop_editor/property/comparison.py
+++ src/smartprop_editor/property/comparison.py
@@ -1,6 +1,6 @@
 import ast
 import re
-from src.smartprop_editor.properties.ui_comparison import Ui_Widget
+from src.smartprop_editor.property.ui_comparison import Ui_Widget
 from src.completer.main import CompletingPlainTextEdit
 from PySide6.QtWidgets import QWidget
 from PySide6.QtCore import Signal
