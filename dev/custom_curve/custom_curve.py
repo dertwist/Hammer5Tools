b45020c5200ac13a8ba2865bdc3ddcce7a5726b4
dertwist <farsmok@gmail.com>
2025-01-17 13:48:46 +0100

    Rewrote custom_curve code to NumPy.

--- dev/custom_curve/custom_curve.py
+++ dev/custom_curve/custom_curve.py
@@ -1,3 +1,6 @@
+import time
+import numpy as np
+
 # This code was originally decompiled C code extracted directly from the Counter Strike 2 executable using Ghidra.
 # Then, it was rewritten with some minor C++ syntax, and then converted into python for the sake of this project.
 # So what you see here is as close as possible to how the machine code that executes inside the CS2, determines the volume from these curves.
@@ -61,14 +64,14 @@ def _setup_curve_point(point, prev_point, next_point):
     # The last 2 numbers in each curve point are actually integer modes that determine how to interpolate values near the points themselves.
     # Contrary to what is told, the values "0, 0, 2, 3" does not give you a straight linear interpolation. It's more like an S curve.
     # Linear would be "0, 0, 0, 0" and you may be able to see that by reading this code. But it is understandably strange.
-    
+
     # The first 2 of the 4 numbers that nobody understood up to this point, are actually just "slope" values.
     # So they essentially represent an angle for how the curve "tangents" away from each curve point. Which makes sence, but it means that configuring the values directly is hard.
-    
+
     # I suspect that Valve has an internal tool for visualising/editing these curves, or, whoever decided to setup the interface like this was just feeling smart one day,
     # but then never wrote a tool to allow people to use his code to the extent of what it could do, and everyone just said: "use 0,0,2,3 and you'll be fine".
     # Rant over.
-    
+
     # Here we are setting the slope for the left side of the curve point.
     if point.modeLeft == 0:
         point.slopeLeft = slope2
@@ -77,15 +80,8 @@ def _setup_curve_point(point, prev_point, next_point):
     elif point.modeLeft == 3:
         point.slopeLeft = 0.0
     elif point.modeLeft == 4:
-        if delta_y <= 0.0:
-            if delta_x == 0.0:
-                point.slopeLeft = -1.60305
-            else:
-                point.slopeLeft = (1.0 / delta_x) * -1.60305
-        elif delta_x == 0.0:
-            point.slopeLeft = -0.0413377
-        else:
-            point.slopeLeft = (1.0 / delta_x) * -0.0413377
+        point.slopeLeft = np.where(delta_x == 0.0, -1.60305 if delta_y <= 0.0 else -0.0413377, (1.0 / delta_x) * (-1.60305 if delta_y <= 0.0 else -0.0413377))
+
 
     # Here we are setting the slope for the right side of the curve point.
     if point.modeRight == 0:
@@ -95,15 +91,7 @@ def _setup_curve_point(point, prev_point, next_point):
     elif point.modeRight == 3:
         point.slopeRight = point.slopeLeft
     elif point.modeRight == 4:
-        if delta_y2 <= 0.0:
-            if delta_x2 == 0.0:
-                point.slopeRight = 0.0413377
-            else:
-                point.slopeRight = (1.0 / delta_x2) * 0.0413377
-        elif delta_x2 == 0.0:
-            point.slopeRight = 1.60305
-        else:
-            point.slopeRight = (1.0 / delta_x2) * 1.60305
+        point.slopeRight = np.where(delta_x2 == 0.0, 0.0413377 if delta_y2 <= 0.0 else 1.60305, (1.0 / delta_x2) * (0.0413377 if delta_y2 <= 0.0 else 1.60305))
 
     if point.modeLeft == 3:
         point.slopeLeft = point.slopeRight
@@ -112,28 +100,29 @@ def _setup_curve_point(point, prev_point, next_point):
 def setup_all_curve_values(points, totalPoints):
     if totalPoints == 0:
         return
-    
+
     if totalPoints == 1:
         _setup_curve_point(points[0], None, None)
         return
-    
+
     lastIndex = totalPoints-1
-    
+
     for i in range(totalPoints):
         prevPoint = points[i-1] if i>0 else None
         nextPoint = points[i+1] if i<lastIndex else None
-        
+
         _setup_curve_point(points[i], prevPoint, nextPoint)
 
 # If anyone wants to help rename some of the local variables, that would be awesome.
 def sample_curve(xValue, points, total_points):
+    # start_time = time.time()
     # Validate that we were given a list of points, and that there are more than 2 points to sample between.
     if points is not None and total_points > 1:
         last_point = total_points - 1
         u_var2 = 1
         u_var1 = 1
         u_var3 = last_point
-        
+
         # I believe this logic is looking for which 2 curve points the "xValue" value lies between.
         # So this assumes that the list of points is properly sorted based on each point's .xValue
         if last_point != 0:
@@ -149,16 +138,16 @@ def sample_curve(xValue, points, total_points):
             else:
                 cur_point = u_var1
 
-            if points[u_var1].xValue <= xValue and (cur_point := u_var1 + 1) and last_point <= u_var1:
+            if points[u_var1].xValue <= xValue and (u_var1 + 1 <= last_point) and last_point <= u_var1: #Fixed potential IndexError
                 cur_point = u_var1
-        
+
         # This is where the actual sampling begins.
         left_point = points[cur_point - 1]
         right_point = points[cur_point]
-        
+
         delta_x = right_point.xValue - left_point.xValue
         delta_y = right_point.yValue - left_point.yValue
-        
+
         # Get a normalized value for the 'xValue' parameter between our left_point and right_point
         # If you are directly in between the 2 points, the value would be 0.5
         yValue_result = xValue - left_point.xValue
@@ -166,14 +155,15 @@ def sample_curve(xValue, points, total_points):
             yValue_result /= delta_x
 
         yValue = max(0.0, min(yValue_result, 1.0))
-        
+
         yValue_result = left_point.slopeRight
-        
+
         # This is the actual math which generates the curve. I have no clue if this is actually bezier or somthing else.
         p1 = ((yValue_result + right_point.slopeLeft) * delta_x - (delta_y + delta_y)) * yValue
         p2 = (p1 + (-right_point.slopeLeft - (yValue_result + yValue_result)) * delta_x + delta_y * 3.0)
         calc_yValue = (p2 * yValue + yValue_result * delta_x) * yValue + left_point.yValue
-
+        end_time = time.time()
+        # print(f"Calculation time: {(end_time - start_time) * 1000} milliseconds")
         return calc_yValue
 
     return -1.0 # If we reach this point, somthing bad has happened. This should probably be an error log or thrown exception.
