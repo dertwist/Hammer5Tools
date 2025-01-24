1cb1c29ef628a67065b9c9046d714aa6cf47e287
dertwist <farsmok@gmail.com>
2025-01-24 13:52:22 +0100

    Cleanup, enhancing coding format and project structure.

--- src/about/ui_main.py
+++ src/about/ui_main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'documentation_dialog.ui'
+## Form generated from reading UI file 'main.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.7.2
 ##
