54315fd0a324ca14d316decb287af2b17e2aed4b
dertwist <45437750+dertwist@users.noreply.github.com>
2025-01-25 13:12:04 +0100

    spe properties optimization

--- src/smartprop_editor/property/legacy.py
+++ src/smartprop_editor/property/legacy.py
@@ -8,6 +8,7 @@ from PySide6.QtCore import Signal
 
 class PropertyLegacy(QWidget):
     edited = Signal()
+
     def __init__(self, value_class, value, variables_scrollArea):
         super().__init__()
         self.ui = Ui_Widget()
@@ -17,23 +18,31 @@ class PropertyLegacy(QWidget):
         self.value = value
         self.variables_scrollArea = variables_scrollArea
 
+        # Cache for variable names to avoid recomputing repeatedly
+        self._variables_cache = []
+        self._variable_count = 0
+
         self.text_line = CompletingPlainTextEdit()
         self.ui.layout.insertWidget(1, self.text_line)
 
         self.text_line.setPlainText(str(self.value))
         self.ui.value_label.setText(str(self.value_class))
+
         self.text_line.textChanged.connect(self.on_changed)
 
-
         self.change_value()
-
-
 
     def on_changed(self):
-        variables = self.get_variables()
-        self.text_line.completions.setStringList(variables)
+        # Update the cache only if the number of items has changed
+        new_count = self.variables_scrollArea.count()
+        if new_count != self._variable_count:
+            self._variables_cache = self.get_variables()
+            self._variable_count = new_count
+
+        self.text_line.completions.setStringList(self._variables_cache)
         self.change_value()
         self.edited.emit()
+
     def change_value(self):
         value = self.text_line.toPlainText()
         try:
