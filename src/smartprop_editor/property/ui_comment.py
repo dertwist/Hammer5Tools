476c3e27caa3e01f84019980d4b282cd0097c430
dertwist <farsmok@gmail.com>
2025-02-03 13:33:21 +0100

    Comment property

---
+++ src/smartprop_editor/property/ui_comment.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'comment.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QPlainTextEdit, QSizePolicy, QVBoxLayout,
+    QWidget)
+
+class Ui_Widget(object):
+    def setupUi(self, Widget):
+        if not Widget.objectName():
+            Widget.setObjectName(u"Widget")
+        Widget.resize(830, 157)
+        Widget.setMinimumSize(QSize(0, 128))
+        Widget.setMaximumSize(QSize(16777215, 256))
+        Widget.setStyleSheet(u".QWidget {\n"
+"    font: 580 10pt \"Segoe UI\";\n"
+"    border: 2px solid black;\n"
+"    border-radius: 0px;\n"
+"    border-left: 0px;\n"
+"    border-right: 0px;\n"
+"	border-top: 0px;\n"
+"    border-color: rgba(50, 50, 50, 255);\n"
+"    padding: 8px;\n"
+"    padding-left: 6px;\n"
+"    padding-right: 6px;\n"
+"    color: #E3E3E3;\n"
+"    background-color: #1C1C1C;\n"
+"}\n"
+"\n"
+".QWidget::hover {\n"
+"}\n"
+".QWidget::selected {\n"
+"    background-color: #414956;\n"
+"}")
+        self.verticalLayout = QVBoxLayout(Widget)
+        self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.text_field = QPlainTextEdit(Widget)
+        self.text_field.setObjectName(u"text_field")
+
+        self.verticalLayout.addWidget(self.text_field)
+
+
+        self.retranslateUi(Widget)
+
+        QMetaObject.connectSlotsByName(Widget)
+    # setupUi
+
+    def retranslateUi(self, Widget):
+        Widget.setWindowTitle(QCoreApplication.translate("Widget", u"Form", None))
+    # retranslateUi
+
