bba5620abe7df82c7ed711afb614ff334755aa5d
dertwist <45437750+dertwist@users.noreply.github.com>
2025-02-03 17:51:18 +0100

    Viewport preview

--- src/main.py
+++ src/main.py
@@ -327,7 +327,7 @@ class Widget(QMainWindow):
         if settings.value("APP/minimize_to_tray", type=bool, defaultValue=True):
             event.ignore()
             self.hide()
-            settings.setValue("APP/minimize_to_tray", type=bool, value=True)
+            settings.setValue("APP/minimize_to_tray", True)
             self.show_minimize_message_once()
         else:
             self.exit_application()
