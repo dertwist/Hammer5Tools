2265baf4f34c1d90266738ec9f3871f7809475d6
dertwist <45437750+dertwist@users.noreply.github.com>
2025-02-06 18:38:05 +0100

    4.1.2 fixed critical error with skiping int values to load in FLoatWdiget Property Extended skopes range to from [-2,2] to [-10,10]

--- src/common.py
+++ src/common.py
@@ -34,7 +34,7 @@ def enable_dark_title_bar(window):
 
 #===============================================================<  Variables  >=============================================================
 # Versions
-app_version = '4.1.1'
+app_version = '4.1.2'
 
 editor_info = {
     'editor_info':
