e4929d0bd69f9460113aaeb8f32c769a218fe097
dertwist <45437750+dertwist@users.noreply.github.com>
2025-03-29 17:17:43 +0100

    4.5.1 fixed loop kv3 error

--- src/common.py
+++ src/common.py
@@ -37,7 +37,7 @@ def enable_dark_title_bar(window):
 
 #===============================================================<  Variables  >=============================================================
 # Versions
-app_version = '4.5.0'
+app_version = '4.5.1'
 
 editor_info = {
     'editor_info':
