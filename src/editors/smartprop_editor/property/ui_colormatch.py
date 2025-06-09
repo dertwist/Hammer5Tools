bbca10f862b355bb6f3acdaf2bec359ef7f7cb1d
dertwist <45437750+dertwist@users.noreply.github.com>
2025-06-09 16:03:30 +0200

    smartprop editor viewport and properties browser

--- src/editors/smartprop_editor/property/ui_colormatch.py
+++ src/editors/smartprop_editor/property/ui_colormatch.py
@@ -18,7 +18,6 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
 from PySide6.QtWidgets import (QApplication, QFrame, QHBoxLayout, QLabel,
     QLayout, QPushButton, QSizePolicy, QSpacerItem,
     QVBoxLayout, QWidget)
-import resources_rc
 
 class Ui_Widget(object):
     def setupUi(self, Widget):
