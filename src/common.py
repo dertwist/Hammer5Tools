976424bc513929c3d7c21c37080e53317ac39758
dertwist <45437750+dertwist@users.noreply.github.com>
2025-06-30 07:57:00 +0200

    Add function to run PowerShell scripts as admin

--- src/common.py
+++ src/common.py
@@ -11,7 +11,7 @@ import ctypes
 import re, unicodedata, random, string
 
 # Versions
-app_version = '4.7.2'
+app_version = '4.7.3'
 
 #======================================================<  Copied from preferences.py file  >===================================================
 
