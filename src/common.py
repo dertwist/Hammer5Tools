821d4a3070ba2fae14eac1942b9983c09b7e4468
dertwist <45437750+dertwist@users.noreply.github.com>
2025-06-30 11:42:36 +0200

    4.7.4

--- src/common.py
+++ src/common.py
@@ -11,7 +11,7 @@ import ctypes
 import re, unicodedata, random, string
 
 # Versions
-app_version = '4.7.3'
+app_version = '4.7.4'
 
 #======================================================<  Copied from preferences.py file  >===================================================
 
