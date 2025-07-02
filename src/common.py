7d234f8269b543105d6853a6ee51dd0941fc63d8
Max <nucky3d@gmail.com>
2025-07-02 04:59:52 +0200

    4.7.6

--- src/common.py
+++ src/common.py
@@ -11,7 +11,7 @@ import ctypes
 import re, unicodedata, random, string
 
 # Versions
-app_version = '4.7.5'
+app_version = '4.7.6'
 
 #======================================================<  Copied from preferences.py file  >===================================================
 
