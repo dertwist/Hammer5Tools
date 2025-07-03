f4d05d0a0e03e0aa7d2577698d2ca8ac39a56b90
Max <nucky3d@gmail.com>
2025-07-03 08:04:45 +0200

    Refactor: set_variable property with adding expression and value options

--- src/editors/smartprop_editor/property/ui_set_variable.py
+++ src/editors/smartprop_editor/property/ui_set_variable.py
@@ -99,6 +99,7 @@ class Ui_Widget(object):
         self.logic_switch = QComboBox(self.frame)
         self.logic_switch.addItem("")
         self.logic_switch.addItem("")
+        self.logic_switch.addItem("")
         self.logic_switch.setObjectName(u"logic_switch")
         sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
         sizePolicy1.setHorizontalStretch(0)
@@ -280,17 +281,101 @@ class Ui_Widget(object):
         self.layout_3 = QHBoxLayout()
         self.layout_3.setObjectName(u"layout_3")
         self.layout_3.setSizeConstraint(QLayout.SizeConstraint.SetFixedSize)
-        self.property_class_5 = QLabel(self.frame_3)
-        self.property_class_5.setObjectName(u"property_class_5")
-        sizePolicy.setHeightForWidth(self.property_class_5.sizePolicy().hasHeightForWidth())
-        self.property_class_5.setSizePolicy(sizePolicy)
-        self.property_class_5.setStyleSheet(u"border:0px;\n"
-"background-color: rgba(255, 255, 255, 0);\n"
-"font: 8pt \"Segoe UI\";\n"
-"padding-right: 16px;\n"
+        self.logic_switch_value = QComboBox(self.frame_3)
+        self.logic_switch_value.addItem("")
+        self.logic_switch_value.addItem("")
+        self.logic_switch_value.setObjectName(u"logic_switch_value")
+        sizePolicy1.setHeightForWidth(self.logic_switch_value.sizePolicy().hasHeightForWidth())
+        self.logic_switch_value.setSizePolicy(sizePolicy1)
+        self.logic_switch_value.setMaximumSize(QSize(16777215, 24))
+        self.logic_switch_value.setStyleSheet(u"QComboBox {\n"
+"    font: 580 8pt \"Segoe UI\";\n"
+"    border: 2px solid black;\n"
+"    border-radius: 0px;\n"
+"    border-color: rgba(80, 80, 80, 255);\n"
+"    height: 18px;\n"
+"    padding-top: 2px;\n"
+"    padding-bottom: 2px;\n"
+"	border-top:none;\n"
+"border-bottom:none;\n"
+"border:none;\n"
+"    color: #E3E3E3;\n"
+"    background-color: #1C1C1C;\n"
+"}\n"
+"\n"
+"QComboBox:hover {\n"
+"    background-color: #414956;\n"
+"    color: white;\n"
+"}\n"
+"\n"
+"QComboBox:pressed {\n"
+"}\n"
+"\n"
+"QComboBox:item {\n"
+"    font: 580  8pt \"Segoe UI\";\n"
+"    color: #E3E3E3;\n"
+"    padding-left: 5px;\n"
+"    background-color: #1C1C1C;\n"
+"    border-style: none;\n"
+"}\n"
+"\n"
+"QComboBox::drop-down {\n"
+"    color: #E3E3E3;\n"
+"    padding: 2px;\n"
+"    background: url(://icons/arrow_drop_down_16dp.svg) no-repeat center;\n"
+"    border-bottom: 0px solid black;\n"
+"    border-top: 0px solid black;\n"
+"    border-right: 0px;\n"
+"    border-left: 2px solid;\n"
+"    margin-left: 5px;\n"
+"    padding: 5px;\n"
+"    width: 7px;"
+                        "\n"
+"    border-color: rgba(80, 80, 80, 255);\n"
+"    background-color: #1C1C1C;\n"
+"border:none;\n"
+"}\n"
+"\n"
+"QComboBox::drop-down:hover {\n"
+"    background-color: #414956;\n"
+"    color: white;\n"
+"}\n"
+"\n"
+"\n"
+"QComboBox QAbstractItemView {\n"
+"    border: 2px solid gray;\n"
+"    border-color: rgba(80, 80, 80, 255);\n"
+"    selection-background-color: #414956;\n"
+"    background-color: #1C1C1C;\n"
+"}\n"
+"\n"
+"\n"
+"QComboBox QAbstractItemView::item {\n"
+"    height: 16px; /* Set the height of each item */\n"
+"    padding: 4px; /* Add padding to each item */\n"
+"    padding-left: 5px;\n"
+"    padding-right: 5px;\n"
+"    color: #ff8a8a8a;\n"
+"    border-style: none;\n"
+"    border-bottom: 0.5px solid black;\n"
+"    border-color: rgba(255, 255, 255, 10);\n"
+"}\n"
+"\n"
+"\n"
+"QComboBox QAbstractItemView::item:selected {\n"
+"    height: 16px; /* Set the height of each item */\n"
+"    padding: 4px; /* Add padding to each item */\n"
+"    padding-left: 5px;\n"
+"    padding-right: 5px;\n"
+"    background-color: #"
+                        "414956;\n"
+"    color: white;\n"
+"    border: none; /* Remove border */\n"
+"    outline: none; /* Remove outline */\n"
+"}\n"
 "")
 
-        self.layout_3.addWidget(self.property_class_5)
+        self.layout_3.addWidget(self.logic_switch_value)
 
 
         self.horizontalLayout_4.addLayout(self.layout_3)
@@ -302,6 +387,7 @@ class Ui_Widget(object):
         self.retranslateUi(Widget)
 
         self.logic_switch.setCurrentIndex(0)
+        self.logic_switch_value.setCurrentIndex(0)
 
 
         QMetaObject.connectSlotsByName(Widget)
@@ -312,8 +398,11 @@ class Ui_Widget(object):
         self.property_class.setText(QCoreApplication.translate("Widget", u"Type", None))
         self.logic_switch.setItemText(0, QCoreApplication.translate("Widget", u"Bool", None))
         self.logic_switch.setItemText(1, QCoreApplication.translate("Widget", u"Float", None))
+        self.logic_switch.setItemText(2, QCoreApplication.translate("Widget", u"Int", None))
 
         self.property_class_4.setText(QCoreApplication.translate("Widget", u"Variable", None))
-        self.property_class_5.setText(QCoreApplication.translate("Widget", u"Value", None))
+        self.logic_switch_value.setItemText(0, QCoreApplication.translate("Widget", u"Value", None))
+        self.logic_switch_value.setItemText(1, QCoreApplication.translate("Widget", u"Expression", None))
+
     # retranslateUi
 
