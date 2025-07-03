6208d47eb47c6959f6bb1b9dda2beaa4176896f1
Max <nucky3d@gmail.com>
2025-07-03 08:04:45 +0200

    Refactor: set_variable property with adding expression and value options

--- src/editors/smartprop_editor/property/set_variable.py
+++ src/editors/smartprop_editor/property/set_variable.py
@@ -1,7 +1,9 @@
 from src.editors.smartprop_editor.property.ui_set_variable import Ui_Widget
+from src.widgets.completer.main import CompletingPlainTextEdit
 from PySide6.QtWidgets import QSizePolicy, QSpacerItem, QHBoxLayout, QWidget
 from PySide6.QtCore import Signal
 from src.editors.smartprop_editor.widgets.main import ComboboxVariablesWidget
+from src.editors.smartprop_editor.objects import expression_completer
 from src.widgets import FloatWidget, BoolWidget
 
 # m_VariableValue =
@@ -28,124 +30,224 @@ class PropertyVariableValue(QWidget):
 
     def __init__(self, value_class, value, variables_scrollArea, element_id_generator):
         super().__init__()
-        self.ui = Ui_Widget()
-        self.ui.setupUi(self)
-        self.setAcceptDrops(False)
-        self.value_class = value_class
-        self.value = value
-        self.m_TargetName: str = None
-        self.m_DataType: str = None
-        self.m_Value = None
-        self.variables_scrollArea = variables_scrollArea
+        try:
+            self.ui = Ui_Widget()
+            self.ui.setupUi(self)
+            self.setAcceptDrops(False)
+            self.value_class = value_class
+            self.value = None
+            self.m_TargetName: str = None
+            self.m_DataType: str = None
+            self.m_Value = None
+            self.variables_scrollArea = variables_scrollArea
 
-        # Float widget setup
-        self.float_widget = FloatWidget(slider_range=[0,0], int_output=False)
-        self.float_widget.edited.connect(self.on_changed)
-        self.ui.layout_3.addWidget(self.float_widget)
+            # Float widget setup
+            self.float_widget = FloatWidget(slider_range=[0,0], int_output=False, spacer_enable=False)
+            self.float_widget.edited.connect(self.on_changed)
+            self.ui.layout_3.addWidget(self.float_widget)
 
+            # init colors
+            self.ui.property_class_4.setStyleSheet(f"color: rgb{STRING_COLOR};")
+            self.ui.property_class.setStyleSheet(f"color: rgb{STRING_COLOR};")
 
+            # Bool widget setup
+            self.bool_widget = BoolWidget(spacer_enable=False)
+            self.bool_widget.edited.connect(self.on_changed)
+            self.bool_widget.checkbox.setStyleSheet('border:None; font: 580 9pt "Segoe UI";')
+            self.ui.layout_3.addWidget(self.bool_widget)
 
-        # init colors
-        self.ui.property_class_4.setStyleSheet(f"color: rgb{STRING_COLOR};")
-        self.ui.property_class.setStyleSheet(f"color: rgb{STRING_COLOR};")
+            # EditLine setup
+            self.text_line = CompletingPlainTextEdit()
+            self.text_line.completion_tail = ''
+            self.text_line.OnlyFloat = False
+            self.text_line.setPlaceholderText('Variable name, float or expression')
+            self.ui.layout_3.addWidget(self.text_line)
+            self.text_line.textChanged.connect(self.on_changed)
 
+            # Spacer frame - Create separate spacer items for different layouts
+            value_spacer_item = QSpacerItem(0, 0, QSizePolicy.Expanding, QSizePolicy.Minimum)
+            self.value_spacer_item = QWidget()
+            value_spacer_layout = QHBoxLayout()
+            value_spacer_layout.addItem(value_spacer_item)
+            value_spacer_layout.setContentsMargins(0, 0, 0, 0)
+            self.value_spacer_item.setLayout(value_spacer_layout)
+            self.ui.layout_3.addWidget(self.value_spacer_item)
 
+            # Create separate spacer item for main spacer
+            main_spacer_item = QSpacerItem(0, 0, QSizePolicy.Expanding, QSizePolicy.Minimum)
+            self.spacer = QWidget()
+            spacer_layout = QHBoxLayout()
+            spacer_layout.addItem(main_spacer_item)
+            spacer_layout.setContentsMargins(0, 0, 0, 0)
+            self.spacer.setLayout(spacer_layout)
+            self.spacer.setStyleSheet('border:None;')
+            self.spacer.setContentsMargins(0, 0, 0, 0)
+            self.ui.layout.addWidget(self.spacer)
 
+            # Connect signals
+            self.ui.logic_switch.currentTextChanged.connect(self.on_changed)
+            self.ui.logic_switch_value.currentTextChanged.connect(self.on_changed)
 
-        # Bool widget setup
-        self.bool_widget = BoolWidget()
-        self.bool_widget.edited.connect(self.on_changed)
-        self.bool_widget.checkbox.setStyleSheet('border:None; font: 580 9pt "Segoe UI";')
-        self.ui.layout_3.addWidget(self.bool_widget)
+            # Variable setup
+            self.variable = ComboboxVariablesWidget(
+                variables_layout=self.variables_scrollArea,
+                filter_types=['Float', 'Int', 'Bool'],
+                variable_name=self.value_class,
+                element_id_generator=element_id_generator
+            )
+            layout = QHBoxLayout()
+            layout.setContentsMargins(0, 0, 0, 0)
+            layout.addWidget(self.variable)
+            # Create separate spacer item for variable layout
+            variable_spacer_item = QSpacerItem(0, 0, QSizePolicy.Expanding, QSizePolicy.Minimum)
+            layout.addItem(variable_spacer_item)
+            self.variable_frame = QWidget()
+            self.variable_frame.setLayout(layout)
+            self.variable.setFixedWidth(256)
+            self.variable.setMaximumHeight(24)
+            self.variable.search_button.set_size(width=24, height=24)
+            self.variable_frame.setMinimumHeight(32)
+            self.variable.combobox.changed.connect(self.on_changed)
+            self.ui.layout_2.addWidget(self.variable_frame)
 
-        # Spacer frame
-        spacer_item = QSpacerItem(0, 0, QSizePolicy.Expanding, QSizePolicy.Minimum)
-        self.spacer = QWidget()
-        spacer_layout = QHBoxLayout()
-        spacer_layout.addSpacerItem(spacer_item)
-        spacer_layout.setContentsMargins(0, 0, 0, 0)
-        self.spacer.setLayout(spacer_layout)
-        self.spacer.setStyleSheet('border:None;')
-        self.spacer.setContentsMargins(0, 0, 0, 0)
-        self.ui.layout.addWidget(self.spacer)
+            # Initialize value
+            self.initialize_values(value)
 
-        self.ui.logic_switch.currentTextChanged.connect(self.on_changed)
+            # Initial state update
+            self.on_changed()
 
+        except Exception as e:
+            print(f"Error in PropertyVariableValue.__init__: {e}")
+            raise
 
-        # Variable setup
-        self.variable = ComboboxVariablesWidget(variables_layout=self.variables_scrollArea, filter_types=['Float', 'Int', 'Bool'], variable_name=self.value_class, element_id_generator=element_id_generator)
-        layout = QHBoxLayout()
-        layout.setContentsMargins(0, 0, 0, 0)
-        layout.addWidget(self.variable)
-        layout.addSpacerItem(QSpacerItem(0, 0, QSizePolicy.Expanding, QSizePolicy.Minimum))
-        self.variable_frame = QWidget()
-        self.variable_frame.setLayout(layout)
-        self.variable.setFixedWidth(256)
-        self.variable.setMaximumHeight(24)
-        self.variable.search_button.set_size(width=24, height=24)
-        self.variable_frame.setMinimumHeight(32)
-        self.variable.combobox.changed.connect(self.on_changed)
-        self.ui.layout_2.addWidget(self.variable_frame)
+    def initialize_values(self, value):
+        """Initialize widget values from the provided value dictionary"""
+        try:
+            if 'm_Value' in value:
+                print(value)
+                self.m_Value = value['m_Value']
+                print(self.m_Value)
+                if isinstance(self.m_Value, bool):
+                    self.bool_widget.set_value(self.m_Value)
+                elif isinstance(self.m_Value, (float, int)):
+                    self.float_widget.set_value(self.m_Value)
+                elif isinstance(self.m_Value, dict) and ('m_Expression' in self.m_Value):
+                    self.text_line.setPlainText(str(self.m_Value['m_Expression']))
+                    self.ui.logic_switch_value.setCurrentText('Expression')
 
+            if 'm_DataType' in value:
+                self.m_DataType = value['m_DataType']
+                if self.m_DataType == 'FLOAT':
+                    self.ui.logic_switch.setCurrentText('Float')
+                elif self.m_DataType == 'BOOL':  # Fixed typo: 'BOOl' -> 'BOOL'
+                    self.ui.logic_switch.setCurrentText('Bool')
+                elif self.m_DataType == 'INT':
+                    self.ui.logic_switch.setCurrentText('Int')
 
-        # EditLine setup
+            if 'm_TargetName' in value:
+                self.m_TargetName = value['m_TargetName']
+                self.variable.combobox.set_variable(self.m_TargetName)
 
+        except Exception as e:
+            print(f"Error in initialize_values: {e}")
 
-
-        # Initialize value
-        if 'm_DataType' in value:
-            self.m_DataType = value['m_DataType']
-            if self.m_DataType == 'FLOAT':
-                self.ui.logic_switch.setCurrentText('Float')
-            elif self.m_DataType == 'BOOl':
-                self.ui.logic_switch.setCurrentText('Bool')
-        if 'm_Value' in value:
-            self.m_Value = value['m_Value']
-            if isinstance(self.m_Value, bool):
-                self.bool_widget.set_value(self.m_Value)
-            elif isinstance(self.m_Value, float):
-                self.float_widget.set_value(self.m_Value)
-            elif isinstance(self.m_Value, int):
-                self.float_widget.set_value(self.m_Value)
-        if 'm_TargetName' in value:
-            self.m_TargetName = value['m_TargetName']
-            self.variable.combobox.set_variable(self.m_TargetName)
-
-        self.on_changed()
+    def logic_switch_value(self):
+        """Handle switching between Value and Expression modes"""
+        try:
+            if self.ui.logic_switch_value.currentText() == 'Value':
+                self.text_line.hide()
+                self.value_spacer_item.show()
+                # Show appropriate widget based on current data type
+                current_type = self.ui.logic_switch.currentText()
+                if current_type == 'Bool':
+                    self.float_widget.hide()
+                    self.bool_widget.show()
+                else:  # Float or Int
+                    self.float_widget.show()
+                    self.bool_widget.hide()
+            elif self.ui.logic_switch_value.currentText() == 'Expression':
+                self.text_line.show()
+                self.value_spacer_item.hide()
+                self.float_widget.hide()
+                self.bool_widget.hide()
+        except Exception as e:
+            print(f"Error in logic_switch_value: {e}")
 
     def logic_switch(self):
-        if self.ui.logic_switch.currentText() == 'Float':
-            self.float_widget.show()
-            self.bool_widget.hide()
-            self.spacer.show()
-            self.ui.property_class_5.setStyleSheet(f"color: rgb{FLOAT_COLOR};")
-            self.ui.property_class_4.setStyleSheet(f"color: rgb{FLOAT_COLOR};")
-            self.variable.combobox.filter_types = ['Float', 'int']
-        elif self.ui.logic_switch.currentText() == 'Bool':
-            self.float_widget.hide()
-            self.bool_widget.show()
-            self.spacer.show()
-            self.ui.property_class_5.setStyleSheet(f"color: rgb{BOOL_COLOR};")
-            self.ui.property_class_4.setStyleSheet(f"color: rgb{BOOL_COLOR};")
-            self.variable.combobox.filter_types = ['Bool']
-        self.m_DataType = self.ui.logic_switch.currentText().upper()
+        """Handle switching between data types (Bool, Float, Int)"""
+        try:
+            current_type = self.ui.logic_switch.currentText()
+
+            if current_type in ['Float', 'Int']:
+                if self.ui.logic_switch_value.currentText() == 'Value':
+                    self.float_widget.show()
+                    self.bool_widget.hide()
+                    self.spacer.show()
+                self.ui.logic_switch_value.setStyleSheet(f"color: rgb{FLOAT_COLOR};")
+                self.ui.property_class_4.setStyleSheet(f"color: rgb{FLOAT_COLOR};")
+                self.variable.combobox.filter_types = ['Float', 'Int']  # Fixed capitalization
+
+            elif current_type == 'Bool':
+                if self.ui.logic_switch_value.currentText() == 'Value':
+                    self.float_widget.hide()
+                    self.bool_widget.show()
+                self.spacer.show()
+                self.ui.logic_switch_value.setStyleSheet(f"color: rgb{BOOL_COLOR};")
+                self.ui.property_class_4.setStyleSheet(f"color: rgb{BOOL_COLOR};")
+                self.variable.combobox.filter_types = ['Bool']
+
+            self.m_DataType = current_type.upper()
+        except Exception as e:
+            print(f"Error in logic_switch: {e}")
+
     def on_changed(self):
-        self.logic_switch()
-        variables = self.get_variables()
-        self.change_value()
-        self.edited.emit()
+        """Handle any change in the widget state"""
+        try:
+            self.logic_switch_value()
+            self.logic_switch()
+            variables = self.get_variables()
+            self.text_line.completions.setStringList(variables + expression_completer)
+            self.change_value()
+            self.edited.emit()
+        except Exception as e:
+            print(f"Error in on_changed: {e}")
+
     def change_value(self):
-        if self.ui.logic_switch.currentText() == 'Float':
-            self.m_Value = self.float_widget.value
-        elif self.ui.logic_switch.currentText() == 'Bool':
-            self.m_Value = self.bool_widget.get_value()
-        self.m_TargetName = self.variable.combobox.get_variable()
-        self.value = {self.value_class: {'m_TargetName': str(self.m_TargetName), 'm_DataType': str(self.m_DataType), 'm_Value': self.m_Value}}
+        """Update the internal value based on current widget state"""
+        try:
+            current_type = self.ui.logic_switch.currentText()
+            current_mode = self.ui.logic_switch_value.currentText()
+
+            if current_type in ['Float', 'Int']:
+                if current_mode == 'Value':
+                    self.m_Value = self.float_widget.value
+                elif current_mode == 'Expression':
+                    self.m_Value = {'m_Expression': str(self.text_line.toPlainText())}
+            elif current_type == 'Bool':
+                if current_mode == 'Value':
+                    self.m_Value = self.bool_widget.get_value()
+                elif current_mode == 'Expression':
+                    self.m_Value = {'m_Expression': str(self.text_line.toPlainText())}
+
+            self.m_TargetName = self.variable.combobox.get_variable()
+            self.value = {
+                self.value_class: {
+                    'm_TargetName': str(self.m_TargetName),
+                    'm_DataType': str(self.m_DataType),
+                    'm_Value': self.m_Value
+                }
+            }
+        except Exception as e:
+            print(f"Error in change_value: {e}")
 
     def get_variables(self, search_term=None):
+        """Get list of available variable names"""
         data_out = []
-        for i in range(self.variables_scrollArea.count()):
-            widget = self.variables_scrollArea.itemAt(i).widget()
-            if widget:
-                data_out.append(widget.name)
+        try:
+            for i in range(self.variables_scrollArea.count()):
+                widget = self.variables_scrollArea.itemAt(i).widget()
+                if widget and hasattr(widget, 'name'):
+                    data_out.append(widget.name)
+        except Exception as e:
+            print(f"Error in get_variables: {e}")
         return data_out
