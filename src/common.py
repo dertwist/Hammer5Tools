ae7e4668c8ea5bf630d8e00b44962509cc6a1653
dertwist <45437750+dertwist@users.noreply.github.com>
2026-03-01 15:55:26 +0100

    4.9.5

--- src/common.py
+++ src/common.py
@@ -13,7 +13,7 @@ import time
 from typing import Dict, List, Optional
 
 # Versions
-app_version = '4.9.4'
+app_version = '4.9.5'
 
 #======================================================<  Copied from preferences.py file  >===================================================
 
