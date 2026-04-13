b2873247d4f9d945f85e893ba08e092d9f0f7894
dertwist <45437750+dertwist@users.noreply.github.com>
2026-04-13 11:54:32 +0200

    cleanup

--- test_dep2.py
+++
@@ -1,7 +0,0 @@
-from src.editors.asset_exporter.dependency_resolver import DependencyResolver
-import sys
-dr = DependencyResolver("D:/CG/Projects/Other/Hammer5Tools")
-dr._visited.clear()
-dr.missing_deps.clear()
-print(dr.ASSET_DEPENDENCY_KEYS)
-print("done")
