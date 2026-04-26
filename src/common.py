0de15e6388f841e4ba0582e7b37bb279f2348d45
dertwist <45437750+dertwist@users.noreply.github.com>
2026-04-26 17:35:07 +0200

    ## 🔧 Main App
    * Fixed error "No module named 'resources_rc'"

--- src/common.py
+++ src/common.py
@@ -46,7 +46,7 @@ def generate_unique_name(base_name: str, existing_names: Set[str], separator: st
 from pathlib import Path
 
 # Versions
-app_version = '5.0.1'
+app_version = '5.0.2'
 
 def get_channel() -> str:
     """
