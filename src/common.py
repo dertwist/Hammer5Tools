e372cefa79eec1481794e4a9ad1b908fa25a14d6
dertwist <45437750+dertwist@users.noreply.github.com>
2026-04-27 17:00:43 +0200

    5.0.3

--- src/common.py
+++ src/common.py
@@ -46,7 +46,7 @@ def generate_unique_name(base_name: str, existing_names: Set[str], separator: st
 from pathlib import Path
 
 # Versions
-app_version = '5.0.2'
+app_version = '5.0.3'
 
 def get_channel() -> str:
     """
